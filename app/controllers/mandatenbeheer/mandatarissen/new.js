722b0fb86cc53a30303efbd9f37563ea2ea09d54
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-04-19 21:08:14 +0200

    draft working version create

--- app/controllers/mandatenbeheer/mandatarissen/new.js
+++ app/controllers/mandatenbeheer/mandatarissen/new.js
@@ -1,9 +1,80 @@
 import Controller from '@ember/controller';
 import { computed } from '@ember/object';
+import { task } from 'ember-concurrency';
+import { A } from '@ember/array';
+import moment from 'moment';
 
 export default Controller.extend({
+  disableSave: computed('mandaat', 'start', function(){
+    return !(this.get('mandaat') && this.get('start'));
+  }),
+
+  disableCancel: computed('save.isIdle', function(){
+    return !this.save.isIdle;
+  }),
+
+  save: task(function* (){
+    yield this.createNewBeleidsdomeinen();
+
+    let lidmaatschap;
+
+    if(this.get('fractie')){
+      lidmaatschap = yield this.createLidmaatschap();
+    }
+
+    let mandataris = yield this.get('store').createRecord('mandataris', {
+      start: this.get('start'),
+      einde: this.get('einde'),
+      bekleedt: this.get('mandaat'),
+      heeftLidmaatschap: lidmaatschap,
+      isBestuurlijkeAliasVan: this.get('persoon'),
+      beleidsdomein: this.get('beleidsdomeinen')
+    });
+
+    return mandataris.save();
+  }).drop(),
+
+  async createNewBeleidsdomeinen(){
+    if(!this.get('beleidsdomeinen')){
+      this.set('beleidsdomeinen', A());
+      return;
+    }
+
+    //TODO: should we cleanup on fail?
+    let savingD = this.get('beleidsdomeinen').map(d => {
+      if(d.get('isNew'))
+         return d.save();
+    });
+    
+    return Promise.all(savingD);
+  },
+
+  async createLidmaatschap(){
+    let fractie = this.get('fractie');
+    let tijdsinterval = await this.get('store').createRecord('tijdsinterval', {
+      begin: this.get('start'), einde: this.get('einde')
+    });
+    await tijdsinterval.save();
+
+    try{
+      let lidmaatschap =  await this.get('store').createRecord('lidmaatschap', {
+        binnenFractie: fractie, lidGedurende: tijdsinterval
+      });
+      await lidmaatschap.save();
+      return lidmaatschap;
+    }
+    catch (error){
+      await tijdsinterval.delete();
+      throw error;
+    }
+  },
+
+  parseDate(){
+
+  },
+
   actions: {
-    setPersoon(persoon){
+    async setPersoon(persoon){
       this.set('persoon', persoon);
     },
     setFractie(fractie){
@@ -11,6 +82,16 @@ export default Controller.extend({
     },
     setMandaat(mandaat){
       this.set('mandaat', mandaat);
+    },
+    setBeleidsdomeinen(beleidsdomeinen){
+      this.set('beleidsdomeinen', beleidsdomeinen);
+    },
+    async save(){
+      let mandataris = await this.save.perform();
+      alert('ready');
+    },
+    cancel(){
+      this.transitionToRoute('mandatenbeheer.mandatarissen');
     }
   }
 });
