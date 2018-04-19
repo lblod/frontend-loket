9ffc15bc84d64ae367d2701e7dad62db80bf7f92
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-04-19 11:49:21 +0200

    added mandaten selector

---
+++ app/components/mandatenbeheer/mandaat-bestuursorganen-selector.js
@@ -0,0 +1,25 @@
+import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { task, timeout } from 'ember-concurrency';
+
+export default Component.extend({
+  store: service(),
+
+  searchByBestuursfunctie: task(function* (searchData) {
+    yield timeout(300);
+    let queryParams = {
+      sort:'bestuursfunctie.label',
+      include: 'bestuursfunctie',
+      'filter[bestuursfunctie]': searchData,
+      'filter[bevat-in][id]': this.get('bestuursorganen').map(o => o.get('id')).join(',')
+    };
+    return yield this.get('store').query('mandaat', queryParams);
+  }),
+
+  actions: {
+    select(mandaat){
+      this.set('mandaat', mandaat);
+      this.get('onSelect')(mandaat);
+    }
+  }
+});
