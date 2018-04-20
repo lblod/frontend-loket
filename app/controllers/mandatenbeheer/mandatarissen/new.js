0226ba1d921f7b73ec362d7d8c8bb854c52e95aa
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-04-20 10:29:19 +0200

    correct parsing of date

--- app/controllers/mandatenbeheer/mandatarissen/new.js
+++ app/controllers/mandatenbeheer/mandatarissen/new.js
@@ -5,8 +5,18 @@ import { A } from '@ember/array';
 import moment from 'moment';
 
 export default Controller.extend({
-  disableSave: computed('mandaat', 'start', function(){
-    return !(this.get('mandaat') && this.get('start'));
+  dateFormat: 'DD.MM.YYYY',
+
+  startDate: computed('rawStartDate', function(){
+    return moment(this.get('rawStartDate'), this.get('dateFormat'));
+  }),
+
+  endDate: computed('rawEndDate', function(){
+    return moment(this.get('rawEndDate'), this.get('dateFormat'));
+  }),
+
+  disableSave: computed('mandaat', 'rawStartDate', function(){
+    return !(this.get('mandaat') && this.get('rawStartDate'));
   }),
 
   disableCancel: computed('save.isIdle', function(){
@@ -23,8 +33,8 @@ export default Controller.extend({
     }
 
     let mandataris = yield this.get('store').createRecord('mandataris', {
-      start: this.get('start'),
-      einde: this.get('einde'),
+      start: this.get('startDate'),
+      einde: this.get('eindeDate'),
       bekleedt: this.get('mandaat'),
       heeftLidmaatschap: lidmaatschap,
       isBestuurlijkeAliasVan: this.get('persoon'),
@@ -45,14 +55,14 @@ export default Controller.extend({
       if(d.get('isNew'))
          return d.save();
     });
-    
+
     return Promise.all(savingD);
   },
 
   async createLidmaatschap(){
     let fractie = this.get('fractie');
     let tijdsinterval = await this.get('store').createRecord('tijdsinterval', {
-      begin: this.get('start'), einde: this.get('einde')
+      begin: this.get('startDate'), einde: this.get('eindeDate')
     });
     await tijdsinterval.save();
 
@@ -69,27 +79,28 @@ export default Controller.extend({
     }
   },
 
-  parseDate(){
-
-  },
-
   actions: {
     async setPersoon(persoon){
       this.set('persoon', persoon);
     },
+
     setFractie(fractie){
       this.set('fractie', fractie);
     },
+
     setMandaat(mandaat){
       this.set('mandaat', mandaat);
     },
+
     setBeleidsdomeinen(beleidsdomeinen){
       this.set('beleidsdomeinen', beleidsdomeinen);
     },
+
     async save(){
       let mandataris = await this.save.perform();
       alert('ready');
     },
+
     cancel(){
       this.transitionToRoute('mandatenbeheer.mandatarissen');
     }
