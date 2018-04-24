33c777406bdcdca00c11893e89ddb228419fda55
Niels Vandekeybus <progster@gmail.com>
2018-04-24 10:15:45 +0200

    fix faulty merge

--- app/controllers/mandatenbeheer/mandatarissen/new.js
+++ app/controllers/mandatenbeheer/mandatarissen/new.js
@@ -4,18 +4,12 @@ export default Controller.extend({
   clearProperties(){
     this.set('mandataris', null);
   },
+  createPersoon: false,
   actions: {
     setPersoon(persoon){
       let mandataris = this.get('store').createRecord('mandataris');
       mandataris.set('isBestuurlijkeAliasVan', persoon);
       this.set('mandataris', mandataris);
-    },
-  createPersoon: false,
-  actions: {
-    async setPersoon(persoon){
-      this.set('createPersoon', false);
-      console.log(persoon);
-      this.set('persoon', persoon);
     },
     toggleCreate() {
       this.toggleProperty('createPersoon');
