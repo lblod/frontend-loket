d25ed893f345fa8c64b02881e5834ce8048d58c0
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-04-24 09:18:25 +0200

    updated controlles to use mandataris-edit component new interface

--- app/controllers/mandatenbeheer/mandatarissen/new.js
+++ app/controllers/mandatenbeheer/mandatarissen/new.js
@@ -3,7 +3,9 @@ import Controller from '@ember/controller';
 export default Controller.extend({
   actions: {
     setPersoon(persoon){
-      this.set('persoon', persoon);
+      let mandataris = this.get('store').createRecord('mandataris');
+      mandataris.set('isBestuurlijkeAliasVan', persoon);
+      this.set('mandataris', mandataris);
     },
     save(){
       this.transitionToRoute('mandatenbeheer.mandatarissen');
