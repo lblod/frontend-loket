eeb950cf6f14b2eec9efcbb9cb36421ccf27b667
Niels Vandekeybus <progster@gmail.com>
2018-04-25 09:49:40 +0200

    include authenticatedroute mixin in all authenticated routes

--- app/routes/mandatenbeheer/mandatarissen.js
+++ app/routes/mandatenbeheer/mandatarissen.js
@@ -1,7 +1,8 @@
 import Route from '@ember/routing/route';
 import DataTableRouteMixin from 'ember-data-table/mixins/route';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Route.extend(DataTableRouteMixin, {
+export default Route.extend(AuthenticatedRouteMixin, DataTableRouteMixin, {
   modelName: 'mandataris',
 
   async beforeModel(){
