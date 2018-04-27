93da7d7e5b3c9eb36c1d29908456eb4adf33c95a
Niels Vandekeybus <progster@gmail.com>
2018-04-27 10:01:49 +0200

    update wiring of new person to edit mandaat

--- app/controllers/mandatenbeheer/mandatarissen/new-person.js
+++ app/controllers/mandatenbeheer/mandatarissen/new-person.js
@@ -3,10 +3,10 @@ import Controller from '@ember/controller';
 export default Controller.extend({
   actions: {
     onCreate(user) {
-      this.transitionToRoute('mandatenbeheer.mandatarissen.new', {queryParams: {persoonId: user.get('id')}});
+      this.transitionToRoute('mandatenbeheer.mandatarissen.edit', user.get('id'));
     },
     onCancel() {
-      this.transitionToRoute('mandatenbeheer.mandatarissen.new', {queryParams: {persoonId: ''}});
+      this.transitionToRoute('mandatenbeheer.mandatarissen.new');
     }
   }
 });
--- app/controllers/mandatenbeheer/mandatarissen/new.js
+++ app/controllers/mandatenbeheer/mandatarissen/new.js
@@ -3,13 +3,6 @@ import { isBlank } from '@ember/utils';
 import { observer } from '@ember/object';
 
 export default Controller.extend({
-  queryParams: ['persoonId'],
-  persoonId: '',
-  persoonIdObserver: observer('persoonId', async function() {
-    if (isBlank(this.get('persoonId'))) return;
-    const persoon = await this.get('store').findRecord('persoon', this.get('persoonId'));
-    this.setPersoon(persoon);
-  }),
   setPersoon(persoon){
     this.transitionToRoute('mandatenbeheer.mandatarissen.edit', persoon.get('id'));
   },
