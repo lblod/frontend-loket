4daccad57346e3ca4ac28f87438908b3ec73b775
Erika Pauwels <erika.pauwels@gmail.com>
2018-05-01 16:05:50 +0200

    No need to set AuthenticatedRouteMixin on nested routes

--- app/routes/mandatenbeheer/mandatarissen.js
+++ app/routes/mandatenbeheer/mandatarissen.js
@@ -1,8 +1,7 @@
 import Route from '@ember/routing/route';
 import DataTableRouteMixin from 'ember-data-table/mixins/route';
-import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Route.extend(AuthenticatedRouteMixin, DataTableRouteMixin, {
+export default Route.extend(DataTableRouteMixin, {
   modelName: 'mandataris',
 
   async beforeModel(){
--- app/routes/mandatenbeheer/mandatarissen/edit.js
+++ app/routes/mandatenbeheer/mandatarissen/edit.js
@@ -1,8 +1,7 @@
 import Route from '@ember/routing/route';
 import RSVP from 'rsvp';
-import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Route.extend(AuthenticatedRouteMixin, {
+export default Route.extend({
 
   async model(params){
     const parentModel = this.modelFor('mandatenbeheer');
