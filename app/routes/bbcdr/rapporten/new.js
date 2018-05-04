ed02f211a34a8b41a782beacb3a9bc05ed1632bf
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-05-04 13:07:46 +0200

    rewiring with new component

--- app/routes/bbcdr/rapporten/new.js
+++ app/routes/bbcdr/rapporten/new.js
@@ -1,4 +1,8 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  async model(){
+    let status = (await this.get('store').findAll('document-status')).find(s => s.isConcept);
+    return this.get('store').createRecord('bbcdr-report', {status});
+  }
 });
