f03d0f2e7800d27909bda7fd1584bd4b1b7233e7
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-05-04 13:02:02 +0200

    updated file card

--- app/components/bbcdr/file-card.js
+++ app/components/bbcdr/file-card.js
@@ -1,15 +1,9 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
-import humanReadableSize from '../../utils/human-readable-bytesize';
 
 export default Component.extend({
   classNames: ["upload__files upload__files--has-files"],
   active: true,
-
-  fileSize: computed('file', function(){
-    let size = humanReadableSize(this.get('file.size'));
-    return `(${size})`;
-  }),
 
   actions: {
     delete(){
