b6b67ba5718ae5608cd20f8e3291c91cbfa94f19
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-05-04 13:06:12 +0200

    removed obsolete util

--- app/utils/human-readable-bytesize.js
+++
@@ -1,7 +0,0 @@
-//ripped from https://stackoverflow.com/questions/15900485/correct-way-to-convert-size-in-bytes-to-kb-mb-gb-in-javascript
-export default function humanReadableBytesize(bytes) {
-  var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-  if (bytes == 0) return '0 Byte';
-  var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-  return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
-}
