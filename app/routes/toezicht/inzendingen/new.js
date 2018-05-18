f87fc492d96516528f7fcfead20583eef640a613
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-05-18 18:08:11 +0200

    added first form

--- app/routes/toezicht/inzendingen/new.js
+++ app/routes/toezicht/inzendingen/new.js
@@ -1,4 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model(){
+    return this.get('store').findRecord('form-node', '8a0eae1a867ace6641db65eb1f7d9d4a22927eb614dcf210550d66d801f4481e');
+  }
 });
