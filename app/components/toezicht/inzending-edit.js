f1997ecfaa4f4356f7c0a66962f703fd5b7e2a0f
Niels Vandekeybus <progster@gmail.com>
2018-05-22 10:50:56 +0200

    initial support for file uploads in 'toezicht'

--- app/components/toezicht/inzending-edit.js
+++ app/components/toezicht/inzending-edit.js
@@ -1,9 +1,15 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import { A } from '@ember/array';
 
 export default Component.extend({
   classNames: ['col--4-12 col--9-12--m col--12-12--s container-flex--contain'],
   router: service(),
+  files: null,
+  init() {
+    this._super(...arguments);
+    this.set('files',A());
+  },
 
   actions: {
     async initDynamicForm(dForm){
@@ -13,7 +19,15 @@ export default Component.extend({
       this.get('router').transitionTo('toezicht.inzendingen.index');
     },
     async save(){
-      this.get('dynamicForm').save();
+      const model = await this.get('dynamicForm').save();
+      model.set('files', this.files);
+      model.save();
+    },
+    async addFile(file) {
+      this.files.pushObject(file);
+    },
+    async deleteFile(file) {
+      this.files.removeObject(file);
     }
   }
 });
