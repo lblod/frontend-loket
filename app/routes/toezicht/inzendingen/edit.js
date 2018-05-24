fb82d842ee3a15fbe5ef0ad98d2ee38c23b67cae
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-05-24 09:32:39 +0200

    start loading relations in route

--- app/routes/toezicht/inzendingen/edit.js
+++ app/routes/toezicht/inzendingen/edit.js
@@ -2,8 +2,17 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   async model(params) {
-    let models =  (await this.get('store').query('form-solution', {'filter[inzending-voor-toezicht][id]': params.id,
-                                                                   include: 'inzending-voor-toezicht'}));
+    let models =  (await this.get('store').query('form-solution',
+                                                 {'filter[inzending-voor-toezicht][id]': params.id,
+                                                  include: ['inzending-voor-toezicht',
+                                                            'inzending-voor-toezicht.status',
+                                                            'inzending-voor-toezicht.last-modifier',
+                                                            'inzending-voor-toezicht.bestuurseenheid',
+                                                            'inzending-voor-toezicht.inzending-type',
+                                                            'inzending-voor-toezicht.besluit-type',
+                                                            'inzending-voor-toezicht.bestuursorgaan',
+                                                            'inzending-voor-toezicht.tax-rates'
+                                                           ].join(',')}));
     return models.firstObject;
   }
 });
