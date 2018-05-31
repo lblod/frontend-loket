b4f747b1fce0978998eff6e1ebdbbc6f22609847
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-05-31 13:46:37 +0200

    exporting components to ember-mu-dynamic-forms

--- app/components/toezicht/input-fields/dynamic-select/power-select.js
+++
@@ -1,7 +0,0 @@
-import EmberPowerSelect from 'ember-power-select/components/power-select';
-
-export default EmberPowerSelect.extend({
-  loadingMessage: 'Aan het laden...',
-  noMatchesMessage: 'Geen resultaten',
-  searchMessage: 'Typ om te zoeken'
-});
--- app/models/form-node.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-import { hasMany } from 'ember-data/relationships';
-import attr from 'ember-data/attr';
-
-export default DS.Model.extend({
-  inputTypeMap: attr('json'),
-  children: hasMany('form-input')
-});
