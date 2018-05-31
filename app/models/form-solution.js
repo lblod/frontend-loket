c82b8bbc33f4e189f6eb3e5504c706364982d8e4
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-05-31 13:47:42 +0200

    custom dynamic-form components should follow new structure

--- app/models/form-solution.js
+++ app/models/form-solution.js
@@ -1,9 +1,6 @@
-import DS from 'ember-data';
-import attr from 'ember-data/attr';
+import FormSolution from '@lblod/ember-mu-dynamic-forms/models/form-solution' ;
 import { belongsTo } from 'ember-data/relationships';
 
-export default DS.Model.extend({
-  hasOwner: attr(),
-  inzendingVoorToezicht: belongsTo('inzending-voor-toezicht'),
-  formNode: belongsTo('form-node')
+export default FormSolution.extend({
+  inzendingVoorToezicht: belongsTo('inzending-voor-toezicht')
 });
