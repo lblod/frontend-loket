5cb8ca6dca876df167e4df435e832affaa2d6e86
Aad Versteden <madnificent@gmail.com>
2018-08-27 14:40:22 +0200

    Style required fields in dynamic forms
    The dynamic fields are now styled in the forms.  The design is
    different from the intended design.  This may be updated in future
    releases.

---
+++ app/components/input-fields/input/edit.js
@@ -0,0 +1,6 @@
+import Component from '@ember/component';
+import InputFieldComponent from '@lblod/ember-mu-dynamic-forms/components/input-fields/input/edit';
+
+export default InputFieldComponent.extend({
+  classNames: 'u-spacer--bottom--small'
+});
