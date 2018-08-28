07580ad80a8e83f1ef81290c92238feaccf8b431
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-08-28 10:56:07 +0200

    use of internal value for date input

--- app/components/input-fields/date/show.js
+++ app/components/input-fields/date/show.js
@@ -1,7 +1,9 @@
+import { oneWay } from '@ember/object/computed';
 import Component from '@ember/component';
 import InputField from '@lblod/ember-mu-dynamic-forms/mixins/input-field';
 
 export default Component.extend( InputField, {
   datePickerClass: 'date-picker-mandataris-edit',
-  dateFormat: 'DD-MM-YYYY'
+  dateFormat: 'DD-MM-YYYY',
+  internalValue: oneWay('value')
 });
