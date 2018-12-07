bddf67b4d63d399bbc8d64ebc8c5d388baaf0db4
Mehran Naghizadeh <tarhineh@gmail.com>
2018-12-07 14:23:31 +0100

    conversatie model is connected to the table

--- app/routes/berichtencentrum/berichten.js
+++ app/routes/berichtencentrum/berichten.js
@@ -1,4 +1,13 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+    modelName: 'conversatie',
+
+    async model () {
+
+        let data = this.store.findAll('conversatie');
+        return data;
+    }
+
 });
