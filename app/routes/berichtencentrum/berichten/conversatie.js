73c3cb92b59612c18a7af0cbdd76f84af35a5fd4
Mehran Naghizadeh <tarhineh@gmail.com>
2018-12-07 14:39:08 +0100

    Fetch model data based on convertatie id

--- app/routes/berichtencentrum/berichten/conversatie.js
+++ app/routes/berichtencentrum/berichten/conversatie.js
@@ -1,4 +1,9 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+    model(params) {
+        let record = this.store.peekRecord ('conversatie', params.id);
+        return record;
+    }
 });
