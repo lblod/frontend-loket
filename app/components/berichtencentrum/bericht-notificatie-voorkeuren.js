bd2b8669ed7ffee7dafc91fe20e7bbf97f523cb3
Aad Versteden <madnificent@gmail.com>
2018-12-19 16:52:14 +0100

    Update style of javascript sources
    No functional changes, should match better with commonly accepted
    JavaScript style.

--- app/components/berichtencentrum/bericht-notificatie-voorkeuren.js
+++ app/components/berichtencentrum/bericht-notificatie-voorkeuren.js
@@ -2,47 +2,39 @@ import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 
 export default Component.extend({
+  isHidden: true,
+  currentSession: service(),
 
+  wilMailOntvangen: true,
+  emailAddress: 'mail@adres.com',
 
-    isHidden: true,
-    store: service(),
-    currentSession: service(),
+  didReceiveAttrs() {
+    this._super(...arguments);
+    this
+      .get('currentSession.group')
+      .then((group) => {
+        this.set('wilMailOntvangen', group.get("wilMailOntvangen"));
+        this.set('emailAddress', group.get("mailAdres"));
+      });
+  },
 
-    wilMailOntvangen: true,
-    emailAddress: 'mail@adres.com',
-
-    actions: {
-
-        show() {
-            this.set("isHidden", false);
-        },
-
-        cancel() {
-            this.set("isHidden", true);
-        },
-
-        async commit() {
-            
-            this.cancel();
-            
-            let group = await this.get('currentSession.group');
-            group.set('mailAdres', this.emailAddress);
-            group.set('wilMailOntvangen', this.wilMailOntvangen);
-            await group.save();
-            
-        }
+  actions: {
+    show() {
+      this.set("isHidden", false);
     },
 
+    cancel() {
+      this.set("isHidden", true);
+    },
 
-
-    didReceiveAttrs() {
-
-        this._super(...arguments);
-        
-            this.get('currentSession.group')
-            .then((group) => {
-                this.set('wilMailOntvangen',    group.get("wilMailOntvangen"));
-                this.set('emailAddress',        group.get("mailAdres"));
-            });
+    async commit() {
+      this.cancel();
+      
+      let group = await this.get('currentSession.group');
+      group.set('mailAdres', this.emailAddress);
+      group.set('wilMailOntvangen', this.wilMailOntvangen);
+      await group.save();
     }
+  }
+
 });
