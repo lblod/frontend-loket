460ec1ccd346002164fc56aca20d443587612419
Aad Versteden <madnificent@gmail.com>
2018-12-23 18:25:50 +0100

    Removing dead code

--- app/components/berichtencentrum/conversatie-view.js
+++ app/components/berichtencentrum/conversatie-view.js
@@ -4,10 +4,7 @@ import { inject as service } from '@ember/service';
 export default Component.extend({
   classNames      : ['col--4-12 col--9-12--m col--12-12--s container-flex--contain'],
   router          : service(),
-  store           : service(),
-  currentSession  : service(),
   showExitModal   : false,
-  
   actions: {
     close(){
       this.close();
