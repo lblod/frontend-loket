da686c0b77134d05c910808c9ebeb05e18e2a066
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-01-01 15:17:00 +0100

    added 'hooks' for future

--- app/components/toezicht/url-box.js
+++ app/components/toezicht/url-box.js
@@ -1,10 +1,17 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
-import { task } from 'ember-concurrency';
+import { task, timeout } from 'ember-concurrency';
 
 export default Component.extend({
   store: service(),
+
+  saveAddress: task(function * (address){
+    //TODO: this is needs probably some discussion with designer UX to tweak the XP. So now deactivated
+    yield address.save();
+    //In the future there will be some backend service checking wether file exists and can be downloaded
+    //That's why we want to save it here. User needs to know immediately if his url is ok.
+  }),
 
   actions: {
     addUrlField() {
@@ -14,6 +21,10 @@ export default Component.extend({
 
     delete(address){
       this.onDelete(address);
+    },
+
+    save(address){
+      this.saveAddress.perform(address);
     }
   }
 });
