1e3882d3ad28c9f88e638bc1d3a3e4a5adf8b672
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-01-01 15:26:37 +0100

    linting

--- app/components/toezicht/url-box.js
+++ app/components/toezicht/url-box.js
@@ -1,6 +1,5 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
-import { computed } from '@ember/object';
 import { task, timeout } from 'ember-concurrency';
 
 export default Component.extend({
