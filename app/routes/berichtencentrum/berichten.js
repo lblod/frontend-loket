ccf80623d80a97c611081579b89d0fe383534177
Claire Lovisa <lovisa.claire@gmail.com>
2019-01-02 14:58:37 +0100

    Complete title of berichtencentrum page

--- app/routes/berichtencentrum/berichten.js
+++ app/routes/berichtencentrum/berichten.js
@@ -1,6 +1,19 @@
 import Route from '@ember/routing/route';
 import DataTableRouteMixin from 'ember-data-table/mixins/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend(DataTableRouteMixin, {
-  modelName: 'conversatie'
+  currentSession: service(),
+
+  modelName: 'conversatie',
+
+  async beforeModel() {
+    const bestuurseenheid = await this.get('currentSession.group');
+    this.set('bestuurseenheid', bestuurseenheid);
+  },
+
+  setupController(controller, model) {
+    this._super(controller, model);
+    controller.set('bestuurseenheid', this.bestuurseenheid);
+  }
 });
