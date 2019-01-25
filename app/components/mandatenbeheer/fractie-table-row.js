90a91105bb3f38b519cc244f9de72bc9436e09d7
Claire Lovisa <lovisa.claire@gmail.com>
2019-01-25 13:13:37 +0100

    WIP Check is the fractie has memberships

--- app/components/mandatenbeheer/fractie-table-row.js
+++ app/components/mandatenbeheer/fractie-table-row.js
@@ -1,8 +1,12 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
 import { reads } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
+import { task } from 'ember-concurrency';
 
 export default Component.extend({
+  store: service(),
+
   tagName: 'tr',
   isEditing: false,
   isAdding: false,
@@ -24,13 +28,24 @@ export default Component.extend({
   bestuursorganenInTijdStart: reads('fractie.bestuursorganenInTijd.firstObject.bindingStart'),
   bestuursorganenInTijdEinde: reads('fractie.bestuursorganenInTijd.firstObject.bindingEinde'),
 
+  didReceiveAttrs() {
+    this._super(...arguments);
+    this.hasFractieLidmaatschap.perform();
+  },
+
+  hasFractieLidmaatschap: task(function* () {
+    const fractie = yield this.fractie;
+    const lidmaatschap = yield this.store.query('lidmaatschap', {'filter[lidmaatschap][binnenFractie]': fractie});
+    // return lidmaatschap;
+  }).drop(),
+
   actions: {
     startFractieEdit() {
       this.set("nameBeforeEdit", this.fractie.naam);
       this.openEditSession();
     },
     cancelFractieEdit() {
-      if (this.isAdding) { 
+      if (this.isAdding) {
         this.onEditFinish('');
       } else {
         this.set("fractie.naam", this.nameBeforeEdit);
@@ -50,7 +65,7 @@ export default Component.extend({
       } catch (err) {
         this.onEditFinish(this.fractie);
       }
-      
+
 
       //-- finalize the session
       this.closeEditSession();
