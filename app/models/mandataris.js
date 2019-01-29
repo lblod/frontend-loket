4d0029357d03c1ee23ff89a5872059ec6704b06f
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-01-29 18:21:30 +0100

    feature: admin-mode only -> shows where the data comes from

--- app/models/mandataris.js
+++ app/models/mandataris.js
@@ -1,6 +1,7 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { belongsTo, hasMany } from 'ember-data/relationships';
+import { computed } from '@ember/object';
 
 export default Model.extend({
   rangorde: attr('language-string'),
@@ -13,5 +14,10 @@ export default Model.extend({
   rechtsgrondenBeeindiging: hasMany('rechtsgrond-beeindiging', { inverse: 'bekrachtigtOntslagenVan' }),
   tijdelijkeVervangingen: hasMany('mandataris', { inverse: null }),
   beleidsdomein: hasMany('beleidsdomein-code', { inverse: null }),
-  status: belongsTo('mandataris-status-code', { inverse: null })
+  status: belongsTo('mandataris-status-code', { inverse: null }),
+  generatedFrom: attr('uri-set'),
+
+  generatedFromGelinktNotuleren: computed('generatedFrom', function(){
+    return (this.generatedFrom || []).some(uri => uri == 'http://mu.semte.ch/vocabularies/ext/mandatenExtractorService');
+  })
 });
