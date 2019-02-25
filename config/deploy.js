ad985198bf43bc178e034a4acafa15de8ea250f0
Niels Vandekeybus <progster@gmail.com>
2019-02-25 13:37:45 +0100

    use new dev environment for deploy

--- config/deploy.js
+++ config/deploy.js
@@ -10,17 +10,17 @@ module.exports = function(deployTarget) {
     },
     'ssh-index': {
       username: 'root',
-      host: 'dock.semte.ch',
-      remoteDir: '/data/digitaal-loket/dev-loket/loket-app',
+      host: 'rpio-dev.s.redpencil.io',
+      remoteDir: '/data/app-digitaal-loket/dev/loket-app',
       agent: process.env.SSH_AUTH_SOCK,
-      port: 2275,
+      port: 22,
       allowOverwrite: true
     },
     'rsync': {
-      dest: '/data/digitaal-loket/dev-loket/loket-app',
+      dest: '/data/app-digitaal-loket/dev/loket-app',
       username: 'root',
-      host: 'dock.semte.ch',
-      port: 2275,
+      host: 'rpio-dev.s.redpencil.io',
+      port: 22,
       delete: false,
       privateKey: process.env.SSH_AUTH_SOCK,
       arg:['--verbose']
