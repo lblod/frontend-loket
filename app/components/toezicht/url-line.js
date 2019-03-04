3ab567dded7f8f7418061dcf421d229a9d0e66b5
Mehran Naghizadeh <tarhineh@gmail.com>
2019-03-04 21:25:48 +0100

    Getting rid of unnecessary ember-concurrency task

--- app/components/toezicht/url-line.js
+++ app/components/toezicht/url-line.js
@@ -1,23 +1,18 @@
 import Component from '@ember/component';
-import { task } from 'ember-concurrency';
 
 export default Component.extend({
-
-  init() {
-    this._super(...arguments);
-    this.set('downloadLink', null);
-  },
+  downloadLink: null,
 
   async didReceiveAttrs() {
     this._super(...arguments);
-    this.get('getDownloadLink').perform();
+    this.getDownloadLink();
   },
 
-  getDownloadLink: task(function* () {
-    const cr = yield this.url.get('cacheResource');
+  getDownloadLink: async function () {
+    const cr = await this.url.get('cacheResource');
     if (cr) {
       const link = `/files/${cr.get('id')}/download?name=${cr.get('filename')}`;
       this.set('downloadLink', link);
     }
-  }),
+  },
 });
