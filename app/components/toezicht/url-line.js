b8bfa073dcea3695bc859b0d3e83e8024f86e252
Mehran Naghizadeh <tarhineh@gmail.com>
2019-03-04 23:26:36 +0100

    A computed model property model rather than an async controller method

--- app/components/toezicht/url-line.js
+++ app/components/toezicht/url-line.js
@@ -1,22 +1,5 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  downloadLink: null,
-
-  async didReceiveAttrs() {
-    this._super(...arguments);
-    this.getDownloadLink();
-  },
-
-  getDownloadLink: async function () {
-    try {
-      const cr = await this.url.get('cacheResource');
-      if (cr) {
-        const link = `/files/${cr.get('id')}/download?name=${cr.get('filename')}`;
-        this.set('downloadLink', link);
-      }
-    } catch (err) {
-      // no or bad url object is provided
-    }
-  },
+  
 });
--- app/models/file-address.js
+++ app/models/file-address.js
@@ -1,7 +1,17 @@
 import DS from 'ember-data';
 import { belongsTo } from 'ember-data/relationships';
+import { computed }  from '@ember/object';
 
 export default DS.Model.extend({
   address: DS.attr(),
   cacheResource: belongsTo('file', { inverse: null }),
+
+  downloadLink: computed('cacheResource.{id,filename}', function() {
+    const cacheResourceId = this.get('cacheResource.id');
+    // TODO: as said in the PR: update the name cacheResource, it's disturbing :-)
+    const filename = this.get('cacheResource.filename');
+    const result =  cacheResourceId && filename && `/files/${cacheResourceId}/download?name=${filename}`;
+    debugger;
+    return result;
+  })
 });
