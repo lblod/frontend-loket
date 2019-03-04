8252013d3a0080afd049f74f146c4af7a8529490
Mehran Naghizadeh <tarhineh@gmail.com>
2019-03-04 22:06:33 +0100

    catch the failure to make the ember component test happy

--- app/components/toezicht/url-line.js
+++ app/components/toezicht/url-line.js
@@ -9,10 +9,14 @@ export default Component.extend({
   },
 
   getDownloadLink: async function () {
-    const cr = await this.url.get('cacheResource');
-    if (cr) {
-      const link = `/files/${cr.get('id')}/download?name=${cr.get('filename')}`;
-      this.set('downloadLink', link);
+    try {
+      const cr = await this.url.get('cacheResource');
+      if (cr) {
+        const link = `/files/${cr.get('id')}/download?name=${cr.get('filename')}`;
+        this.set('downloadLink', link);
+      }
+    } catch (err) {
+      // no or bad url object is provided
     }
   },
 });
