2ed1440e81143fdb509ce9a64149189da5cc5c42
Aad Versteden <madnificent@gmail.com>
2019-03-11 22:43:38 +0100

    Extract helper function for logging in
    The session helper ensures it's easy to log in.  This should help in
    testing the components related to logging in.

---
+++ tests/helpers/session.js
@@ -0,0 +1,39 @@
+import { currentSession, authenticateSession, invalidateSession} from 'ember-simple-auth/test-support';
+
+const session = async function(server, options = {}) {
+  const defaultOptions = {
+    roles: [
+      "LoketLB-mandaatGebruiker","LoketLB-berichtenGebruiker","LoketLB-bbcdrGebruiker","LoketLB-toezichtGebruiker","LoketLB-leidinggevendenGebruiker","LoketLB-toezichtGebruiker","LoketLB-leidinggevendenGebruiker","LoketLB-bbcdrGebruiker","LoketLB-berichtenGebruiker","LoketLB-mandaatGebruiker"
+    ]};
+
+  options = Object.assign( defaultOptions, options );
+
+  await authenticateSession({
+    "data":{
+      "type":"sessions",
+      "id":"5c86864ada98f8000c000015",
+      "attributes":{"roles": options.roles}},
+    "relationships":{
+      "account":{
+        "data":{"type":"accounts","id":"9b875bc387960fd6efa0065bdff32877"}},
+      "group":{
+        "data":{"type":"bestuurseenheden","id":"974816591f269bb7d74aa1720922651529f3d3b2a787f5c60b73e5a0384950a4"}
+      }
+    }
+  }
+  );
+  const user = server.create('gebruiker', {
+    voornaam: "John",
+    achternaam: "Doe"
+  });
+  const acc = server.create('account', {
+    "provider":"https://github.com/lblod/mock-login-service",
+    "id":"9b875bc387960fd6efa0065bdff32877",
+    "gebruiker": user
+  });
+  const bestuurseenheid = server.create('bestuurseenheid', {
+    id: "974816591f269bb7d74aa1720922651529f3d3b2a787f5c60b73e5a0384950a4"
+  });
+}
+
+export default session;
