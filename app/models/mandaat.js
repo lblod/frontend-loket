920968d241712c46ba1e21035d75cbf7c660a0f3
Erika Pauwels <erika.pauwels@gmail.com>
2019-04-19 16:27:51 +0200

    Set inverse of many-to-many relation to null
    Ember Data incorrectly fetches data in a many-to-many relation. It
    assumes to have the correct data already. By setting inverse: null we
    enforce Ember Data to load data on both sides.

--- app/models/mandaat.js
+++ app/models/mandaat.js
@@ -5,5 +5,5 @@ import { belongsTo, hasMany } from 'ember-data/relationships';
 export default Model.extend({
   aantalHouders: attr(),
   bestuursfunctie: belongsTo('bestuursfunctie-code', { inverse: null }),
-  bevatIn: hasMany('bestuursorgaan', { inverse: 'bevat' })
+  bevatIn: hasMany('bestuursorgaan', { inverse: null })
 });
