2fb4978b2e0a6dcdeeaddb93a93876e766c652dc
Niels Vandekeybus <progster@gmail.com>
2019-04-23 14:47:21 +0200

    remove dead code

--- app/models/tijdsgebonden-entiteit.js
+++
@@ -1,9 +0,0 @@
-import Model from 'ember-data/model';
-import attr from 'ember-data/attr';
-import { belongsTo } from 'ember-data/relationships';
-
-export default Model.extend({
-  bindingEinde: attr('date'),
-  bindingStart: attr('date'),
-  isTijdsspecialisatieVan: belongsTo('entiteit', { inverse: null })
-});
