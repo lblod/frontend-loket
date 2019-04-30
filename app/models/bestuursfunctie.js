2fd288b49ff4ae821d6e391e6dd8ba33a4935877
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-04-30 14:16:52 +0200

    update model

--- app/models/bestuursfunctie.js
+++ app/models/bestuursfunctie.js
@@ -1,8 +1,15 @@
+import attr from 'ember-data/attr';
 import Model from 'ember-data/model';
+import { collect } from '@ember/object/computed';
 import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
-  contactinfo: belongsTo('contact-punt'),
-  bevatIn: hasMany('bestuursorgaan'),
-  bestuursfunctieCode: belongsTo('bestuurs-functie-code')
+  // A string representation of this model, based on its attributes.
+  // This is what mu-cl-resources uses to search on, and how the model will be presented while editing relationships.
+  stringRep: collect.apply(this,['id']),
+
+  uri: attr(),
+  rol: belongsTo('bestuursfunctie-code', { inverse: null }),
+  contactinfo: belongsTo('contact-punt', { inverse: null }),
+  bevatIn: hasMany('bestuurorgaan', { inverse: null })
 });
