863288001e3499220d15320e39058bf42d3d1d0b
Mehran Naghizadeh <tarhineh@gmail.com>
2019-05-13 13:30:47 +0200

    Set modelName for the  'bestuursfuncties' route

--- app/routes/leidinggevendenbeheer/bestuursfuncties.js
+++ app/routes/leidinggevendenbeheer/bestuursfuncties.js
@@ -4,6 +4,8 @@ import { inject as service } from '@ember/service';
 
 export default Route.extend(DataTableRouteMixin, {
   currentSession: service(),
+  modelName: 'bestuursfunctie',
+
   async beforeModel() {
     const bestuurseenheid = await this.get('currentSession.group');
     this.set('bestuurseenheid', bestuurseenheid);
