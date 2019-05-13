9bdf457a45908ff541cfd86f67083187ecd27145
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-05-13 16:20:00 +0200

    route mixin

--- app/routes/leidinggevendenbeheer.js
+++ app/routes/leidinggevendenbeheer.js
@@ -1,12 +1,13 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Route.extend({
+export default Route.extend(AuthenticatedRouteMixin, {
   currentSession: service(),
 
   beforeModel() {
     if (!this.currentSession.canAccessLeidinggevenden)
       this.transitionTo('index');
   }
-  
+
 });
