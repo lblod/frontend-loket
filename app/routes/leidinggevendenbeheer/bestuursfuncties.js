1e86242207b850da35e65b813dd7e36254b88968
Mehran Naghizadeh <tarhineh@gmail.com>
2019-05-13 13:20:24 +0200

    Implement session related stuff in route

--- app/routes/leidinggevendenbeheer/bestuursfuncties.js
+++ app/routes/leidinggevendenbeheer/bestuursfuncties.js
@@ -1,4 +1,16 @@
 import Route from '@ember/routing/route';
+import DataTableRouteMixin from 'ember-data-table/mixins/route';
+import { inject as service } from '@ember/service';
 
-export default Route.extend({
+export default Route.extend(DataTableRouteMixin, {
+  currentSession: service(),
+  async beforeModel() {
+    const bestuurseenheid = await this.get('currentSession.group');
+    this.set('bestuurseenheid', bestuurseenheid);
+  },
+
+  setupController(controller, model) {
+    this._super(controller, model);
+    controller.set('bestuurseenheid', this.bestuurseenheid);
+  }
 });
