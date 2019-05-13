b59850400be78f1da1660406ee1b4d912d3c36b2
Mehran Naghizadeh <tarhineh@gmail.com>
2019-05-13 15:18:09 +0200

    Insert the person saerch form to the template

--- app/controllers/leidinggevendenbeheer/functionarissen/new/select-persoon.js
+++ app/controllers/leidinggevendenbeheer/functionarissen/new/select-persoon.js
@@ -4,6 +4,10 @@ export default Controller.extend({
   actions: {
     cancel(){
       this.transitionToRoute('leidinggevendenbeheer.functionarissen');
-    }
+    },
+    setPersoon(persoon){
+      this.model.functionaris.set('isBestuurlijkeAliasVan', persoon);
+      this.transitionToRoute('leidinggevendenbeheer.functionarissen.new.provide-details');
+    },
   }
 });
