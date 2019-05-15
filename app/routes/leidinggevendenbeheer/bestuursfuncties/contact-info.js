814c28db928e8054cdb491f0c89b7977ad22f0e3
Mehran Naghizadeh <tarhineh@gmail.com>
2019-05-15 14:05:13 +0200

    Implement contact-info route's model hook
    If the bestuursfuctie is not already associated with a cantact-punt object, we will want to create one and associate to it.

--- app/routes/leidinggevendenbeheer/bestuursfuncties/contact-info.js
+++ app/routes/leidinggevendenbeheer/bestuursfuncties/contact-info.js
@@ -1,4 +1,17 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  async model(params) {
+    const model = await this.store.findRecord('bestuursfunctie', params.id);
+
+    if (! await model.contactinfo) {
+      const info = await this.store.createRecord('contact-punt');
+      await info.save();
+      
+      model.set('contactinfo', info);
+      await model.save();
+    }
+
+    return model;
+  }
 });
