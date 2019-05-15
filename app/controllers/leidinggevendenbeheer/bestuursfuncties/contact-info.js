44a140570a1bd53e4df20cc5083b2c6a67a33218
Mehran Naghizadeh <tarhineh@gmail.com>
2019-05-15 14:01:21 +0200

    Make contact-info controller & use it in template

---
+++ app/controllers/leidinggevendenbeheer/bestuursfuncties/contact-info.js
@@ -0,0 +1,39 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+  //--- variables
+  dataIsGettingLost: false,
+  
+  //--- methods
+  exit() {
+    this.set('dataIsGettingLost', false);
+    this.transitionToRoute('leidinggevendenbeheer.functionarissen', this.model.id);
+  },
+  
+  //--- actions
+  actions: {
+    async annuleer() {
+      this.set('dataIsGettingLost', false);
+    },
+    async bewaar() {
+      await this.model.save();
+      this.exit();
+    },
+    cancel() {
+      if (! this.model.get('contactinfo.hasDirtyAttributes'))
+      this.exit();
+      else {
+        this.set('dataIsGettingLost', true);
+      }
+    },
+    async confirmChanges() {
+      await (await this.model.contactinfo).save();
+      await this.model.save();
+      this.exit();
+    },
+    async dismissChanges() {
+      (await this.model.contactinfo).rollbackAttributes();
+      this.exit();
+    }
+  }
+});
