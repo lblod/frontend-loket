fd7c2e8cfe75ae31a70663760d4c8985b257b318
Mehran Naghizadeh <tarhineh@gmail.com>
2019-05-15 13:44:04 +0200

    Use 'DataTableRouteMixin' to handle the data table

--- app/routes/leidinggevendenbeheer/functionarissen.js
+++ app/routes/leidinggevendenbeheer/functionarissen.js
@@ -1,24 +1,25 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import DataTableRouteMixin from 'ember-data-table/mixins/route';
 
-export default Route.extend({
+export default Route.extend(DataTableRouteMixin, {
   currentSession: service(),
+  modelName: 'functionaris',
+  bestuursFunctieId: null,
 
   async beforeModel() {
     const bestuurseenheid = await this.get('currentSession.group');
     this.set('bestuurseenheid', bestuurseenheid);
   },
 
-  async model(params){
-    const bestuursfunctie = await this.store.findRecord('bestuursfunctie', params.id);
-    return {
-      functionarissen: await this.store.query('functionaris', {'filter[bekleedt][id]': params.id}),
-      bestuursfunctie: bestuursfunctie
-    }
+  mergeQueryOptions(params) {
+    this.set('bestuursFunctieId', params.bestuursfunctieId);
+    return {'filter[bekleedt][id]': params.bestuursfunctieId};
   },
-
-  setupController(controller, model) {
+  
+  async setupController(controller, model) {
     this._super(controller, model);
     controller.set('bestuurseenheid', this.bestuurseenheid);
+    controller.set('bestuursfunctie', await this.store.findRecord('bestuursfunctie', this.bestuursFunctieId));
   }
 });
