8dc2912e2c133a64e74b5a4d40ae3695d8f513a5
Mehran Naghizadeh <tarhineh@gmail.com>
2019-05-15 13:46:15 +0200

    Update the model hook of 'functionarissen.new'
    Since the model has changed in 'leidinggevendenbeheer.functionarissen', we can no longer rely on it for the bestuursfunctie.

--- app/routes/leidinggevendenbeheer/functionarissen/new.js
+++ app/routes/leidinggevendenbeheer/functionarissen/new.js
@@ -1,16 +1,18 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  async model() {
-    const parentModel = await this.modelFor('leidinggevendenbeheer.functionarissen');
+  async model(params, transition) {
+    const bestuursfunctieId = this.paramsFor('leidinggevendenbeheer.functionarissen').bestuursfunctieId;
+
     const aangesteldStatus = (await this.store.query('functionaris-status-code', {
       filter: { ':uri:': 'http://data.vlaanderen.be/id/concept/functionarisStatusCode/45b4b155-d22a-4eaf-be3a-97022c6b7fcd' }})).firstObject;
     const waarnemendStatus = (await this.store.query('functionaris-status-code', {
       filter: { ':uri:': 'http://data.vlaanderen.be/id/concept/functionarisStatusCode/188fc9c0-dae7-43b2-b2b3-6122c1594479' }})).firstObject;
     const defaultStatus = aangesteldStatus;
+
     const functionaris = this.store.createRecord('functionaris', {
       status: defaultStatus,
-      bekleedt: parentModel.bestuursfunctie
+      bekleedt: await this.store.findRecord('bestuursfunctie', bestuursfunctieId)
     });
 
     return {
