b967e9fd0e9a65af2ec5616dc540b6cd0aabaf37
Mehran Naghizadeh <tarhineh@gmail.com>
2019-05-16 16:46:53 +0200

    Remove model handling logic from select-persoon

--- app/controllers/leidinggevendenbeheer/functionarissen/new/select-persoon.js
+++ app/controllers/leidinggevendenbeheer/functionarissen/new/select-persoon.js
@@ -6,8 +6,7 @@ export default Controller.extend({
       this.transitionToRoute('leidinggevendenbeheer.functionarissen');
     },
     setPersoon(persoon){
-      this.model.functionaris.set('isBestuurlijkeAliasVan', persoon);
-      this.transitionToRoute('leidinggevendenbeheer.functionarissen.new.provide-details');
+      this.transitionToRoute('leidinggevendenbeheer.functionarissen.new.provide-details', persoon.id);
     },
   }
 });
