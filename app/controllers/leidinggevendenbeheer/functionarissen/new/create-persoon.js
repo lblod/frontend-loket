09081ffb3654a740e83db28eca52dd8fbbc6baea
Mehran Naghizadeh <tarhineh@gmail.com>
2019-05-16 16:47:23 +0200

    Remove model handling logic from create-persoon

--- app/controllers/leidinggevendenbeheer/functionarissen/new/create-persoon.js
+++ app/controllers/leidinggevendenbeheer/functionarissen/new/create-persoon.js
@@ -3,8 +3,7 @@ import Controller from '@ember/controller';
 export default Controller.extend({
   actions: {
     onCreate(persoon) {
-      this.model.functionaris.set('isBestuurlijkeAliasVan', persoon);
-      this.transitionToRoute('leidinggevendenbeheer.functionarissen.new.provide-details');
+      this.transitionToRoute('leidinggevendenbeheer.functionarissen.new.provide-details', persoon.id);
     },
     onCancel() {
       this.transitionToRoute('leidinggevendenbeheer.functionarissen');
--- app/routes/leidinggevendenbeheer/functionarissen/new/create-persoon.js
+++ app/routes/leidinggevendenbeheer/functionarissen/new/create-persoon.js
@@ -1,7 +1,4 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  async model() {
-    return await this.modelFor('leidinggevendenbeheer.functionarissen.new');
-  }
 });
