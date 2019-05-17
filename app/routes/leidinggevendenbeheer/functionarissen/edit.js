433e4be1adbaf17153cdac04acb08e0aecc5a4d3
Mehran Naghizadeh <tarhineh@gmail.com>
2019-05-17 12:56:46 +0200

    Provide documentation for a property

--- app/routes/leidinggevendenbeheer/functionarissen/edit.js
+++ app/routes/leidinggevendenbeheer/functionarissen/edit.js
@@ -5,6 +5,10 @@ export default Route.extend({
     const functionaris = await this.store.findRecord('functionaris', params.functionarisId);
     return {
       functionaris: functionaris,
+      /**
+       * This value will be used to determine whether the status has been changed or not.
+       * This check is necessary when we are leaving the route.
+       */
       initialStatus: await functionaris.status
     }
   }
