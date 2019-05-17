e45d3e521a0f6492e25aabbabbe6e3ae712f9419
Mehran Naghizadeh <tarhineh@gmail.com>
2019-05-17 10:37:09 +0200

    Provide controller with a default status
    This will be used in the radio buttons' 'checked' property logic.

--- app/routes/leidinggevendenbeheer/functionarissen/new/provide-details.js
+++ app/routes/leidinggevendenbeheer/functionarissen/new/provide-details.js
@@ -13,10 +13,12 @@ export default Route.extend({
       filter: { ':uri:': 'http://data.vlaanderen.be/id/concept/functionarisStatusCode/188fc9c0-dae7-43b2-b2b3-6122c1594479' }})).firstObject;
     this.set('waarnemendStatus', waarnemendStatus);
 
+    this.set('defaultStatus', aangesteldStatus);
+
     const functionaris = await this.store.createRecord('functionaris', {
       bekleedt: await this.store.findRecord('bestuursfunctie', bestuursfunctieId),
       isBestuurlijkeAliasVan: await this.store.findRecord('persoon', params.persoonId),
-      status: aangesteldStatus,
+      status: this.defaultStatus,
     });
 
     return functionaris;
@@ -24,7 +26,11 @@ export default Route.extend({
 
   setupController(controller, model) {
     this._super(controller, model);
+    //--- Provide the controller with default status values
+    //--- These will be used for checks in radio buttons 
     controller.set('aangesteldStatus', this.aangesteldStatus);
     controller.set('waarnemendStatus', this.waarnemendStatus);
+    controller.set('defaultStatus', this.defaultStatus);
+
   }
 });
