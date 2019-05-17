28e94dc8f3b7ea80e4dc3126848e35aa701f1726
Mehran Naghizadeh <tarhineh@gmail.com>
2019-05-17 10:24:32 +0200

    Remove hasDirtyAttributes. It is checked in child.

--- app/controllers/leidinggevendenbeheer/functionarissen/new.js
+++ app/controllers/leidinggevendenbeheer/functionarissen/new.js
@@ -21,15 +21,6 @@ export default Controller.extend({
     this.set('userHasRequestedToClose', false);
     this.transitionToRoute('leidinggevendenbeheer.functionarissen');
   },
-  async hasDirtyAttributes(){
-    const f = this.model.functionaris;
-    const startHasChanged = f.start !== undefined;
-    const eindeHasChanged = f.einde !== undefined;
-    const statusHasChanged = (await f.status) !== this.model.defaultStatus;
-    const hasPersoon = (await f.isBestuurlijkeAliasVan) !== null;
-
-    return startHasChanged || eindeHasChanged || statusHasChanged || hasPersoon;
-  },
   
   //--- actions
   actions: {
