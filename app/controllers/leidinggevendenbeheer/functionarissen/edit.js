3b00177134b256baa55553c55a2133b5919be01a
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-05-22 17:47:54 +0200

    readability

--- app/controllers/leidinggevendenbeheer/functionarissen/edit.js
+++ app/controllers/leidinggevendenbeheer/functionarissen/edit.js
@@ -2,14 +2,14 @@ import Controller from '@ember/controller';
 import { computed }  from '@ember/object';
 
 export default Controller.extend({
-  //--- variables
+
   dataIsGettingLost: false,
 
   canConfirm: computed('model.functionaris.start', 'model.functionaris.einde', function(){
     return this.model.functionaris.start && this.model.functionaris.einde && this.model.functionaris.datesAreCompatible;
   }),
 
-  //--- methods
+
   exit() {
     this.set('dataIsGettingLost', false);
     this.transitionToRoute('leidinggevendenbeheer.functionarissen');
@@ -24,35 +24,40 @@ export default Controller.extend({
     filter: { ':uri:': 'http://data.vlaanderen.be/id/concept/functionarisStatusCode/188fc9c0-dae7-43b2-b2b3-6122c1594479' }})).firstObject);
   },
 
-  //--- actions
+
   actions: {
+
     annuleer(){
       this.set('dataIsGettingLost', false);
     },
+
     async bewaar(){
       await this.model.functionaris.save();
       this.set('dataIsGettingLost', false);
       this.transitionToRoute('leidinggevendenbeheer.functionarissen');
     },
+
     async cancel(){
       const f = this.model.functionaris;
       const statusHasChanged = (await f.status) !== this.model.initialStatus;
       if (!f.hasDirtyAttributes && !statusHasChanged) {
         this.exit();
       } else {
-        //--- This will reveal the modal confirmation dialog
         this.set('dataIsGettingLost', true);
       }
     },
+
     async confirmChanges() {
       await this.model.functionaris.save();
       this.transitionToRoute('leidinggevendenbeheer.functionarissen');
     },
+
     dismissChanges() {
       this.model.functionaris.rollbackAttributes();
       this.model.functionaris.set('status', this.model.initialStatus),
       this.exit();
     },
+
     setStatus(statusId){
       if (statusId == this.aangesteldStatus.id)
         this.model.functionaris.set('status', this.aangesteldStatus);
