ee965e0e632be73090b0b1cdf31d0c169849e5a0
Mehran Naghizadeh <tarhineh@gmail.com>
2019-06-19 10:08:58 +0200

    Set more clear action names
    Currently, we don't show a confirmation dialog. This needs to be discussed with the designer.

--- app/controllers/leidinggevendenbeheer/functionarissen/new/create-persoon.js
+++ app/controllers/leidinggevendenbeheer/functionarissen/new/create-persoon.js
@@ -5,13 +5,25 @@ export default Controller.extend({
     onCreate(persoon) {
       this.transitionToRoute('leidinggevendenbeheer.functionarissen.new.provide-details', persoon.id);
     },
-    onCancel() {
+    /**
+     * This is called when the 'Annuleer' button is clicked
+     * in the main UI
+     * or in the (to be) confirmation dialog
+     */
+    immediateCancel() {
       this.transitionToRoute('leidinggevendenbeheer.functionarissen');
     },
     gotoPreviousStep() {
       this.transitionToRoute('leidinggevendenbeheer.functionarissen.new.select-persoon');
     },
-    cancel() {
+    /**
+     * This is called when the x button is clicked
+     * on the top right side of the main UI
+     * 
+     * This is meant to be used for showing a confirmation dialog like the provide-details route.
+     * However, we currently don't show any. TODO: Discuss with the designer.
+     */
+    gentleCancel() {
       this.transitionToRoute('leidinggevendenbeheer.functionarissen');
     }
   }
