9841e1730e765a58f704d4964a17d8a9ff1ba837
Mehran Naghizadeh <tarhineh@gmail.com>
2019-06-20 15:55:20 +0200

    Prevent re-requesting the existing statuses
    Updating any bound attribute (like those incorporated in computed values) will trigger the didReceiveAttrs hook. We don't need request backend for the data we already have.

--- app/components/leidinggevendenbeheer/functionaris-form.js
+++ app/components/leidinggevendenbeheer/functionaris-form.js
@@ -6,17 +6,29 @@ export default Component.extend({
   store: service(),
   hasErrors: computed('model.start', function() { return ! this.functionaris.start }),
 
+  async fetchStatuses() {
+    if(!this.aangesteldStatus) {
+      const a = await this.store.query('functionaris-status-code', {
+          filter: { ':uri:': 'http://data.vlaanderen.be/id/concept/functionarisStatusCode/45b4b155-d22a-4eaf-be3a-97022c6b7fcd' }
+        });
+      this.set('aangesteldStatus', a.firstObject);
+    }
+  
+    if(!this.waarnemendStatus) {
+      const w = await this.store.query('functionaris-status-code', {
+        filter: { ':uri:': 'http://data.vlaanderen.be/id/concept/functionarisStatusCode/188fc9c0-dae7-43b2-b2b3-6122c1594479' }
+      });
+      this.set('waarnemendStatus', w.firstObject);
+    }
+  
+    if (!this.defaultStatus) {
+      this.set('defaultStatus', this.aangesteldStatus);
+    }
+  },
+  
   async didReceiveAttrs() {
-
-    const aangesteldStatus = (await this.store.query('functionaris-status-code', { filter: { ':uri:': 'http://data.vlaanderen.be/id/concept/functionarisStatusCode/45b4b155-d22a-4eaf-be3a-97022c6b7fcd' }})).firstObject;
-    this.set('aangesteldStatus', aangesteldStatus);
-
-    const waarnemendStatus = (await this.store.query('functionaris-status-code', { filter: { ':uri:': 'http://data.vlaanderen.be/id/concept/functionarisStatusCode/188fc9c0-dae7-43b2-b2b3-6122c1594479' }})).firstObject;
-    this.set('waarnemendStatus', waarnemendStatus);
-
-    this.set('defaultStatus', aangesteldStatus);
-
-    if (!await this.functionaris.status) {
+    this.fetchStatuses();
+    if (! await this.functionaris.status) {
       this.functionaris.set('status', this.defaultStatus);
     }
   },
