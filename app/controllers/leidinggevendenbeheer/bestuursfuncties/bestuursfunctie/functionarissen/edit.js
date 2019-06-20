7a19fdd3ed69dd9f543e8ba197b77b2485f82733
Mehran Naghizadeh <tarhineh@gmail.com>
2019-06-20 11:49:04 +0200

    Add some documentation

--- app/controllers/leidinggevendenbeheer/bestuursfuncties/bestuursfunctie/functionarissen/edit.js
+++ app/controllers/leidinggevendenbeheer/bestuursfuncties/bestuursfunctie/functionarissen/edit.js
@@ -1,7 +1,9 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-
+  /**
+   * This parameter toggles the close confirmation dialog
+   */
   dataIsGettingLost: false,
 
   exit() {
@@ -17,12 +19,20 @@ export default Controller.extend({
   },
 
   actions: {
+    /**
+     * This action is called for saving to the data store
+     * and navigating back to the list of functionarissen.
+     * It is triggered with either the 'Voeg aanstellingsperiode toe' button in the main UI
+     * or the 'Bewaar' button in the close confirmation dialog
+     */
     async applyModifications() {
       await this.model.functionaris.save();
       this.exit();
     },
-    
-
+    /**
+     * This action is called when the close button on the top right side 
+     * of the UI is clicked
+     */
     async gentleCancel(){
       const statusHasChanged = this.model.initialStatus != await this.model.functionaris.status;
       const someOtherDataHasChanged = this.model.functionaris.hasDirtyAttributes;
@@ -30,7 +40,10 @@ export default Controller.extend({
       if (! this.dataIsGettingLost)
         this.exit();
     },
-
+    /**
+     * This action is called when the 'Annuleer' button is clicked
+     * either in the main UI or in the close confirmation dialog
+     */
     immediateCancel() {
       this.model.functionaris.rollbackAttributes();
       this.model.functionaris.set('status', this.model.initialStatus),
