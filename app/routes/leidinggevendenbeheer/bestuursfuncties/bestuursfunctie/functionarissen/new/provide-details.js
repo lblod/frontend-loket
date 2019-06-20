024a9a0d8afff7ccfe91131760dbc800de027d4e
Mehran Naghizadeh <tarhineh@gmail.com>
2019-06-20 13:31:37 +0200

    Remove unnecessary await

--- app/routes/leidinggevendenbeheer/bestuursfuncties/bestuursfunctie/functionarissen/new/provide-details.js
+++ app/routes/leidinggevendenbeheer/bestuursfuncties/bestuursfunctie/functionarissen/new/provide-details.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   async model(params) {
-    return await this.store.createRecord('functionaris', {
+    return this.store.createRecord('functionaris', {
       bekleedt: this.modelFor('leidinggevendenbeheer.bestuursfuncties.bestuursfunctie'),
       isBestuurlijkeAliasVan: await this.store.findRecord('persoon', params.persoon_id)
     });
