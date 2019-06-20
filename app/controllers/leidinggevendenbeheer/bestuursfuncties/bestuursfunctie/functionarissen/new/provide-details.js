f460c767096d55ae20d7c40d11b3933bf8e14429
Mehran Naghizadeh <tarhineh@gmail.com>
2019-06-20 15:54:19 +0200

    Set isSaving to false by default

--- app/controllers/leidinggevendenbeheer/bestuursfuncties/bestuursfunctie/functionarissen/new/provide-details.js
+++ app/controllers/leidinggevendenbeheer/bestuursfuncties/bestuursfunctie/functionarissen/new/provide-details.js
@@ -5,7 +5,7 @@ export default Controller.extend({
   /**
    * This property disables the save and cancel buttons while saving is in progress
    */
-  isSaving: true,
+  isSaving: false,
   /**
    * This is set to true when the close button is clicked
    * on the top right side of the UI
