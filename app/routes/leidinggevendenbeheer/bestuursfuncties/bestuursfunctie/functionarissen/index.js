b7a0a59a91deff275cb1046e8696d018c00a7139
Mehran Naghizadeh <tarhineh@gmail.com>
2019-06-21 11:14:05 +0200

    Fix the value supplied as bestuursfunctie id
    'params' is not accessible here because the model hook can not access the query params of a parent route.

--- app/routes/leidinggevendenbeheer/bestuursfuncties/bestuursfunctie/functionarissen/index.js
+++ app/routes/leidinggevendenbeheer/bestuursfuncties/bestuursfunctie/functionarissen/index.js
@@ -4,15 +4,16 @@ import DataTableRouteMixin from 'ember-data-table/mixins/route';
 export default Route.extend(DataTableRouteMixin, {
   modelName: 'functionaris',
 
-  mergeQueryOptions(params) {
+  mergeQueryOptions() {
+    this.set('bestuursfunctie', this.modelFor('leidinggevendenbeheer.bestuursfuncties.bestuursfunctie'));
     return {
-      'filter[bekleedt][id]': params.bestuursfunctie_id, include:'is-bestuurlijke-alias-van'
+      'filter[bekleedt][id]': this.bestuursfunctie.id, include:'is-bestuurlijke-alias-van'
     };
   },
 
   setupController(controller, model) {
     this._super(controller, model);
     controller.set('bestuurseenheid', this.modelFor('leidinggevendenbeheer'));
-    controller.set('bestuursfunctie', this.modelFor('leidinggevendenbeheer.bestuursfuncties.bestuursfunctie'));
+    controller.set('bestuursfunctie', this.bestuursfunctie);
   }
 });
