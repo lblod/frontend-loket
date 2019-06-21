8c2d37f92d7ea5372330506809d566b5dd183f8b
Erika Pauwels <erika.pauwels@gmail.com>
2019-06-21 20:00:55 +0200

    Simplify functionaris form

--- app/components/leidinggevendenbeheer/functionaris-form.js
+++ app/components/leidinggevendenbeheer/functionaris-form.js
@@ -4,32 +4,14 @@ import { computed }  from '@ember/object';
 
 export default Component.extend({
   store: service(),
-  hasErrors: computed('model.start', function() { return ! this.functionaris.start }),
+  hasErrors: computed('model.start', function() { return ! this.functionaris.start; }),
 
-  async fetchStatuses() {
-    if(!this.aangesteldStatus) {
-      const a = await this.store.query('functionaris-status-code', {
-        filter: { ':uri:': 'http://data.vlaanderen.be/id/concept/functionarisStatusCode/45b4b155-d22a-4eaf-be3a-97022c6b7fcd' }
-      });
-      this.set('aangesteldStatus', a.firstObject);
-    }
-
-    if(!this.waarnemendStatus) {
-      const w = await this.store.query('functionaris-status-code', {
-        filter: { ':uri:': 'http://data.vlaanderen.be/id/concept/functionarisStatusCode/188fc9c0-dae7-43b2-b2b3-6122c1594479' }
-      });
-      this.set('waarnemendStatus', w.firstObject);
-    }
-
-    if (!this.defaultStatus) {
-      this.set('defaultStatus', this.aangesteldStatus);
-    }
-  },
-
-  async didReceiveAttrs() {
-    this.fetchStatuses();
-    if (! await this.functionaris.status) {
-      this.functionaris.set('status', this.defaultStatus);
-    }
+  async init() {
+    this._super(...arguments);
+    const statusOptions = await this.store.query('functionaris-status-code', {
+      sort: 'label',
+      page: { size: 100 }
+    });
+    this.set('statusOptions', statusOptions);
   }
 });
