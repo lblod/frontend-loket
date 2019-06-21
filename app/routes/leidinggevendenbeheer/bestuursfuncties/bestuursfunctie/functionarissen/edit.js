4da5d519cd18d94d06ca506e148869612cd81777
Mehran Naghizadeh <tarhineh@gmail.com>
2019-06-21 11:32:40 +0200

    Use RSVP.hash instead of a POJO.

--- app/routes/leidinggevendenbeheer/bestuursfuncties/bestuursfunctie/functionarissen/edit.js
+++ app/routes/leidinggevendenbeheer/bestuursfuncties/bestuursfunctie/functionarissen/edit.js
@@ -1,16 +1,17 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 
 export default Route.extend({
   async model(params){
     const functionaris = await this.store.findRecord('functionaris', params.functionaris_id);
-    return {
+    return RSVP.hash({
       functionaris: functionaris,
       /**
        * This value will be used to determine whether the status has been changed or not.
        * This check is necessary when we are leaving the route.
        */
       initialStatus: await functionaris.status
-    };
+    });
   },
 
   actions: {
