7acacbbb9698fcf95883a1c235445bbb2965ac34
Mehran Naghizadeh <tarhineh@gmail.com>
2019-06-21 15:38:53 +0200

    Get rid of two redundant properties

--- app/controllers/leidinggevendenbeheer/bestuursfuncties/bestuursfunctie/functionarissen/new/provide-details.js
+++ app/controllers/leidinggevendenbeheer/bestuursfuncties/bestuursfunctie/functionarissen/new/provide-details.js
@@ -3,33 +3,24 @@ import { computed }  from '@ember/object';
 
 export default Controller.extend({
   isSaving: false,
-  userHasRequestedToClose: false,
-  userHasRequestedToSave: false,
 
   hasUnsavedData: computed('model.start', function() { return this.model.start }),
   hasErrors: computed('model.start', function() { return ! this.model.start }),
-  showConfirmationDialog: computed('hasUnsavedData', 'userHasRequestedToClose', function(){
-    return this.hasUnsavedData && this.userHasRequestedToClose;
-  }),
-
-  shallDisplayErrors: computed('hasErrors', 'userHasRequestedToSave', function() {
-    return this.hasErrors && this.userHasRequestedToSave;
-  }),
 
   exit() {
-    this.set('userHasRequestedToClose', false);
-    this.set('userHasRequestedToSave', false);
-    this.set('isSaving', false);
+    this.set('showConfirmationDialog', false);
     this.transitionToRoute('leidinggevendenbeheer.bestuursfuncties.bestuursfunctie.functionarissen');
   },
-
+  
   actions: {
     async save() {
-      this.set('userHasRequestedToSave', true);
       if(! this.hasErrors) {
         this.set('isSaving', true);
         await this.model.save();
+        this.set('isSaving', false);
         this.transitionToRoute('leidinggevendenbeheer.bestuursfuncties.bestuursfunctie.functionarissen');
+      } else {
+        this.set('showConfirmationDialog', true);
       }
     },
 
@@ -37,7 +28,7 @@ export default Controller.extend({
       if (!this.isSaving && !this.hasUnsavedData) {
         this.exit();
       } else {
-        this.set('userHasRequestedToClose', true);
+        this.set('showConfirmationDialog', true);
       }
     },
 
