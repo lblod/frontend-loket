fac3033a76341ac08858598418df1f55c51e7965
Erika Pauwels <erika.pauwels@gmail.com>
2019-07-02 14:20:25 +0200

    Simplify toezicht inzending edit form

--- app/components/toezicht/inzending-edit.js
+++ app/components/toezicht/inzending-edit.js
@@ -2,8 +2,9 @@ import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 import { A } from '@ember/array';
 import { computed } from '@ember/object';
-import { gte } from '@ember/object/computed';
+import { alias } from '@ember/object/computed';
 import { task } from 'ember-concurrency';
+import { and, gte, not, or } from 'ember-awesome-macros';
 
 export default Component.extend({
   classNames: ['col--10-12 col--9-12--m col--12-12--s container-flex--contain'],
@@ -18,70 +19,23 @@ export default Component.extend({
   hasError: gte('errorMsg.length', 1),
   deleteModal: false,
 
-  flushErrors(){
-    this.set('errorMsg', '');
-  },
+  inzending: alias('model.inzendingVoorToezicht'),
+  isSent: alias('model.inzendingVoorToezicht.status.isVerstuurd'),
+  canSave: not('isSent'),
+  canDelete: and(not('model.isNew'), 'canSave'),
+  canSend: and('canSave', or('files.length', not('allEmptyFileAddresses'))),
+  isWorking: or('save.isRunning', 'delete.isRunning', 'send.isRunning'),
 
-  isSent: computed('model.inzendingVoorToezicht.status.id', function(){
-    return this.model.get('inzendingVoorToezicht.status.isVerstuurd');
-  }),
-
-  canSave: computed('isSent', function(){
-    return !this.isSent;
-  }),
-
-  canDelete: computed('model.isNew', 'model.inzendingVoorToezicht.status.id', function(){
-    return !this.get('model.isNew') && !this.model.get('inzendingVoorToezicht.status.isVerstuurd');
-  }),
-
-  canSend: computed('model.inzendingVoorToezicht.status.id', 'files.[]', 'allEmptyFileAddresses', function(){
-    return !this.isSent && (this.get('files.length') || !this.allEmptyFileAddresses);
-  }),
-
-  isWorking: computed('save.isRunning','delete.isRunning','send.isRunning', function(){
-    return this.save.isRunning || this.delete.isRunning || this.send.isRunning || false;
-  }),
-
-  allEmptyFileAddresses: computed('fileAddresses', 'fileAddresses.{[],@each.address}', function(){
+  allEmptyFileAddresses: computed('fileAddresses', 'fileAddresses.{[],@each.address}', function() {
     return !(this.fileAddresses || []).any(a => a.address && a.address.length > 0);
   }),
 
-  /**
-   * url-box is used to either show existing urls or enter new ones
-   * It is needed in two cases
-   *  1. The inzending is not yet finalized/sent: !isSent
-   *  2. The inzending has been sent and it contains at least one url: fileAddress.length > 0
-   */
   needsUrlBox: computed('isSent', 'fileAddresses.length', function() {
-    const notSent = !this.isSent;
-    const hasFileAddresses = this.get('fileAddresses.length') > 0;
-    return notSent || hasFileAddresses;
+    return !this.isSent || this.get('fileAddresses.length') > 0;
   }),
 
-  async validate(){
-    let errors = [];
-    let states = await this.get('dynamicForm.formNode.unionStates');
-    if(states.filter((s) => { return s == 'noSend'; }).length > 0)
-      errors.push('Gelieve alle verplichte velden in te vullen.');
-
-    if((await this.files).length == 0 && this.allEmptyFileAddresses)
-      errors.push('Gelieve minstens één bestand of link naar document op te laden.');
-
-    this.set('errorMsg', errors.join(' '));
-  },
-
-  async updateInzending(){
-    let inzending = await this.model.get('inzendingVoorToezicht');
-    inzending.set('modified', new Date());
-    (await inzending.get('files')).setObjects(this.files);
-
-    // TODO add stricter validation on URLs
-    this.fileAddresses.forEach(a => a.set('address', a.address && a.address.trim()));
-    await Promise.all(this.fileAddresses.map(a => a.save()));
-    (await inzending.get('fileAddresses')).setObjects(this.fileAddresses);
-
-    inzending.set('lastModifier', await this.currentSession.get('user'));
-    return inzending.save();
+  flushErrors() {
+    this.set('errorMsg', '');
   },
 
   init() {
@@ -90,114 +44,129 @@ export default Component.extend({
     this.set('fileAddresses', A([]));
   },
 
-  async didReceiveAttrs(){
+  async didReceiveAttrs() {
     try {
       this._super(...arguments);
-      let inzending = await this.model.get('inzendingVoorToezicht');
-      this.set('inzending', inzending);
-      let files = await inzending.get('files');
-      if(files)
+      const files = await this.inzending.get('files');
+      if (files)
         this.files.setObjects(files.toArray());
-      let fileAddresses = await inzending.get('fileAddresses');
-      if(fileAddresses)
+      const fileAddresses = await this.inzending.get('fileAddresses');
+      if (fileAddresses)
         this.fileAddresses.setObjects(fileAddresses.toArray());
-    }
-    catch(e){
+    } catch (e) {
       this.set('errorMsg', `Fout bij het inladen: ${e.message}. Gelieve opnieuw te proberen.`);
     }
   },
 
-  save: task(function* (){
+  updateInzendingAttachments: task(function*() {
+    const inzending = yield this.inzending;
+    inzending.set('modified', new Date());
+    (yield inzending.get('files')).setObjects(this.files);
+
+    // TODO add stricter validation on URLs
+    this.fileAddresses.forEach(a => a.set('address', a.address && a.address.trim()));
+    yield Promise.all(this.fileAddresses.map(a => a.save()));
+    (yield inzending.get('fileAddresses')).setObjects(this.fileAddresses);
+
+    const lastModifier = yield this.currentSession.get('user');
+    inzending.set('lastModifier', lastModifier);
+    return inzending.save();
+  }),
+
+  validate: task(function*() {
+    this.flushErrors();
+    const errors = [];
+    const states = yield this.get('dynamicForm.formNode.unionStates');
+    if (states.filter((s) => s == 'noSend').length)
+      errors.push('Gelieve alle verplichte velden in te vullen.');
+
+    if ((yield this.files).length == 0 && this.allEmptyFileAddresses)
+      errors.push('Gelieve minstens één bestand of link naar document op te laden.');
+
+    this.set('errorMsg', errors.join(' '));
+  }),
+
+  save: task(function*() {
+    this.flushErrors();
     try {
       yield this.dynamicForm.save();
-      yield this.updateInzending();
-    }
-    catch(e){
+      yield this.updateInzendingAttachments.perform();
+    } catch (e) {
       this.set('errorMsg', `Fout bij het opslaan: ${e.message}`);
     }
   }).drop(),
 
-  send: task(function* (){
+  sendInzending: task(function*() {
     try {
       yield this.save.perform();
       const statusSent = (yield this.store.query('document-status', {
-          filter: { ':uri:': 'http://data.lblod.info/document-statuses/verstuurd' }
+        filter: {
+          ':uri:': 'http://data.lblod.info/document-statuses/verstuurd'
+        }
       })).firstObject;
-      const inzending = yield this.model.get('inzendingVoorToezicht');
+      const inzending = yield this.inzending;
       inzending.set('status', statusSent);
       inzending.set('sentDate', new Date());
       yield inzending.save();
-    }
-    catch(e){
+    } catch (e) {
       this.set('errorMsg', `Fout bij het verzenden: ${e.message}`);
     }
   }).drop(),
 
-  delete: task(function* (){
+  delete: task(function*() {
     try {
-      let files = yield this.model.get('inzendingVoorToezicht.files');
-      yield (yield this.model.get('inzendingVoorToezicht')).destroyRecord();
+      const files = yield this.model.get('inzendingVoorToezicht.files');
+      yield(yield this.model.get('inzendingVoorToezicht')).destroyRecord();
       yield Promise.all(files.map(f => f.destroyRecord()));
       yield this.model.destroyRecord();
-    }
-    catch(e){
+    } catch (e) {
       this.set('errorMsg', `Fout bij het verwijderen: ${e.message}`);
     }
   }).drop(),
 
   actions: {
-    async setFormVersion(formVersion){
+    async setFormVersion(formVersion) {
       const formNode = await formVersion.get('formNode');
       this.set('model.formNode', formNode);
       this.formVersionTracker.updateFormVersion(formVersion);
     },
 
-    initDynamicForm(dForm){
-      this.set('dynamicForm', dForm);
-    },
-    close(){
-      this.router.transitionTo('toezicht.inzendingen.index');
-    },
-    async save(){
-      this.flushErrors();
+    async create() {
       await this.save.perform();
-    },
-    async create(){
-      this.flushErrors();
-      await this.save.perform();
-      if(this.hasError) return;
+      if (this.hasError) return;
       this.router.transitionTo('toezicht.inzendingen.edit', this.model.get('inzendingVoorToezicht.id'));
     },
-    async send(){
-      this.flushErrors();
-      await this.validate();
-      if(this.hasError) return;
-      await this.send.perform();
-      if(this.hasError) return;
+
+    async send() {
+      await this.validate.perform();
+      if (this.hasError) return;
+      await this.sendInzending.perform();
+      if (this.hasError) return;
       this.router.transitionTo('toezicht.inzendingen.index');
     },
-    deleteInzending(){
+
+    async confirmDelete() {
       this.flushErrors();
-      this.set('deleteModal', true);
-    },
-    async confirmDelete(){
       this.set('deleteModal', false);
       await this.delete.perform();
-      if(this.hasError) return;
+      if (this.hasError) return;
       this.router.transitionTo('toezicht.inzendingen.index');
     },
-    cancelDelete(){
-      this.set('deleteModal', false);
-    },
+
     addFile(file) {
       this.files.pushObject(file);
     },
+
     deleteFile(file) {
       this.files.removeObject(file);
     },
 
-    deleteFileAddress(fileAddress){
+    deleteFileAddress(fileAddress) {
       this.fileAddresses.removeObject(fileAddress);
+    },
+
+    close() {
+      this.router.transitionTo('toezicht.inzendingen.index');
     }
   }
 });
