717fe0eb377380a0e18fbca849b8cd7411579d5a
Claire Lovisa <lovisa.claire@gmail.com>
2019-07-11 11:39:05 +0200

    [WIP] refactor address searching to render results faster

--- app/components/adressenregister-selector.js
+++ app/components/adressenregister-selector.js
@@ -2,64 +2,34 @@ import Component from '@ember/component';
 import { task, timeout } from 'ember-concurrency';
 import fetch from 'fetch';
 
-const emptyAdresRegister = {
-  land: null, //seriously no land?
-  gemeente: null,
-  postcode: null,
-  adres: null,
-  adresRegisterId: null,
-  adresRegisterUri: null
-}
-
 export default Component.extend({
+  addressUncomplete: false,
 
   async didReceiveAttrs(){
-    if(await this.adres) {
-      this.set('_address', { volledigAdres : { geografischeNaam : { spelling: this.adres.get('volledigAdres') } } });
+    if(this.adres.get('volledigAdres')) {
+      this.set('_address', { FormattedAddress : await this.adres.get('volledigAdres') });
     }
   },
 
   search: task(function* (searchData) {
     yield timeout(400);
-    let result =  yield fetch(`/adressenregister/search?query=${searchData}`);
+    const result =  yield fetch(`/adressenregister/search?query=${searchData}`);
     if (result.ok){
-      let addresses = yield result.json();
+      const addresses = yield result.json();
       return addresses['adressen'];
     }
     return [];
   }).keepLatest(),
 
-  getDetailForOverviewItem: task(function* (overviewAddress){
-    //the api returns only a 'summary', we need to fetch details.
-    this.set('_address', null);
-
-    if (overviewAddress) {
-      let result =  yield fetch(`/adressenregister/detail?uri=${overviewAddress.detail}`);
-      if (result.ok){
-        let adresRegister = yield result.json();
-        this.set('_address', adresRegister);
-        this.onSelect(this.extractRelevantInfo(adresRegister));
-      }
-    } else {
-      this.onSelect(emptyAdresRegister);
-    }
-  }),
-
-  extractRelevantInfo(adresRegister){
-    return {
-      land: null, //seriously no land?
-      // And also adding house number etc etc
-      gemeente: adresRegister['gemeente']['gemeentenaam']['geografischeNaam']['spelling'],
-      postcode: adresRegister['postinfo']['objectId'],
-      volledigAdres: adresRegister['volledigAdres']['geografischeNaam']['spelling'],
-      adresRegisterId: adresRegister['identificator']['objectId'],
-      adresRegisterUri: adresRegister['identificator']['id']
-    };
-  },
-
   actions: {
-    async select(overviewAddress){
-      await this.getDetailForOverviewItem.perform(overviewAddress);
+    async select(selectedAddress){
+      if (!selectedAddress.Housenumber) {
+        this.set('addressUncomplete', true);
+      } else {
+        this.set('addressUncomplete', false);
+        this.onSelect(selectedAddress);
+        this.set('_address', selectedAddress);
+      }
     }
   }
 });
--- app/controllers/leidinggevendenbeheer/bestuursfuncties/bestuursfunctie/contact-info.js
+++ app/controllers/leidinggevendenbeheer/bestuursfuncties/bestuursfunctie/contact-info.js
@@ -1,6 +1,15 @@
 import Controller from '@ember/controller';
 import { computed }  from '@ember/object';
 
+const emptyAdresRegister = {
+  land: null,
+  gemeente: null,
+  postcode: null,
+  adres: null,
+  adresRegisterId: null,
+  adresRegisterUri: null
+}
+
 export default Controller.extend({
   newAddressData: null,
   showConfirmationDialog: false,
@@ -8,6 +17,17 @@ export default Controller.extend({
   isDirty: computed('model.hasDirtyAttributes', 'newAddress', function() {
     return this.model.hasDirtyAttributes || this.newAddressData;
   }),
+
+  extractRelevantInfo(adresRegister) {
+    return {
+      land: null, //seriously no land?
+      gemeente: adresRegister['gemeente']['gemeentenaam']['geografischeNaam']['spelling'],
+      postcode: adresRegister['postinfo']['objectId'],
+      volledigAdres: adresRegister['volledigAdres']['geografischeNaam']['spelling'],
+      adresRegisterId: adresRegister['identificator']['objectId'],
+      adresRegisterUri: adresRegister['identificator']['id']
+    };
+  },
 
   exit() {
     this.set('showConfirmationDialog', false);
@@ -17,19 +37,42 @@ export default Controller.extend({
 
   actions: {
     async save() {
-      if (this.newAddressData){
-        var adres = await this.model.adres;
-        adres.setProperties(this.newAddressData);
-      }
-      await adres.save();
-      this.model.set('adres', adres);
-      await this.model.save();
+      // Make is a task ?
 
-      this.exit();
+      if (this.newAddressData) {
+        // Locate precisely the new address
+        let adresRegister = null;
+        const matchResult = await fetch(`/adressenregister/match?municipality=${this.newAddressData.Municipality}&zipcode=${this.newAddressData.Zipcode}&thoroughfarename=${this.newAddressData.Thoroughfarename}&housenumber=${this.newAddressData.Housenumber}`);
+        if (matchResult.ok) {
+          const matchedAddress = await matchResult.json();
+          if (matchedAddress) {
+            const detailResult = await fetch(`/adressenregister/detail?uri=${matchedAddress.detail}`);
+            if (detailResult.ok) {
+              adresRegister = await detailResult.json();
+              adresRegister = this.extractRelevantInfo(adresRegister);
+            }
+          }
+        }
+
+        // Save the address
+        let adres = await this.model.adres;
+        if (adresRegister) {
+          adres.setProperties(adresRegister);
+        } else {
+          adres.setProperties(emptyAdresRegister);
+        }
+        await adres.save();
+
+        this.model.set('adres', adres);
+        await this.model.save();
+
+        this.exit();
+      }
+
     },
 
     cancel(){
-      if (! this.isDirty)
+      if (!this.isDirty)
         this.exit();
       else
         this.set('showConfirmationDialog', true);
