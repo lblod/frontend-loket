8fe8260c9849becc0c99a62fc327e9d8fd87cca7
Claire Lovisa <lovisa.claire@gmail.com>
2019-07-12 16:47:12 +0200

    Filter functionarisStatusCode when bestuursfunctie is leiden ambtenaar

--- app/components/leidinggevendenbeheer/functionaris-form.js
+++ app/components/leidinggevendenbeheer/functionaris-form.js
@@ -11,10 +11,23 @@ export default Component.extend({
 
   async init() {
     this._super(...arguments);
-    const statusOptions = await this.store.query('functionaris-status-code', {
-      sort: 'label',
-      page: { size: 100 }
-    });
+    const bestuursfunctie = await this.model.bekleedt;
+    const bestuursfunctieCode = await bestuursfunctie.rol;
+    const bestuursfunctieCodeUri = await bestuursfunctieCode.uri;
+
+    let queryParams = {};
+    if (bestuursfunctieCodeUri == 'http://data.vlaanderen.be/id/concept/BestuursfunctieCode/855489b9-b584-4f34-90b2-39aea808cd9f') { // Leidend ambtenaar
+      queryParams =  {
+        filter: { ':uri:': 'http://data.vlaanderen.be/id/concept/functionarisStatusCode/45b4b155-d22a-4eaf-be3a-97022c6b7fcd' } // aangesteld
+      };
+    } else {
+      queryParams =  {
+        sort: 'label',
+        page: { size: 100 }
+      };
+    }
+
+    const statusOptions = await this.store.query('functionaris-status-code', queryParams);
     this.set('statusOptions', statusOptions);
   }
 });
