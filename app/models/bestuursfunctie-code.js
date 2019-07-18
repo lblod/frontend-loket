177a992cb767ccdfadf9b75bdd6260e39a9f5bef
Claire Lovisa <lovisa.claire@gmail.com>
2019-07-18 17:00:00 +0200

    Use built-in computed property

--- app/models/bestuursfunctie-code.js
+++ app/models/bestuursfunctie-code.js
@@ -1,18 +1,11 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
-import { computed } from '@ember/object';
+import { equal } from '@ember/object/computed';
 
 export default Model.extend({
   uri: attr(),
   label: attr(),
-
-  isLeidinggevendAmbtenaar: computed('uri', function() {
-    if (this.uri == "http://data.vlaanderen.be/id/concept/BestuursfunctieCode/855489b9-b584-4f34-90b2-39aea808cd9f") { // Leidend ambtenaar
-      return true;
-    } else {
-      return false;
-    }
-  }),
+  isLeidinggevendAmbtenaar: equal('uri', 'http://data.vlaanderen.be/id/concept/BestuursfunctieCode/855489b9-b584-4f34-90b2-39aea808cd9f'),
 
   rdfaBindings: { // eslint-disable-line ember/avoid-leaking-state-in-ember-objects
     class: "http://www.w3.org/2004/02/skos/core#Concept",
