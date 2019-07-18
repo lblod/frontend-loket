57561450d45ed2fa47a7247b5185f0aa24d64a34
Erika Pauwels <erika.pauwels@gmail.com>
2019-07-18 13:39:07 +0200

    Simplify action to create a new inzending

--- app/controllers/toezicht/inzendingen/index.js
+++ app/controllers/toezicht/inzendingen/index.js
@@ -1,19 +1,13 @@
 import Controller from '@ember/controller';
-import { inject as service } from '@ember/service';
-import { computed } from '@ember/object';
 
 export default Controller.extend({
-  router: service(),
   page: 0,
   size: 20,
   sort: 'status.label,-sent-date',
 
   actions: {
-    handleAddClick() {
-      if (this.get('router.currentRouteName') === 'toezicht.inzendingen.new')
-        this.transitionToRoute('toezicht.inzendingen.index');
-      else
-        this.transitionToRoute('toezicht.inzendingen.new');
+    goToNewInzending() {
+      this.transitionToRoute('toezicht.inzendingen.new');
     }
   }
 });
