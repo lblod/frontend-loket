98f864f953b191db764dc8112f103f2ea4d8045a
Claire Lovisa <lovisa.claire@gmail.com>
2019-08-06 17:10:20 +0200

    Add a remove button on functionarissen

--- app/components/leidinggevendenbeheer/functionaris-table.js
+++ app/components/leidinggevendenbeheer/functionaris-table.js
@@ -1,4 +1,11 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  actions: {
+    async removeFunctionaris(functionaris) {
+      await functionaris.destroyRecord();
+      const count = this.content.meta.count - 1;
+      this.content.set('meta.count', count);
+    }
+  }
 });
