b64c9b31279ca00a1db578181750f49020c3504a
Erika Pauwels <erika.pauwels@gmail.com>
2020-04-15 12:54:22 +0200

    Fix skin of status pill in submissions data table

--- app/models/submission-document-status.js
+++ app/models/submission-document-status.js
@@ -3,8 +3,11 @@ import Model, {attr} from '@ember-data/model';
 export default class SubmissionDocumentStatus extends Model {
   @attr uri;
   @attr('string') label;
-}
 
+  get isSent() {
+    return this.uri == SENT_STATUS;
+  }
+}
 
 const CONCEPT_STATUS = 'http://lblod.data.gift/concepts/79a52da4-f491-4e2f-9374-89a13cde8ecd';
 const SENT_STATUS = 'http://lblod.data.gift/concepts/9bd8d86d-bb10-4456-a84e-91e9507c374c';
