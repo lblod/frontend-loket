3225dbca6f3da9c1131265c2c55b510b02fa1a04
Erika Pauwels <erika.pauwels@gmail.com>
2020-04-15 12:20:41 +0200

    Use new appuniversum styling instead of old webuniversum styling

--- app/controllers/supervision/submissions/index.js
+++ app/controllers/supervision/submissions/index.js
@@ -1,14 +1,7 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
 
 export default class SupervisionSubmissionsIndexController extends Controller {
   page = 0
   size = 20
   sort = 'status.label,-sent-date,-modified'
-
-
-  @action
-  goToNewSubmission() {
-    this.transitionToRoute('supervision.submissions.new');
-  }
 }
