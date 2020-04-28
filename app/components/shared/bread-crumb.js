3656fa398ffb081299bdd95db99b93a306d38dbe
Dries Hugaerts <dries.hugaerts@live.be>
2020-04-28 18:06:49 +0200

    improved bread-crumb component.

--- app/components/shared/bread-crumb.js
+++ app/components/shared/bread-crumb.js
@@ -1,33 +1,97 @@
 import Component from '@glimmer/component';
-import { equal } from '@ember/object/computed';
-import { inject as service } from '@ember/service';
+import {equal} from '@ember/object/computed';
+import {inject as service} from '@ember/service';
 
 export default class SharedBreadCrumbComponent extends Component {
-@service router;
-  // @TODO use isActive for less maintenance
-  @equal('router.currentRouteName', 'toezicht.inzendingen.index') isToezicht;
-  @equal('router.currentRouteName', 'toezicht.inzendingen.edit') isToezichtDossier;
-  @equal('router.currentRouteName', 'toezicht.inzendingen.new') isToezichtDossierNew;
 
-  @equal('router.currentRouteName', 'berichtencentrum.berichten.index') isBerichten;
-  @equal('router.currentRouteName', 'berichtencentrum.berichten.conversatie.index') isBerichtenDossier;
+  @service router;
 
-  @equal('router.currentRouteName', 'bbcdr.rapporten.index') isBbcdr;
-  @equal('router.currentRouteName', 'bbcdr.rapporten.edit') isBbcdrDossier;
-  @equal('router.currentRouteName', 'bbcdr.rapporten.new') isBbcdrDossierNew;
+  bread = [
+    {
+      route: 'toezicht.inzendingen.index',
+      crumbs: [{label: 'Toezicht'}]
+    },
+    {
+      route: 'toezicht.inzendingen.edit',
+      crumbs: [{label: 'Toezicht',link: 'toezicht.inzendingen'},{label: 'Bekijk melding'}]
+    },
+    {
+      route: 'toezicht.inzendingen.new',
+      crumbs: [{label: 'Toezicht',link: 'toezicht.inzendingen'},{label: 'Nieuwe melding'}]
+    },
+    {
+      route: 'berichtencentrum.berichten.index',
+      crumbs: [{label: 'Berichtencentrum'}]
+    },
+    {
+      route: 'berichtencentrum.berichten.conversatie.index',
+      crumbs: [{label: 'Berichtencentrum',link: 'toezicht.inzendingen'},{label: 'Dossier'}]
+    },
+    {
+      route: 'bbcdr.rapporten.index',
+      crumbs: [{label: 'BBCDR Beleids- en Beheercyclus'}]
+    },
+    {
+      route: 'bbcdr.rapporten.edit',
+      crumbs: [{label: 'BBCDR Beleids- en Beheercyclus',link: 'bbcdr.rapporten'},{label: 'Rapport'}]
+    },
+    {
+      route: 'bbcdr.rapporten.new',
+      crumbs: [{label: 'BBCDR Beleids- en Beheercyclus',link: 'bbcdr.rapporten'},{label: 'Nieuw rapport'}]
+    },
+    {
+      route: 'mandatenbeheer.mandatarissen.index',
+      crumbs: [{label: 'Mandatenbeheer'}]
+    },
+    {
+      route: 'mandatenbeheer.mandatarissen.edit',
+      crumbs: [{label: 'Mandatenbeheer',link: 'mandatenbeheer.mandatarissen'},{label: 'Bewerk'}]
+    },
+    {
+      route: 'mandatenbeheer.mandatarissen.new',
+      crumbs: [{label: 'Mandatenbeheer',link: 'mandatenbeheer.mandatarissen'},{label: 'Voeg mandaat toe'}]
+    },
+    {
+      route: 'mandatenbeheer.mandatarissen.new-person',
+      crumbs: [{label: 'Mandatenbeheer',link: 'mandatenbeheer.mandatarissen'},{label: 'Voeg nieuwe persoon toe'}]
+    },
+    {
+      route: 'mandatenbeheer.fracties.index',
+      crumbs: [{label: 'Mandatenbeheer',link: 'mandatenbeheer.mandatarissen'},{label: 'Beheer fracties'}]
+    },
+    {
+      route: 'leidinggevendenbeheer.bestuursfuncties.index',
+      crumbs: [{label: 'Leidinggevendenbeheer'}]
+    },
+    {
+      route: 'leidinggevendenbeheer.bestuursfuncties.bestuursfunctie.functionarissen.index',
+      crumbs: [{label: 'Leidinggevendenbeheer',link: 'leidinggevendenbeheer.bestuursfuncties'},{label: 'Functionaris'}]
+    },
+    {
+      route: 'leidinggevendenbeheer.bestuursfuncties.bestuursfunctie.functionarissen.edit',
+      crumbs: [{label: 'Leidinggevendenbeheer',link: 'leidinggevendenbeheer.bestuursfuncties'},{label: 'Bewerk functionaris'}]
+    },
+    {
+      route: 'leidinggevendenbeheer.bestuursfuncties.bestuursfunctie.functionarissen.new.index',
+      crumbs: [{label: 'Leidinggevendenbeheer',link: 'leidinggevendenbeheer.bestuursfuncties'},{label: 'Nieuwe aanstellingsperiode'}]
+    },
+    {
+      route: 'leidinggevendenbeheer.bestuursfuncties.bestuursfunctie.contact-info',
+      crumbs: [{label: 'Leidinggevendenbeheer',link: 'leidinggevendenbeheer.bestuursfuncties'},{label: 'Bewerk contactgegevens'}]
+    },
+    {
+      route: 'personeelsbeheer.personeelsaantallen.index',
+      crumbs: [{label: 'Personeelsbeheer'}]
+    },
+    {
+      route: 'personeelsbeheer.personeelsaantallen.periodes.edit',
+      crumbs: [{label: 'Leidinggevendenbeheer',link: 'personeelsbeheer.personeelsaantallen.index'},{label: 'Bewerk aantallen'}]
+    },
+  ]
 
-  @equal('router.currentRouteName', 'mandatenbeheer.mandatarissen.index') isMandaten;
-  @equal('router.currentRouteName', 'mandatenbeheer.mandatarissen.edit') isMandaatBewerk;
-  @equal('router.currentRouteName', 'mandatenbeheer.mandatarissen.new') isMandaatNieuw;
-  @equal('router.currentRouteName', 'mandatenbeheer.mandatarissen.new-person') isNieuwePersoon;
-  @equal('router.currentRouteName', 'mandatenbeheer.fracties.index') isFracties;
-
-  @equal('router.currentRouteName', 'leidinggevendenbeheer.bestuursfuncties.index') isLeidinggevenden;
-  @equal('router.currentRouteName', 'leidinggevendenbeheer.bestuursfuncties.bestuursfunctie.functionarissen.index') isFunctionaris;
-  @equal('router.currentRouteName', 'leidinggevendenbeheer.bestuursfuncties.bestuursfunctie.functionarissen.edit') isFunctionarisBewerk;
-  @equal('router.currentRouteName', 'leidinggevendenbeheer.bestuursfuncties.bestuursfunctie.functionarissen.new.index') isFunctionarisNieuweAanstellingsperiode;
-  @equal('router.currentRouteName', 'leidinggevendenbeheer.bestuursfuncties.bestuursfunctie.contact-info') isFunctionarisContact;
-
-  @equal('router.currentRouteName', 'personeelsbeheer.personeelsaantallen.index') isPersoneel;
-  @equal('router.currentRouteName', 'personeelsbeheer.personeelsaantallen.periodes.edit') isPersoneelAantallen;
+  get crumbsForRoute() {
+    const results = this.bread.filter(value => value.route === this.router.currentRouteName );
+    if(results.length <= 0) return [];
+    return results[0].crumbs;
+  }
 }
