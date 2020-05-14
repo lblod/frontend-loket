756baf84b6dd1aac17c7ccf8b0d7f7a9e2e6a27b
Erika Pauwels <erika.pauwels@gmail.com>
2020-05-14 20:17:17 +0200

    Include some data-points by default in submission list view

--- app/routes/supervision/submissions/index.js
+++ app/routes/supervision/submissions/index.js
@@ -6,6 +6,12 @@ export default class SupervisionSubmissionsIndexRoute extends Route.extend(Authe
   modelName = 'submission'
   mergeQueryOptions() {
     return {
+      include: [
+        'status',
+        'form-data.passed-by',
+        'form-data.decision-type',
+        'form-data.regulation-type'
+      ].join(','),
       'filter[status][id]': [
         '79a52da4-f491-4e2f-9374-89a13cde8ecd',  // Concept status
         '9bd8d86d-bb10-4456-a84e-91e9507c374c'   // Sent status
