3ed059db8c55e48f668ec6562cd8e7e1dc592824
Dieter Peirs <github@dieterpeirs.com>
2020-06-08 14:50:57 +0200

    update appuniversum, remove vo-webuniversum footer

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -4,6 +4,5 @@ import ENV from 'frontend-loket/config/environment';
 export default Controller.extend({
   init() {
     this._super(...arguments);
-    this.set('footer', ENV['vo-webuniversum']['footer']);
   }
 });
