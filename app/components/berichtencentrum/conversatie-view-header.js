abab3908bf2ada1d973acfbc6ba9199b7ca5d7c8
Dieter Peirs <github@dieterpeirs.com>
2020-06-10 16:08:16 +0200

    refactor berichtencentrum sidebar

--- app/components/berichtencentrum/conversatie-view-header.js
+++ app/components/berichtencentrum/conversatie-view-header.js
@@ -3,7 +3,6 @@ import { inject as service } from '@ember/service';
 
 export default Component.extend({
   router: service(),
-  classNames:['u-padding--trl--small u-border--light--bottom'],
   actions: {
     close() {
       this.close();
