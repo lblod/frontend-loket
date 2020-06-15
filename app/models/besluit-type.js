9d07b2e5683e4484e98e5d49b65e857ad058e5ef
Dries Hugaerts <dries.hugaerts@live.be>
2020-06-15 11:32:30 +0200

    removed 'toezicht' models + removed old form initializer.

--- app/models/besluit-type.js
+++
@@ -1,8 +0,0 @@
-import Model from 'ember-data/model';
-import attr from 'ember-data/attr';
-import { hasMany } from 'ember-data/relationships';
-
-export default Model.extend({
-  label: attr(),
-  decidableBy: hasMany('bestuurseenheid-classificatie-code', { inverse: null })
-});
--- app/models/tax-rate.js
+++
@@ -1,8 +0,0 @@
-import Model, {attr} from '@ember-data/model';
-
-export default class TaxRate extends Model {
-  @attr('number') amount;
-  @attr('string') unit;
-  @attr('string') base;
-  @attr('string') remark;
-}
