3c0a7a485c9057063f23c3632d3eca3960e1f62e
Oscar Rodriguez <oscar@verify.as>
2020-07-23 09:58:24 +0200

    moved message to the index route

--- app/routes/leidinggevendenbeheer/bestuursfuncties/bestuursfunctie/contact-info.js
+++ app/routes/leidinggevendenbeheer/bestuursfuncties/bestuursfunctie/contact-info.js
@@ -1,14 +1,9 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
 
 export default Route.extend({
-  currentSession: service('current-session'),
   async model() {
     const bestuursfunctie = this.modelFor('leidinggevendenbeheer.bestuursfuncties.bestuursfunctie');
     this.set('bestuursfunctie', bestuursfunctie);
-    const bestuurseenheidClassificatie = await (await this.currentSession.group).classificatie;
-    this.set('bestuurseenheidClassificatie', bestuurseenheidClassificatie.label);
-
     if (!await bestuursfunctie.contactinfo) {
       const info = await this.store.createRecord('contact-punt');
       await info.save();
@@ -31,9 +26,6 @@ export default Route.extend({
 
   setupController(controller, model) {
     this._super(controller, model);
-    if(this.bestuurseenheidClassificatie !== "OCMW") {
-      controller.set('allowed', true);
-    }
     controller.set('bestuurseenheid', this.modelFor('leidinggevendenbeheer'));
     controller.set('bestuursfunctie', this.bestuursfunctie);
   }
--- app/routes/leidinggevendenbeheer/bestuursfuncties/index.js
+++ app/routes/leidinggevendenbeheer/bestuursfuncties/index.js
@@ -1,8 +1,12 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
-  model() {
+  currentSession: service('current-session'),
+  async model() {
     this.set('bestuurseenheid', this.modelFor('leidinggevendenbeheer'));
+    const bestuurseenheidClassificatie = await (await this.currentSession.group).classificatie;
+    this.set('bestuurseenheidClassificatie', bestuurseenheidClassificatie.label);
 
     return this.store.query('bestuursfunctie', {
       'filter[bevat-in][is-tijdsspecialisatie-van][bestuurseenheid][:id:]':this.bestuurseenheid.id
@@ -11,6 +15,9 @@ export default Route.extend({
 
   setupController(controller, model) {
     this._super(controller, model);
+    if(this.bestuurseenheidClassificatie !== "OCMW") {
+      controller.set('allowed', true);
+    }
     controller.set('bestuurseenheid', this.bestuurseenheid);
   }
 });
