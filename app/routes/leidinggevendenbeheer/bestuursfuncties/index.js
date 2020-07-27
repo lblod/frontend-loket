4f0fd383535ddb0e508749ca8b1e20844fce2dc7
Oscar Rodriguez <oscar@verify.as>
2020-07-27 09:55:26 +0200

    replaced label by uri check on the OCMW-check for executes

--- app/routes/leidinggevendenbeheer/bestuursfuncties/index.js
+++ app/routes/leidinggevendenbeheer/bestuursfuncties/index.js
@@ -6,7 +6,7 @@ export default Route.extend({
   async model() {
     this.set('bestuurseenheid', this.modelFor('leidinggevendenbeheer'));
     const bestuurseenheidClassificatie = await (await this.currentSession.group).classificatie;
-    this.set('bestuurseenheidClassificatie', bestuurseenheidClassificatie.label);
+    this.set('bestuurseenheidClassificatie', bestuurseenheidClassificatie.uri);
 
     return this.store.query('bestuursfunctie', {
       'filter[bevat-in][is-tijdsspecialisatie-van][bestuurseenheid][:id:]':this.bestuurseenheid.id
@@ -15,7 +15,8 @@ export default Route.extend({
 
   setupController(controller, model) {
     this._super(controller, model);
-    if(this.bestuurseenheidClassificatie !== "OCMW") {
+    // Check if the clasificatie of the bestuurseenheid is not OCMW so we allow the use of this view
+    if(this.bestuurseenheidClassificatie !== "http://data.vlaanderen.be/id/concept/BestuurseenheidClassificatieCode/5ab0e9b8a3b2ca7c5e000002") {
       controller.set('allowed', true);
     }
     controller.set('bestuurseenheid', this.bestuurseenheid);
