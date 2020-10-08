df3b0fbf69c2b0f232b409c4aef65e72887ec316
Claire Lovisa <lovisa.claire@gmail.com>
2020-10-08 11:16:04 +0200

    Remove direct call to mandataris archive service

--- app/components/mandatenbeheer/mandataris-table.js
+++ app/components/mandatenbeheer/mandataris-table.js
@@ -1,14 +1,9 @@
 import Component from '@ember/component';
-import fetch from 'fetch';
 
 export default Component.extend({
   actions: {
     async removeMandataris(mandataris) {
-      await fetch(`/mandataris-archive/${mandataris.id}/archive`, {
-        method: 'POST'
-      });
       await mandataris.destroyRecord();
-
       const count = this.content.meta.count - 1;
       this.content.set('meta.count', count);
     }
