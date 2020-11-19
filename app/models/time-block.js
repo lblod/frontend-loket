7c240065eeb2b85c226fadcb75e3f424a4c98f56
Claire Lovisa <lovisa.claire@gmail.com>
2020-11-19 17:22:29 +0100

    Add some model updates

--- app/models/time-block.js
+++ app/models/time-block.js
@@ -1,7 +1,9 @@
-import Model, {attr} from '@ember-data/model';
+import Model, {attr, belongsTo} from '@ember-data/model';
 
 export default class TimeBlock extends Model {
   @attr('string') label;
   @attr('date') hasStart;
   @attr('date') hasEnd;
+
+  @belongsTo('concept-scheme') conceptScheme;
 }
