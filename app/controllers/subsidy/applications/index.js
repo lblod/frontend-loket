46736505b0c8856c42177c1d6b7dfc1ffa03fac2
Claire Lovisa <lovisa.claire@gmail.com>
2020-11-20 09:59:26 +0100

    Fix minor styling issue

--- app/controllers/subsidy/applications/index.js
+++ app/controllers/subsidy/applications/index.js
@@ -1,4 +1,5 @@
 import Controller from '@ember/controller';
+import { SENT_STATUS } from '../../../models/submission-document-status';
 
 export default class SubsidyApplicationsIndexController extends Controller {
   page = 0
