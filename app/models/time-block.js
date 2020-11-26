6ad627980f5f313cb28fada5f1f513642b01510f
Dries Hugaerts <dries.hugaerts@live.be>
2020-11-26 16:06:45 +0100

    added "reeks" column to subsidy applications overview.

--- app/models/time-block.js
+++ app/models/time-block.js
@@ -2,8 +2,8 @@ import Model, {attr, belongsTo} from '@ember-data/model';
 
 export default class TimeBlock extends Model {
   @attr('string') label;
-  @attr('date') hasStart;
-  @attr('date') hasEnd;
+  @attr('date') start;
+  @attr('date') end;
 
   @belongsTo('concept-scheme') conceptScheme;
 }
