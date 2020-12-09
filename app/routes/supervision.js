8015d5a525a64286a7d894cca63181d358ee94d9
Dries Hugaerts <dries.hugaerts@live.be>
2020-12-09 14:22:06 +0100

    added back access check in Toezicht/Supervision route.

--- app/routes/supervision.js
+++ app/routes/supervision.js
@@ -1,7 +1,15 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default class SupervisionRoute extends Route.extend(AuthenticatedRouteMixin) {
+
+  @service currentSession
+
+  beforeModel() {
+    if (!this.currentSession.canAccessToezicht)
+      this.transitionTo('index');
+  }
 
 }
