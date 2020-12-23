5fd31be8d620dc1226b8e6b20fba6f907e7a4c98
achraf <achrafatauil@hotmail.com>
2020-12-23 13:14:42 +0100

    Upgrade index route, template & controller

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,33 +1,47 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
-export default Controller.extend({
-	currentSession: service(),
+export default class IndexController extends Controller {
+  @service() currentSession;
 
-	actions: {
+  @action
     goToToezicht() {
       this.transitionToRoute('supervision.submissions');
-    },
-		goToBbcdr() {
-			this.transitionToRoute('bbcdr.rapporten');
-		},
-		goToMandatenbeheer() {
-			this.transitionToRoute('mandatenbeheer.mandatarissen');
-		},
-		goToBerichtencentrum() {
-			this.transitionToRoute('berichtencentrum.berichten');
-		},
-		goToAdministratievegegevens() {
-			this.transitionToRoute('administratieve-gegevens');
-		},
-		goToLeidinggevendenbeheer() {
-			this.transitionToRoute('leidinggevendenbeheer.bestuursfuncties');
-		},
-		goToPersoneelsbeheer() {
-			this.transitionToRoute('personeelsbeheer.personeelsaantallen.index');
-		},
-		goToSubsidiebeheer() {
-			this.transitionToRoute('subsidy.applications.index');
-		}		
-	}
-});
+    }
+
+  @action
+    goToBbcdr() {
+      this.transitionToRoute('bbcdr.rapporten');
+    }
+    
+  @action
+    goToMandatenbeheer() {
+      this.transitionToRoute('mandatenbeheer.mandatarissen');
+    }
+
+  @action
+    goToBerichtencentrum() {
+      this.transitionToRoute('berichtencentrum.berichten');
+    }
+    
+  @action
+    goToAdministratievegegevens() {
+      this.transitionToRoute('administratieve-gegevens');
+    }
+    
+  @action
+    goToLeidinggevendenbeheer() {
+      this.transitionToRoute('leidinggevendenbeheer.bestuursfuncties');
+    }
+    
+  @action
+    goToPersoneelsbeheer() {
+      this.transitionToRoute('personeelsbeheer.personeelsaantallen.index');
+    }
+    
+  @action
+    goToSubsidiebeheer() {
+      this.transitionToRoute('subsidy.applications.index');
+    }		
+}
