2c46e76ee3895cff10eda1ca1da53a0564c51262
achraf <achrafatauil@hotmail.com>
2020-12-23 15:19:50 +0100

    Upgrade leidinggevendenbeheer/bestuursfuncties/index route, template & controller

--- app/controllers/leidinggevendenbeheer/bestuursfuncties/index.js
+++ app/controllers/leidinggevendenbeheer/bestuursfuncties/index.js
@@ -1,14 +1,14 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object'
 
-export default Controller.extend({
-  router: service(),
-  page: 0,
-  size: 20,
+export default class LeidinggevendenbeheerBestuursfunctiesIndexController extends Controller {
+  @service() router;
+  page = 0;
+  size = 20;
 
-  actions: {
+  @action
     displayBestuursfunctie(bestuursfunctie) {
       this.router.transitionTo('leidinggevendenbeheer.bestuursfuncties.bestuursfunctie.functionarissen', bestuursfunctie.id);
     }
-  }
-});
+}
