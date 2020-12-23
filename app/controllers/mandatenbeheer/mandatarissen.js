6f0b9cc35c6914be9661043cc6df3dac4339f046
achraf <achrafatauil@hotmail.com>
2020-12-23 14:02:16 +0100

    Upgrade mandatenbeheer/mandatarissen route, template & controller

--- app/controllers/mandatenbeheer/mandatarissen.js
+++ app/controllers/mandatenbeheer/mandatarissen.js
@@ -1,42 +1,50 @@
 import Controller from '@ember/controller';
 import { task, timeout } from 'ember-concurrency';
-import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { alias } from '@ember/object/computed';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
-export default Controller.extend({
-  router: service(),
-  sort: 'is-bestuurlijke-alias-van.achternaam',
-  page: 0,
-  size: 20,
-  hasActiveChildRoute: computed('router.currentRouteName', function() {
-    return this.get('router.currentRouteName').startsWith('mandatenbeheer.mandatarissen.') &&
-      this.get('router.currentRouteName') != 'mandatenbeheer.mandatarissen.index';
-  }),
+export default class MandatenbeheerMandatarissenController extends Controller {
+  @service() router;
 
-  startDate: alias('mandatenbeheer.startDate'),
-  bestuursperioden: alias('mandatenbeheer.bestuursperioden'),
-  bestuurseenheid: alias('mandatenbeheer.bestuurseenheid'),
-  bestuursorganen: alias('mandatenbeheer.bestuursorganen'),
+  sort = 'is-bestuurlijke-alias-van.achternaam';
+  page = 0;
+  size = 20;
 
-  search: task(function*(searchData) {
+
+  @tracked mandatenbeheer;
+
+  get hasActiveChildRoute() {
+    return this.router.currentRouteName.startsWith('mandatenbeheer.mandatarissen.') &&
+      this.router.currentRouteName != 'mandatenbeheer.mandatarissen.index';
+  }
+
+  @alias('mandatenbeheer.startDate') startDate;
+  @alias('mandatenbeheer.bestuursperioden') bestuursperioden;
+  @alias('mandatenbeheer.bestuurseenheid') bestuurseenheid;
+  @alias('mandatenbeheer.bestuursorganen') bestuursorganen;
+
+  @task(function*(searchData) {
     yield timeout(300);
     this.set('page', 0);
     this.set('filter', searchData);
-  }).restartable(),
+  }).restartable() search;
 
-  actions: {
+  @action
     handleAddMandatarisClick() {
-      if (this.get('router.currentRouteName') === 'mandatenbeheer.mandatarissen.new')
+      if (this.router.currentRouteName === 'mandatenbeheer.mandatarissen.new')
         this.transitionToRoute('mandatenbeheer.mandatarissen.index');
       else
         this.transitionToRoute('mandatenbeheer.mandatarissen.new');
-    },
+    }
 
+  @action
     handleBeheerFractiesClick() {
       this.router.transitionTo('mandatenbeheer.fracties');
-    },
+    }
 
+  @action  
     selectPeriod(startDate) {
       this.transitionToRoute('mandatenbeheer.mandatarissen', {
         queryParams: {
@@ -45,5 +53,4 @@ export default Controller.extend({
         }
       });
     }
-  }
-});
+}
