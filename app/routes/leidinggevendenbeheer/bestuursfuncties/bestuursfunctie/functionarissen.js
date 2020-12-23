4577efa5745899e7b64def03075a639e5187a4a7
achraf <achrafatauil@hotmail.com>
2020-12-23 15:24:03 +0100

    Upgrade ... bestuursfunctie/functionarissen

--- app/routes/leidinggevendenbeheer/bestuursfuncties/bestuursfunctie/functionarissen.js
+++ app/routes/leidinggevendenbeheer/bestuursfuncties/bestuursfunctie/functionarissen.js
@@ -1,5 +1,4 @@
 import Route from '@ember/routing/route';
 
-export default Route.extend({
-
-});
+export default class LeidinggevendenbeheerBestuursfunctiesBestuursfunctieFunctionarissenRoute extends Route {
+}
