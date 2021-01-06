02075e08d9563e34e5f4064b723bc6fbf720d2af
achraf <achrafatauil@hotmail.com>
2021-01-06 12:40:29 +0100

    Upgrade berichtencentrum/conversatie-view component & cc

--- app/components/berichtencentrum/conversatie-view.js
+++ app/components/berichtencentrum/conversatie-view.js
@@ -1,20 +1,13 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
-export default Component.extend({
-  classNames: ['au-o-grid__item au-u-2-5@medium'],
-  router: service(),
-  showExitModal: false,
+export default class BerichtencentrumConversatieViewComponent extends Component {
+  @service() router;
 
-  async didReceiveAttrs() {
-    const messages = await this.model.berichten;
-    const sortedMessages = messages.sortBy('verzonden');
-    this.set('berichten', sortedMessages);
-  },
-
-  actions: {
-    close(){
-      this.close();
-    }
-  }
-});
+  @tracked showExitModal = false;
+  
+  get berichten(){
+    return this.args.model.berichten
+  } 
+}
