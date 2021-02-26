10b1fcc99440a5eefde7134fef429f23de58984a
Unknown <achrafatauil@hotmail.com>
2021-02-26 12:53:52 +0100

    add session & currentsession + logic (see description)
    Even though index should work as the other routes, the this.currectSession.load function does not get called automatically when redirected by mock-login. 
    
    The requireAuth gets called because when logging out , it will otherwise not redirect you away from the page but stay with every module showing it is disabled

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,13 @@
 import Route from '@ember/routing/route';
-import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import { inject as service } from '@ember/service';
 
-export default class IndexRoute extends Route.extend(AuthenticatedRouteMixin) {
-  
+export default class IndexRoute extends Route{
+  @service session;
+  @service currentSession;
+
+  async beforeModel(transition) {
+    this.currentSession.load()
+    this.session.requireAuthentication(transition, 'login');
+  }
+
 }
