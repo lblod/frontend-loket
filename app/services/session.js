44359e355dfeb7263a20fc8c78e9d480959dcd98
Unknown <achrafatauil@hotmail.com>
2021-03-01 12:59:34 +0100

    create a sessions service that extends ember auths SessionService

---
+++ app/services/session.js
@@ -0,0 +1,23 @@
+import SessionService from 'ember-simple-auth/services/session';
+import ENV from 'frontend-loket/config/environment';
+
+export default SessionService.extend({
+
+  handleInvalidation() {
+    const logoutUrl = ENV['torii']['providers']['acmidm-oauth2']['logoutUrl'];
+    if (logoutUrl.startsWith('http')) {
+      window.location.replace(logoutUrl);
+    }
+    else {
+      warn('Incorrect logout URL configured', { id: 'session-invalidation-failure' });
+    }
+  },
+
+  _loadCurrentSession() {
+    return this.currentSession.load().catch((e) => {
+      warn(e, { id: 'session-load-failure' });
+      this.session.invalidate();
+    });
+  }
+
+});
