0fafa96c56ed49e80de21a4bf32aed65dddc30f0
Unknown <achrafatauil@hotmail.com>
2021-03-10 10:16:54 +0100

    no need for session service in subsidyApplicationController

--- app/controllers/subsidy/applications/index.js
+++ app/controllers/subsidy/applications/index.js
@@ -1,9 +1,7 @@
 import Controller from '@ember/controller';
 import { SENT_STATUS } from '../../../models/submission-document-status';
-import { inject as service } from '@ember/service';
 
 export default class SubsidyApplicationsIndexController extends Controller {
-  @service() session;
   page = 0;
   size = 20;
   sort = '-modified';
