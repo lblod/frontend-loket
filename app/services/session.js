7b009cd15fefc20e7aa2d0e7b1fda4a89819a28e
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-03-12 17:43:09 +0100

    Move the current session loading logic into the session service
    Handling this logic in the session service is a direct replacement of the previous implementation. This also has the benefit that no extra `currentSession.load()` calls are needed if the default page is ever changed in the future.

--- app/services/session.js
+++ app/services/session.js
@@ -1,16 +1,23 @@
+import { warn } from '@ember/debug';
+import { inject as service } from '@ember/service';
 import SessionService from 'ember-simple-auth/services/session';
 import ENV from 'frontend-loket/config/environment';
 
-export default SessionService.extend({
+export default class LoketSessionService extends SessionService {
+  @service currentSession;
+
+  handleAuthentication(routeAfterAuthentication) {
+    super.handleAuthentication(routeAfterAuthentication);
+
+    this.currentSession.load();
+  }
 
   handleInvalidation() {
     const logoutUrl = ENV['torii']['providers']['acmidm-oauth2']['logoutUrl'];
     if (logoutUrl.startsWith('http')) {
-      window.location.replace(logoutUrl);
-    }
-    else {
+      super.handleInvalidation(logoutUrl);
+    } else {
       warn('Incorrect logout URL configured', { id: 'session-invalidation-failure' });
     }
-  },
-
-});
+  }
+}
