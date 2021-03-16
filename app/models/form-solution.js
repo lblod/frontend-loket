bf3dd57be9b254fcdf988cf3a30a3badce9ebe76
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2021-03-16 14:31:19 +0100

    remove unused model-file

--- app/models/form-solution.js
+++
@@ -1,6 +0,0 @@
-import FormSolution from '@lblod/ember-mu-dynamic-forms/models/form-solution' ;
-import { belongsTo } from 'ember-data/relationships';
-
-export default FormSolution.extend({
-  inzendingVoorToezicht: belongsTo('inzending-voor-toezicht')
-});
