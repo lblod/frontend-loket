b8c9f965a815de22b9de9aefae3fe3d4270c9af2
Claire Lovisa <lovisa.claire@gmail.com>
2021-03-17 15:05:17 +0100

    Sort messages in berichtencentrum

--- app/components/berichtencentrum/conversatie-view.js
+++ app/components/berichtencentrum/conversatie-view.js
@@ -1,12 +1,16 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+import { sort } from '@ember/object/computed';
 
 export default class BerichtencentrumConversatieViewComponent extends Component {
   @service() router;
 
+  @sort('berichten', 'generalSort') sortedBerichten;
+
+  @tracked generalSort = Object.freeze(['verzonden']);
   @tracked showExitModal = false;
-  
+
   get berichten(){
     return this.args.model.berichten
   } 
