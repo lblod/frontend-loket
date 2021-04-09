e85d12a31c32844f41907a3af5a46ea966d1c66f
Unknown <achrafatauil@hotmail.com>
2021-04-09 11:22:54 +0200

    cleanup: remove unused methods and properties for functionaris/index

--- app/controllers/leidinggevendenbeheer/bestuursfuncties/bestuursfunctie/functionarissen/new/index.js
+++ app/controllers/leidinggevendenbeheer/bestuursfuncties/bestuursfunctie/functionarissen/new/index.js
@@ -1,39 +1,16 @@
 import Controller from '@ember/controller';
-import { task } from 'ember-concurrency';
-import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 
 export default class LeidinggevendenbeheerBestuursfunctiesBestuursfunctieFunctionarissenNewIndexController extends Controller {
-  @service() store;
-  @service() router;
-
-  @tracked functionaris = null;
-  @tracked defaultStatus = null;
-  @tracked bestuursfunctie;
-  @tracked functionaris;
-  @tracked isCreatingNewPerson;
-
-
   @action
     choosePeriode(person){
-      this.transitionToRoute('leidinggevendenbeheer.bestuursfuncties.bestuursfunctie.functionarissen.new.periode', person.get('id'))
-    }
-
-  @action
-    goBackToSearch() {
-      if (this.functionaris) {
-        this.functionaris.deleteRecord();
-        this.functionaris = null;
-      }
-      this.isCreatingNewPerson = false;
+      this.transitionToRoute('leidinggevendenbeheer.bestuursfuncties.bestuursfunctie.functionarissen.new.periode', person.get('id'));
     }
 
   @action
     createNewPerson() {
       this.transitionToRoute('leidinggevendenbeheer.bestuursfuncties.bestuursfunctie.functionarissen.new-person');
     }
-
 
   @action
     cancel() {
