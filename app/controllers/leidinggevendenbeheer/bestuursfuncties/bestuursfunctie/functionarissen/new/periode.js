7b3e254ee5f5ff7e451b9c1ccd551ac567cc279f
Unknown <achrafatauil@hotmail.com>
2021-04-09 16:31:36 +0200

    The use of auButton is better here instead of auLink

--- app/controllers/leidinggevendenbeheer/bestuursfuncties/bestuursfunctie/functionarissen/new/periode.js
+++ app/controllers/leidinggevendenbeheer/bestuursfuncties/bestuursfunctie/functionarissen/new/periode.js
@@ -1,4 +1,5 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 import { task } from 'ember-concurrency';
 
 export default class LeidinggevendenbeheerBestuursfunctiesBestuursfunctieFunctionarissenNewPeriodeController extends Controller {
@@ -7,4 +8,14 @@ export default class LeidinggevendenbeheerBestuursfunctiesBestuursfunctieFunctio
     yield this.model.save();
     this.transitionToRoute('leidinggevendenbeheer.bestuursfuncties.bestuursfunctie.functionarissen.index');
   }) save;
+
+  @action
+  goBackToSearch() {
+    this.transitionToRoute('leidinggevendenbeheer.bestuursfuncties.bestuursfunctie.functionarissen.new');
+  }
+
+  @action
+  cancel() {
+    this.transitionToRoute('leidinggevendenbeheer.bestuursfuncties.bestuursfunctie.functionarissen.index');
+  }
 }
