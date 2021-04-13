677d72896a278f41941d211cbf9ff6aa1767f973
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-04-13 17:01:15 +0200

    Rework the "new subsidy application" page
    It now creates a subsidy-measure-consumption record.

--- app/routes/subsidy/applications/new.js
+++ app/routes/subsidy/applications/new.js
@@ -1,37 +1,60 @@
+import { assert } from '@ember/debug';
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import { CONCEPT_STATUS } from '../../../models/submission-document-status';
+import { ROLES } from 'frontend-loket/models/participation';
 
 export default class SubsidyApplicationsNewRoute extends Route {
-  @service currentSession
+  @service currentSession;
+  @service router;
+  @service store;
 
-  async beforeModel() {
-    const conceptStatuses = await this.store.query('submission-document-status', {
-      page: { size: 1 },
-      'filter[:uri:]': CONCEPT_STATUS
+  async beforeModel(transition) {
+    let seriesId = transition.to.queryParams.seriesId;
+    assert('A subsidy-measure-offer-series id needs to be provided through the `seriesId` query parameter', Boolean(seriesId));
+
+    transition.data.series = await this.store.findRecord('subsidy-measure-offer-series', seriesId, {
+      backgroundReload: false
     });
 
-    if (conceptStatuses.length)
-      this.conceptStatus = conceptStatuses.firstObject;
+    if (!transition.data.series) {
+      // TODO: Show a warning / error page
+      this.router.replaceWith('subsidy.applications.available-subsidies');
+    }
   }
 
-  async model() {
-    const bestuurseenheid = await this.currentSession.group;
+  async model(params, transition) {
+    let series = transition.data.series;
+    let organisation = await this.currentSession.groupContent;
+    let currentUser = await this.currentSession.userContent;
 
-    const currentUser = await this.currentSession.user;
-    const applicationForm = this.store.createRecord('application-form', {
-      organization: bestuurseenheid,
-      status: this.conceptStatus,
+    let participation = this.store.createRecord('participation', {
+      role: ROLES.APPLICANT,
+      participatingBestuurseenheid: organisation
+    });
+
+    await participation.save();
+
+    let applicationFlow = await series.activeApplicationFlow;
+    let subsidyMeasureOffer = await series.subsidyMeasureOffer;
+
+    let consumption = this.store.createRecord('subsidy-measure-consumption', {
+      subsidyApplicationFlow: applicationFlow,
+      subsidyMeasureOffer,
       creator: currentUser,
-      lastModifier: currentUser
+      lastModifier: currentUser,
+      created: new Date(),
+      modified: new Date(),
+      // TODO Add the concept status once the data is added to the triple store
     });
 
-    await applicationForm.save();
+    consumption.participations.pushObject(participation);
 
-    return applicationForm;
+    await consumption.save();
+
+    return consumption;
   }
 
-  afterModel(model) {
-    this.transitionTo('subsidy.applications.edit', model.id);
+  afterModel(consumption) {
+    this.router.replaceWith('subsidy.applications.edit', consumption.id);
   }
 }
