cd3f926d2e72e7836befbf8ae49cea097ff5d9d1
Claire Lovisa <lovisa.claire@gmail.com>
2021-04-22 16:14:04 +0200

    Fix inverse relationship in applicationFlow model

--- app/models/subsidy-application-flow.js
+++ app/models/subsidy-application-flow.js
@@ -3,8 +3,8 @@ import Model, { belongsTo, hasMany } from '@ember-data/model';
 export default class SubsidyApplicationFlowModel extends Model {
 
   @belongsTo('subsidy-measure-offer-series') subsidyMeasureOfferSeries;
-  @belongsTo('subsidy-application-flow-step', { inverse: 'applicationFlow'}) firstApplicationStep;
-  @hasMany('subsidy-application-flow-step') definedSteps;
+  @belongsTo('subsidy-application-flow-step') firstApplicationStep;
+  @hasMany('subsidy-application-flow-step', { inverse: 'applicationFlow'}) definedSteps;
 
   get sortedDefinedSteps() {
       return this.get('definedSteps').sortBy('order');
