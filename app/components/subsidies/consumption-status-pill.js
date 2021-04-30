96290fe36f3981ba886f555293bc4ba91e5269d5
Dries Hugaerts <dries.hugaerts@live.be>
2021-04-30 11:23:34 +0200

    updated active state to show the active-step.

--- app/components/subsidies/consumption-status-pill.js
+++ app/components/subsidies/consumption-status-pill.js
@@ -3,20 +3,34 @@ import { STATUS } from '../../models/subsidy-measure-consumption-status';
 
 export default class SubsidiesConsumptionStatusPill extends Component {
 
+  get consumption() {
+    return this.args.consumption;
+  }
+
   get status() {
-    return this.args.consumption.status;
+    return this.consumption.status;
+  }
+
+  get label() {
+    let label = this.status.get('label').toUpperCase();
+    if (this.status.get('uri') === STATUS.ACTIVE) {
+      const step = this.consumption.get('activeSubsidyApplicationFlowStep.subsidyProceduralStep');
+      if (step)
+        label = `${label}: ${step.get('description')}`;
+    }
+    return label;
   }
 
   get skin() {
-    switch(this.status.get('uri')) {
+    switch (this.status.get('uri')) {
       case STATUS.SENT:
-        return "success"
+        return 'success';
       case STATUS.ACTIVE:
-        return "action"
+        return 'action';
       case STATUS.CONCEPT:
-        return "border"
+        return 'border';
       default:
-        return ""
+        return '';
     }
   }
 }
