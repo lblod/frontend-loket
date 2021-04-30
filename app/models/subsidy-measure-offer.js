4637edc85b92dda8a694bbbf3075e63e7d1c250f
Claire Lovisa <lovisa.claire@gmail.com>
2021-04-30 16:26:33 +0200

    Add detail links about subsidy types

--- app/models/subsidy-measure-offer.js
+++ app/models/subsidy-measure-offer.js
@@ -2,6 +2,6 @@ import Model, { attr, hasMany } from '@ember-data/model';
 
 export default class SubsidyMeasureOfferModel extends Model {
   @attr title;
-
+  @attr externalInformation;
   @hasMany('subsidy-measure-offer-series') series;
 }
