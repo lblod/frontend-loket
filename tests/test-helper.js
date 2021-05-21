a6a2541e6642120382700b0874053f00abac62c4
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-05-21 12:39:18 +0200

    v3.20.2...v3.24.0

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,8 +1,12 @@
 import Application from 'frontend-loket/app';
 import config from 'frontend-loket/config/environment';
+import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
 
 start();
