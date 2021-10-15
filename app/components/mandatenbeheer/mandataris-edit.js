654cc55d221946ec2b4c2f766958ee12ac85433c
Unknown <achrafatauil@hotmail.com>
2021-10-15 10:25:31 +0200

    Upgrade class component (see description)
    Too complex to explain changes here. mainly upgrade to octane and some code refactor. Not working completely yet but you will  notice what still needs to be done from using the UI. This component is too big and complex and does too much. After upgrade it should probably be split up

--- app/components/mandatenbeheer/mandataris-edit.js
+++ app/components/mandatenbeheer/mandataris-edit.js
@@ -1,100 +1,181 @@
+import Component from '@glimmer/component';
 import { warn } from '@ember/debug';
-import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 import { task } from 'ember-concurrency';
-import { observer } from '@ember/object';
 import { isBlank } from '@ember/utils';
 import { A } from '@ember/array';
-import { computed } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
-export default Component.extend({
-  tag: 'li',
-  classNames: ['au-c-list-vertical__item'],
-  store: service(),
-  dateFormat: 'DD-MM-YYYY',
-  editMode: false, //some components will change behaviour when being in editMode
-  correctMode: false,
-  terminateMode: false,
-  createMode: false,
-  promptMode: false,
-  viewMode: computed('editOnlyMode', 'createMode', function(){
-    return !(this.editOnlyMode || this.createMode);
-  }),
-  saveError: false,
-  hasFatalError: computed('saveError', 'requiredFieldError', function(){
-    return this.saveError || this.requiredFieldError;
-  }),
+export default class MandatenbeheerMandatarisEditComponent extends Component {
+  @service store;
 
-  async didReceiveAttrs(){
-    await this.initComponentProperties();
-  },
+  // STATIC
+  dateFormat = 'DD-MM-YYYY';
 
-  async initComponentProperties(){
+  // ALL MODES THIS COMPONENT CAN BE IN
+  @tracked createMode = false;
+  @tracked editMode = false;
+  @tracked promptMode = false;
+  @tracked terminateMode = false;
+  @tracked correctMode = false;
+
+  // PROPERTIES
+  @tracked mandataris;
+  @tracked mandaat;
+  @tracked status;
+  @tracked beleidsdomeinen;
+  @tracked rangorde;
+  @tracked startDate;
+  @tracked endDate;
+  @tracked fractie;
+
+  //ERRORS
+  @tracked requiredFieldError = false;
+  @tracked saveError = false;
+  @tracked destroyOnError = A();
+
+  get startDateError() {
+    if(isBlank(this.startDate)) {
+      return 'geplande start is een vereist veld';
+    }
+
+    if(this.endDate < this.startDate) {
+      return 'geplande start moet voor gepland einde liggen';
+    }
+
+    return null;
+  }
+
+  get endDateError() {
+    if(this.endDate < this.startDate) {
+      return 'gepland einde moet na geplande start liggen';
+    }
+
+    return null;
+  }
+
+  constructor() {
+    super(...arguments);
+    this.initProperties();
+  }
+
+  initProperties() {
+    this.mandataris = this.args.mandataris;
+    this.mandaat = this.mandataris.bekleedt;
+    this.status = this.mandataris.status;
+    this.beleidsdomeinen = this.mandataris.beleidsdomein || A();
+    this.fractie = this.mandataris.heeftLidmaatschap.get('binnenFractie');
+    this.startDate = this.mandataris.start;
+    this.rangorde = this.mandataris.get('rangorde.content');
+    this.endDate = this.mandataris.einde;
+    this.destroyOnError = A();
     this.toggleCreateMode();
-    this.set('destroyOnError', A());
-    this.set('saveError', false);
-    this.set('requiredFieldError', false);
-    this.set('fractie', await this.get('mandataris.heeftLidmaatschap.binnenFractie'));
-    this.set('beleidsdomeinen', ((await this.get('mandataris.beleidsdomein')) || A()).toArray());
-    this.set('mandaat', await this.get('mandataris.bekleedt'));
-    this.set('startDate', this.get('mandataris.start'));
-    this.set('endDate', this.get('mandataris.einde'));
-    this.set('rangorde', this.get('mandataris.rangorde.content'));
-    this.set('status', await this.get('mandataris.status'));
-  },
+  }
+
 
   toggleCreateMode(){
-    if(!this.get('mandataris.id'))
-      this.set('createMode', true);
-  },
+    if(!this.mandataris.id)
+      this.createMode = true;
+  }
 
-  save: task(function* (){
-    this.set('saveError', false);
-    this.set('requiredFieldError', false);
+  @action
+  openEditPrompt(){
+    this.editMode = false;
+    this.promptMode = true;
+  }
+
+  @action
+  correct(){
+    this.promptMode = false;
+    this.editMode = true;
+    this.correctMode = !this.correctMode;
+  }
+
+  @action
+  terminate(){
+    this.promptMode = false;
+    this.editMode = true;
+    this.terminateMode = !this.terminateMode;
+  }
+
+  @action
+  setMandaat(mandaat) {
+    console.log(mandaat)
+    this.mandaat = mandaat;
+  }
+
+  @action
+  setStatusCode(status){
+    this.status = status;
+  }
+
+  @action
+  setBeleidsdomein(beleidsdomeinen){
+    this.beleidsdomeinen = beleidsdomeinen;
+  }
+
+  @action
+  setFractie(fractie){
+    this.fractie = fractie;
+  }
+
+  /**  Temporary fix until we start using new datepicker. */
+  @action
+  preventPageRefresh(e) {
+    e.preventDefault();
+  }
+
+  // MAIN SAVE ACTION
+  @task
+  *save() {
     try {
-      yield this.saveNewBeleidsdomeinen();
+      this.saveError = false;
+      this.requiredFieldError = false;
 
-      //fractie is a complex object, requires some special flow
+      yield this.saveNewBeleidsdomeinen();
       yield this.saveLidmaatschap();
 
-      if(!this.mandaat){
-        this.set('requiredFieldError', true);
-        return this.mandataris;
+      if(!this.mandaat || !this.status){
+        this.requiredFieldError = true;
+        return;
       }
 
-      this.set('mandataris.bekleedt', this.mandaat);
-      this.get('mandataris.beleidsdomein').setObjects(this.beleidsdomeinen);
-      this.set('mandataris.start', this.startDate);
-      this.set('mandataris.einde', this.endDate);
+      this.mandataris.bekleedt = this.mandaat;
+      this.mandataris.beleidsdomein = this.beleidsdomeinen;
+      this.mandataris.start = this.startDate;
+      this.mandataris.einde = this.endDate;
 
-      if(this.rangorde)
-        this.set('mandataris.rangorde', {content: this.rangorde, language: 'nl'});
-      else
-        this.set('mandataris.rangorde', undefined);
+      if(this.rangorde) {
+        this.mandataris.rangorde = { content: this.rangorde, language: 'nl' };
+      } else {
+        this.mandataris.rangorde = undefined;
+      }
 
-      this.set('mandataris.status', this.status);
+      this.mandataris.status = this.status;
 
-      return yield this.mandataris.save();
+      yield this.mandataris.save();
+      this.resetView();
     }
     catch (e){
-      this.set('saveError', true);
+      this.saveError = true;
       warn(`error during save ${e}`, { id: 'save-error' });
       this.cleanUpOnError();
     }
-  }),
+  }
 
   async saveNewBeleidsdomeinen(){
-    let savingD = this.beleidsdomeinen.map(async d => {
-      if(d.get('isNew')){
-        await d.save();
-        this.destroyOnError.pushObject(d);
+    let savingD = this.beleidsdomeinen.map(async beleidsdomein => {
+      if(beleidsdomein.isNew){
+        await beleidsdomein.save();
+        this.destroyOnError.pushObject(beleidsdomein);
       }
     });
     return Promise.all(savingD);
-  },
+  }
 
   async saveLidmaatschap(){
-    if(!this.get('mandataris.heeftLidmaatschap.id')){
+    if(!this.mandataris.heeftLidmaatschap.get('id')){
       if (this.fractie) {
         await this.createNewLidmaatschap();
         return;
@@ -106,61 +187,37 @@ export default Component.extend({
     }
 
     // if new and old fractie are both onafhankelijk, nothing needs to be done...
-    let currFractie = await this.get('mandataris.heeftLidmaatschap.binnenFractie');
-    if(currFractie && (await currFractie.get('fractietype.isOnafhankelijk')) && this.get('fractie.fractietype.isOnafhankelijk')){
+    let currFractie = await this.mandataris.heeftLidmaatschap.get('binnenFractie');
+    if(currFractie && (await currFractie.fractietype.get('isOnafhankelijk')) && this.fractie.fractietype.get('isOnafhankelijk')){
       return;
     }
 
-    if((this.get('mandataris.heeftLidmaatschap.binnenFractie.id') !== this.get('fractie.id'))){
+    if((this.mandataris.heeftLidmaatschap.get('binnenFractie.id') !== this.fractie.get('id'))){
       await this.updateLidmaatschap();
       return;
     }
     if(!this.fractie)
       return;
-    this.set('mandataris.heeftLidmaatschap.tijdsinterval', await this.getTijdsinterval(this.get('mandataris.start'), this.get('mandataris.einde')));
-  },
-
-  async updateLidmaatschap(){
-    let lidmaatschap = await this.get('mandataris.heeftLidmaatschap');
-    let fractie = await lidmaatschap.get('binnenFractie');
-    await lidmaatschap.destroyRecord();
-    if(fractie.get('fractietype.isOnafhankelijk')){
-      await fractie.destroyRecord();
-    }
-    if(this.fractie){
-      await this.createNewLidmaatschap();
-    }
-  },
+    this.mandataris.heeftLidmaatschap.tijdsinterval = await this.getTijdsinterval(this.startDate, this.endDate);
+  }
 
   async createNewLidmaatschap(){
-    let tijdsinterval = await this.getTijdsinterval(this.get('mandataris.start'), this.get('mandataris.einde'));
+    let tijdsinterval = await this.getTijdsinterval(this.startDate, this.endDate);
     let fractie = this.fractie;
 
-    if(!fractie.get('id')){
+    if(!this.fractie.get('id')){
       await fractie.save();
     }
 
     let lidmaatschap =  await this.store.createRecord('lidmaatschap', {
-      binnenFractie: fractie, lidGedurende: tijdsinterval
+      binnenFractie: fractie,
+      lidGedurende: tijdsinterval
     });
     await lidmaatschap.save();
 
     this.destroyOnError.pushObject(lidmaatschap);
-    this.set('mandataris.heeftLidmaatschap', lidmaatschap);
-  },
-
-  valideerStartEnEinde: observer('startDate', 'endDate', function() {
-    const start = this.startDate;
-    const end = this.endDate;
-    this.set('startDateError', null);
-    this.set('endDateError', null);
-    if (isBlank(start))
-      this.set('startDateError', 'geplande start is een vereist veld');
-    if (start && end  && end < start ) {
-      this.set('startDateError', 'geplande start moet voor gepland einde liggen');
-      this.set('endDateError', 'gepland einde moet na geplande start liggen');
-    }
-  }),
+    this.mandataris.heeftLidmaatschap = lidmaatschap;
+  }
 
   async getTijdsinterval(begin, einde){
     let tijdsinterval = await this.findTijdsinterval(begin, einde);
@@ -170,13 +227,13 @@ export default Component.extend({
       this.destroyOnError.pushObject(tijdsinterval);
     }
     return tijdsinterval;
-  },
+  }
 
   async findTijdsinterval(startDate, endDate){
     let begin = startDate ? startDate.toISOString().substring(0, 10) : '';
     let einde = endDate ? endDate.toISOString().substring(0, 10) : '';
     return await this.store.query('tijdsinterval',{filter: {begin, einde}});
-  },
+  }
 
   async cleanUpOnError(){
     //TODO: better rollback of relations
@@ -184,73 +241,27 @@ export default Component.extend({
     this.destroyOnError.forEach(o => {
         o.destroyRecord();
     });
-  },
+  }
 
-  actions: {
-    /**  Temporary fix until we start using new datepicker. */
-    preventPageRefresh(e) {
-      e.preventDefault();
-    },
-
-    setFractie(fractie){
-      this.set('fractie', fractie);
-    },
-
-    setMandaat(mandaat){
-      this.set('mandaat', mandaat);
-    },
-
-    setBeleidsdomein(beleidsdomeinen){
-      this.set('beleidsdomeinen', beleidsdomeinen);
-    },
-
-    setStatusCode(status){
-      this.set('status', status);
-    },
-
-    async save(){
-      let mandataris = await this.save.perform();
-      if(!this.hasFatalError){
-        this.set('createMode', false);
-        this.set('promptMode', false);
-        this.set('editMode', false);
-        this.set('terminateMode', false);
-        this.set('correctMode', false);
-        this.onSave(mandataris);
-      }
-    },
-
-    cancel(){
-      this.initComponentProperties();
-      if(this.createMode){
-        this.set('createMode', false);
-        this.onCancelCreate(this.mandataris);
-      }
-      else {
-        this.set('promptMode', false);
-        this.set('editMode', false);
-        this.set('terminateMode', false);
-        this.set('correctMode', false);
-      }
-    },
-
-    previous(){
-      this.onPrevious();
-    },
-
-    edit(){
-      this.set('editMode', false);
-      this.set('promptMode', true);
-    },
-    correct(){
-      this.set('promptMode', false);
-      this.set('editMode', true);
-      this.set('correctMode', !this.correctMode);
-    },
-    terminate(){
-      this.set('promptMode', false);
-      this.set('editMode', true);
-      this.set('terminateMode', !this.terminateMode);
+  @action
+  cancel(){
+    this.initProperties();
+    if(this.createMode){
+      this.createMode = false;
+      this.args.onCancelCreate(this.mandataris);
+    } else {
+      this.resetView();
     }
   }
-});
+
+  resetView() {
+    if(!this.hasFatalError){
+      this.createMode = false;
+      this.promptMode = false;
+      this.editMode = false;
+      this.terminateMode = false;
+      this.correctMode = false;
+
+    }
+  }
+}
