b479c2b0a8dd355af723af90c6e88541494bf69f
Dries Hugaerts <dries.hugaerts@live.be>
2021-11-25 14:30:54 +0100

    Added a reopen button on submission row items. For dev.

--- app/controllers/supervision/submissions/index.js
+++ app/controllers/supervision/submissions/index.js
@@ -1,7 +1,19 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 
 export default class SupervisionSubmissionsIndexController extends Controller {
   page = 0;
   size = 20;
   sort = 'status.label,-sent-date,-modified';
+
+  @action
+  reopen(submission) {
+    if (confirm('Are you sure you wan\'t to do this? It might have unexpected consequences!'))
+      this.store
+          .findRecord('submission-document-status', '79a52da4-f491-4e2f-9374-89a13cde8ecd')
+          .then(function(concept) {
+            submission.status = concept;
+            submission.save();
+          });
+  }
 }
