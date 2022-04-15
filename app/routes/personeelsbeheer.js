0d52267b11c175a6bc4f18b21639d20219e93f4f
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2022-04-15 16:56:47 +0200

    Convert all routes to native classes

--- app/routes/personeelsbeheer.js
+++ app/routes/personeelsbeheer.js
@@ -1,18 +1,18 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
-export default Route.extend({
-  currentSession: service(),
-  session: service(),
+export default class PersoneelsbeheerRoute extends Route {
+  @service currentSession;
+  @service session;
 
   beforeModel(transition) {
     this.session.requireAuthentication(transition, 'login');
 
     if (!this.currentSession.canAccessPersoneelsbeheer)
       this.transitionTo('index');
-  },
+  }
 
   model() {
     return this.currentSession.group; // bestuurseenheid
-  },
-});
+  }
+}
