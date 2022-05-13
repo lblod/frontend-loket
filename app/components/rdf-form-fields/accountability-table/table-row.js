ff6b914988d4c4a70d1d560bde1e41dfa537b90e
Achraf Atauil <achrafatauil@hotmail.com>
2022-05-13 16:51:52 +0200

    cleanup & refactor table-row js & hbs (see description)
    - better namespaces
    - make more consistent with engagement & climate table
    - use invalidRow strategy for validation
    - use tracked properties instead of creating a class with old, new & error values
    - removed the this.hasErrors call in hbs AuLabel since it throws a very difficult to debug error

--- app/components/rdf-form-fields/accountability-table/table-row.js
+++ app/components/rdf-form-fields/accountability-table/table-row.js
@@ -4,10 +4,26 @@ import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 import { keepLatestTask, task, timeout } from 'ember-concurrency';
 import rdflib from 'browser-rdflib';
-import { A } from '@ember/array';
+import { XSD } from '@lblod/submission-form-helpers';
+import { scheduleOnce } from '@ember/runloop';
 
+const subsidyBaseUri = 'http://lblod.data.gift/vocabularies/subsidie/ukraine/';
+
+const addressPredicate = new rdflib.NamedNode(
+  `${subsidyBaseUri}address`
+);
+const bedroomCountPredicate = new rdflib.NamedNode(
+  `${subsidyBaseUri}bedroomCount`
+);
+const sharedInvoicePredicate = new rdflib.NamedNode(
+  `${subsidyBaseUri}sharedInvoice`
+);
 const filesPredicate = new rdflib.NamedNode(
-  'http://mu.semte.ch/vocabularies/ext/hasFiles'
+  `${subsidyBaseUri}hasFiles`
+);
+
+const hasInvalidRowPredicate = new rdflib.NamedNode(
+  `${subsidyBaseUri}hasInvalidAccountabilityTableEntry`
 );
 
 class FileField {
@@ -31,28 +47,98 @@ export default class RdfFormFieldsAccountabilityTableTableRowComponent extends C
   @service() addressregister;
   @service() store;
 
+  @tracked address;
+  @tracked bedroomCount;
+  @tracked sharedInvoice;
   @tracked files;
 
-  @tracked addressesWithBusnumbers;
-
-  @tracked address;
-  @tracked busNumber;
+  @tracked addressErrors = [];
+  @tracked bedroomCountErrors = [];
+  @tracked sharedInvoiceErrors = [];
 
   get storeOptions() {
     return this.args.storeOptions;
   }
 
-  get accountabilityEntrySubject() {
-    return this.args.entry.accountabilityEntrySubject
+  get tableSubject() {
+    return this.args.tableSubject;
   }
 
-  get isDisabledBusnumberSelect() {
-    return this.addressesWithBusnumbers > 1;
+  get tableEntrySubject() {
+    return this.args.tableEntrySubject;
+  }
+
+  get onUpdateRow() {
+    return this.args.onUpdateRow;
   }
 
   constructor() {
     super(...arguments);
-    this.files = A(this.args.entry.files);
+    scheduleOnce('actions', this, this.initializeTableRow);
+  }
+
+  initializeTableRow() {
+    this.loadProvidedValue();
+    this.validate();
+  }
+
+  async loadProvidedValue() {
+
+    const entryProperties = this.storeOptions.store.match(
+      this.tableEntrySubject,
+      undefined,
+      undefined,
+      this.storeOptions.sourceGraph
+    );
+      
+    const fileEntryProperties = this.storeOptions.store.match(
+      this.tableEntrySubject,
+      filesPredicate,
+      undefined,
+      this.storeOptions.sourceGraph
+    );
+
+    this.address = this.findEntry(entryProperties, addressPredicate, "");
+    this.bedroomCount = this.findEntry(entryProperties, bedroomCountPredicate, 0);
+    this.sharedInvoice = this.findEntry(entryProperties, sharedInvoicePredicate, false);
+    this.files = await this.parseFileEntry(fileEntryProperties);
+  }
+
+  findEntry(entryProperties, predicate, defaultValue) {
+    return entryProperties.find(
+      (entry) => entry.predicate.value == predicate.value)?.object.value || defaultValue;
+  }
+
+  async parseFileEntry(fileProperties) {
+    const files = [];
+    for (const file of fileProperties) {
+      const fileUri = file.object.value;
+      const fileObject = await this.retrieveFileField(fileUri)
+      files.push(fileObject);
+    }
+    return files;
+  }
+
+  async retrieveFileField(uri) {
+    try {
+      const files = await this.store.query('file', {
+        'filter[:uri:]': uri,
+        page: { size: 1 },
+      });
+      const file = files.get('firstObject');
+      if (file) return new FileField({ record: file, errors: [] });
+      else
+        return new FileField({
+          record: null,
+          errors: ['Geen bestand gevonden'],
+        });
+    } catch (error) {
+      warn(`Failed to retrieve file with URI ${uri}: ${JSON.stringify(error)}`);
+      return new FileField({
+        record: null,
+        errors: ['Ophalen van het bestand is mislukt'],
+      });
+    }
   }
 
   @keepLatestTask
@@ -62,42 +148,26 @@ export default class RdfFormFieldsAccountabilityTableTableRowComponent extends C
     return addressSuggestions;
   }
 
-  @task
-  *selectAddress(value) {
-    const entry = this.args.entry
+  updateTripleObject(subject, predicate, newObject = null) {
+    const triples = this.storeOptions.store.match(
+      subject,
+      predicate,
+      undefined,
+      this.storeOptions.sourceGraph
+    );
 
-    entry.address.errors = [];
-    entry.address.value = value?.fullAddress || "";
+    this.storeOptions.store.removeStatements([...triples]);
 
-    if (this.isEmpty(entry.address.value)) {
-      entry.address.errors.pushObject({
-        message: 'Adres is verplicht.',
-      });
+    if (newObject) {
+      this.storeOptions.store.addAll([
+        {
+          subject: subject,
+          predicate: predicate,
+          object: newObject,
+          graph: this.storeOptions.sourceGraph,
+        },
+      ]);
     }
-
-    this.args.updateFieldValueTriple(entry, 'address');
-  }
-
-  @action
-  updateBedroomCount() {
-    const entry = this.args.entry
-    entry.bedroomCount.errors = [];
-    const parsedValue = parseInt(entry.bedroomCount.value);
-    entry.bedroomCount.value = !isNaN(parsedValue)
-      ? parsedValue
-      : entry.bedroomCount.value;
-
-    this.args.updateFieldValueTriple(entry, 'bedroomCount');
-
-  }
-
-  @action
-  updateSharedInvoice(value) {
-    const entry = this.args.entry
-    entry.sharedInvoice.errors = [];
-    entry.sharedInvoice.value = value
-
-    this.args.updateFieldValueTriple(entry, 'sharedInvoice');
   }
 
   @action
@@ -105,7 +175,7 @@ export default class RdfFormFieldsAccountabilityTableTableRowComponent extends C
     let file = await this.store.findRecord('file', fileId);
     this.storeOptions.store.addAll([
       {
-        subject: this.accountabilityEntrySubject,
+        subject: this.tableEntrySubject,
         predicate: filesPredicate,
         object: new rdflib.NamedNode(file.uri),
         graph: this.storeOptions.sourceGraph,
@@ -143,16 +213,101 @@ export default class RdfFormFieldsAccountabilityTableTableRowComponent extends C
     this.files.removeAt(index);
 
     this.storeOptions.store.removeStatements([{
-      subject: this.accountabilityEntrySubject,
+      subject: this.tableEntrySubject,
       predicate: filesPredicate,
       object: new rdflib.NamedNode(file.uri),
       graph: this.storeOptions.sourceGraph,
     }]);
-
   }
 
+  @action
+  removeEntry() {
+    const propertyTriples = this.storeOptions.store.match(
+      this.tableEntrySubject,
+      undefined,
+      undefined,
+      this.storeOptions.sourceGraph
+    );
+
+    this.storeOptions.store.removeStatements(propertyTriples);
+
+    this.args.onRemove(this.tableEntrySubject)
+  }
+
+  @action
+  updateAddress(value) {
+    this.address = value;
+    this.validate();
+  }
+
+  validateAddress() {
+    this.addressErrors = [];
+
+    if (!this.address || this.isEmpty(this.address)) {
+      this.addressErrors.pushObject({
+        message: 'Adres is verplicht.',
+      });
+    }
+  }
+
+  validateBedroomCount() {
+    this.bedroomCountErrors = [];
+
+    if (parseInt(this.bedroomCount) <= 0) {
+      this.bedroomCountErrors.pushObject({
+        message: 'Het aantal slaapkamers moet groter zijn dan 0.',
+      });
+    }
+  }
+
+  @action
+  validate(e) {
+    if (e && typeof e.preventDefault === 'function') e.preventDefault();
+
+    const invalidRowTriple = [{
+      subject: this.tableSubject,
+      predicate: hasInvalidRowPredicate,
+      object: this.tableEntrySubject,
+      graph: this.storeOptions.sourceGraph,
+    }]
+
+    this.storeOptions.store.addAll(invalidRowTriple)
+
+    /**Validation start */
+    this.validateAddress();
+    this.validateBedroomCount();
+
+    if (this.addressErrors.length) return this.onUpdateRow();
+    if (this.bedroomCountErrors.length) return this.onUpdateRow();
+    /**Validation end */
+
+    this.updateTripleObject(
+      this.tableEntrySubject,
+      addressPredicate,
+      rdflib.literal(this.address, XSD('string'))
+    );
+
+    this.updateTripleObject(
+      this.tableEntrySubject,
+      bedroomCountPredicate,
+      rdflib.literal(this.bedroomCount, XSD('integer'))
+    );
+    this.updateTripleObject(
+      this.tableEntrySubject,
+      sharedInvoicePredicate,
+      rdflib.literal(this.sharedInvoice, XSD('boolean'))
+    );
+
+    this.storeOptions.store.removeStatements(invalidRowTriple);
+
+    return this.onUpdateRow();
+  }
 
   isEmpty(value) {
     return value.toString().length == 0;
+  }
+
+  isPositiveInteger(value) {
+    return parseInt(value) >= 0;
   }
 }
