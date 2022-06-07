fcf4fc4ee7e18b8e9d5d9651a1bb9600e5afda7b
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2022-06-07 16:13:44 +0200

    Replace the mock form setup with the real backend
    Co-authored-by: felix <ruiz.felixruiz@gmail.com>

--- app/components/public-services/details-form.js
+++ app/components/public-services/details-form.js
@@ -4,7 +4,7 @@ import { action } from '@ember/object';
 import Component from '@glimmer/component';
 import { ForkingStore } from '@lblod/ember-submission-form-fields';
 import rdflib from 'browser-rdflib';
-import { dropTask, task, timeout } from 'ember-concurrency';
+import { dropTask, task } from 'ember-concurrency';
 
 const FORM_GRAPHS = {
   formGraph: new rdflib.NamedNode('http://data.lblod.info/form'),
@@ -15,11 +15,6 @@ const FORM_GRAPHS = {
 const FORM = new rdflib.Namespace('http://lblod.data.gift/vocabularies/forms/');
 const RDF = new rdflib.Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#');
 
-// TODO: this isn't correct
-const SOURCE_NODE = new rdflib.NamedNode(
-  'http://ember-submission-form-fields/source-node'
-);
-
 export default class PublicServicesDetailsFormComponent extends Component {
   @service router;
 
@@ -27,13 +22,13 @@ export default class PublicServicesDetailsFormComponent extends Component {
   form;
   formStore;
   graphs = FORM_GRAPHS;
-  sourceNode = SOURCE_NODE;
   hasUnsavedChanges = false;
 
   constructor() {
     super(...arguments);
 
     this.loadForm.perform();
+    this.sourceNode = new rdflib.NamedNode(this.args.publicService.uri);
 
     // TODO: Set up a routeWillChange event handler so we can show a confirmation modal before leaving the tab
     // Even better would be to create a generic component which does this for us and which we can also use on other pages in the app.
@@ -42,17 +37,16 @@ export default class PublicServicesDetailsFormComponent extends Component {
 
   @task
   *loadForm() {
-    yield timeout(1000);
-
-    // TODO: retrieve the form data from the custom microservice
-    let [formTtl, metaTtl] = yield Promise.all([
-      fetchForm(this.args.formId),
-      fetchFormMeta(this.args.formId),
-    ]);
+    const {
+      form: formTtl,
+      meta: metaTtl,
+      source: sourceTtl,
+    } = yield fetchFormGraphs(this.args.publicService.id, this.args.formId);
 
     let formStore = new ForkingStore();
     formStore.parse(formTtl, FORM_GRAPHS.formGraph, 'text/turtle');
     formStore.parse(metaTtl, FORM_GRAPHS.metaGraph, 'text/turtle');
+    formStore.parse(sourceTtl, FORM_GRAPHS.sourceGraph, 'text/turtle');
 
     let form = formStore.any(
       undefined,
@@ -79,17 +73,18 @@ export default class PublicServicesDetailsFormComponent extends Component {
 
     // TODO: Do we need to disable deletion until while the save is running, similar to the subsidy module?
 
-    // TODO: persist the form state to the backend
-    console.log(
-      this.formStore.serializeDataWithAddAndDelGraph(
-        this.graphs.sourceGraph,
-        'application/n-triples'
-      )
+    let serializedData = this.formStore.serializeDataWithAddAndDelGraph(
+      this.graphs.sourceGraph,
+      'application/n-triples'
+    );
+
+    yield saveFormData(
+      this.args.publicService.id,
+      this.args.formId,
+      serializedData
     );
 
     yield this.args.publicService.reload();
-
-    yield timeout(1000);
   }
 
   willDestroy() {
@@ -99,20 +94,17 @@ export default class PublicServicesDetailsFormComponent extends Component {
   }
 }
 
-async function fetchForm(formName) {
-  let response = await fetch(getFormDataPath(formName, 'form.ttl'));
-  let ttl = await response.text();
-
-  return ttl;
+async function fetchFormGraphs(serviceId, formId) {
+  let response = await fetch(`/lpdc-management/${serviceId}/form/${formId}`);
+  return await response.json();
 }
 
-async function fetchFormMeta(formName) {
-  let response = await fetch(getFormDataPath(formName, 'meta.ttl'));
-  let ttl = await response.text();
-
-  return ttl;
-}
-
-function getFormDataPath(formName, fileName) {
-  return `/forms/${formName}/${fileName}`;
+async function saveFormData(serviceId, formId, formData) {
+  await fetch(`/lpdc-management/${serviceId}/form/${formId}`, {
+    method: 'PUT',
+    body: JSON.stringify(formData),
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+  });
 }
