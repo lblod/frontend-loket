a68d5fb827fb16408960256f28f719ba78ec8567
Unknown <achrafatauil@hotmail.com>
2022-06-30 11:33:49 +0200

    add module + setup beforeModel access check

--- app/routes/eredienst-mandatenbeheer.js
+++ app/routes/eredienst-mandatenbeheer.js
@@ -1,3 +1,15 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class EredienstMandatenbeheerRoute extends Route {}
+export default class EredienstMandatenbeheerRoute extends Route {
+  @service currentSession;
+  @service session;
+  @service router;
+
+  beforeModel(transition) {
+    this.session.requireAuthentication(transition, 'login');
+
+    if (!this.currentSession.canAccessEredienstMandatenbeheer)
+      this.router.transitionTo('index');
+  }
+}
