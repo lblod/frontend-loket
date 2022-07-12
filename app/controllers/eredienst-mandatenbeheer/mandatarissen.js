b5756479a66c0ffd9aff89ae0aea324a7f9f2960
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2022-07-12 16:00:28 +0200

    Fix the sorting and pagination of the mandatarissen table

---
+++ app/controllers/eredienst-mandatenbeheer/mandatarissen.js
@@ -0,0 +1,7 @@
+import Controller from '@ember/controller';
+
+export default class EredienstMandatenbeheerMandatarissenController extends Controller {
+  page = 0;
+  size = 10;
+  sort = 'is-bestuurlijke-alias-van.gebruikte-voornaam';
+}
