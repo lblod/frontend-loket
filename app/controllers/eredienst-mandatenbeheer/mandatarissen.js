6d9f7cc2d8633e0d4c0c5f56291de33c2caba9b0
felix <ruiz.felixruiz@gmail.com>
2022-07-14 19:16:30 +0200

    Update controller to handle bestuursperiode-selector

--- app/controllers/eredienst-mandatenbeheer/mandatarissen.js
+++ app/controllers/eredienst-mandatenbeheer/mandatarissen.js
@@ -1,7 +1,37 @@
 import Controller from '@ember/controller';
 
 export default class EredienstMandatenbeheerMandatarissenController extends Controller {
-  page = 0;
-  size = 10;
+  @service() router;
+
   sort = 'is-bestuurlijke-alias-van.gebruikte-voornaam';
+
+  @tracked mandatenbeheer;
+  @tracked filter = '';
+  @tracked page = 0;
+  @tracked size = 10;
+
+  get startDate() {
+    return this.mandatenbeheer.startDate;
+  }
+
+  get bestuursperioden() {
+    return this.mandatenbeheer.bestuursperioden;
+  }
+
+  get bestuurseenheid() {
+    return this.mandatenbeheer.bestuurseenheid;
+  }
+
+  get bestuursorganen() {
+    return this.mandatenbeheer.bestuursorganen;
+  }
+  @action
+  selectPeriod(startDate) {
+    this.router.transitionTo('eredienst-mandatenbeheer.mandatarissen', {
+      queryParams: {
+        page: 0,
+        startDate: startDate,
+      },
+    });
+  }
 }
