76c0373967c0d9ae45d37b08400ef36514e90a4b
felix <ruiz.felixruiz@gmail.com>
2022-07-14 19:15:21 +0200

    Include selector bestuursorganen in overview mandatarissen
    - also added some includes for person

--- app/routes/eredienst-mandatenbeheer/mandatarissen.js
+++ app/routes/eredienst-mandatenbeheer/mandatarissen.js
@@ -7,9 +7,35 @@ export default class EredienstMandatenbeheerMandatarissenRoute extends Route.ext
 ) {
   modelName = 'worship-mandatee';
 
-  mergeQueryOptions() {
-    return {
-      include: ['type-half', 'bekleedt'].join(','),
+  beforeModel() {
+    const mandatenbeheer = this.modelFor('eredienst-mandatenbeheer');
+    this.mandatenbeheer = mandatenbeheer;
+  }
+
+  mergeQueryOptions(params) {
+    const bestuursorganenIds = this.mandatenbeheer.bestuursorganen.map((o) =>
+      o.get('id')
+    );
+
+    const queryParams = {
+      sort: params.sort,
+      filter: {
+        bekleedt: {
+          'bevat-in': {
+            id: bestuursorganenIds.join(','),
+          },
+        },
+      },
+      include: [
+        'type-half',
+        'bekleedt',
+        'is-bestuurlijke-alias-van',
+        'bekleedt.bestuursfunctie',
+      ].join(','),
     };
+
+
+    return queryParams;
+  }
   }
 }
