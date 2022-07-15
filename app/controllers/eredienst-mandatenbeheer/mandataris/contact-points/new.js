80a6f6500be0d59571ea86454c4d1ea138d748f8
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2022-07-15 12:55:03 +0200

    New contact page improvements
    - reset the unsaved data after leaving the page
    - disable the submit button when saving
    - template formatting
    - only try to save the address if one was selected

--- app/controllers/eredienst-mandatenbeheer/mandataris/contact-points/new.js
+++ app/controllers/eredienst-mandatenbeheer/mandataris/contact-points/new.js
@@ -4,43 +4,39 @@ import { task } from 'ember-concurrency-decorators';
 import { inject as service } from '@ember/service';
 
 export default class EredienstMandatenbeheerMandatarisContactPointsNewController extends Controller {
-  @service() store;
-  @service() router;
+  @service store;
+  @service router;
 
   @tracked adres;
   @tracked telephone;
   @tracked email;
 
   @task
-  *updateAdres(adres) {
-    this.adres = yield adres;
-  }
-
-  @task
   *submit() {
-    yield this.adres;
-    const addresses = yield this.store.query('adres', {
-      filter: {
-        'volledig-adres': this.adres.volledigAdres,
-      },
-    });
-
-    let newAdres;
-
-    if (addresses.length == 0) {
-      newAdres = this.store.createRecord('adres', this.adres);
-      yield newAdres.save();
-    }
-
     const contactPoint = this.store.createRecord('contact-punt', {
       email: this.email,
       telefoon: this.telephone,
-      adres: newAdres || addresses.firstObject,
       mandataris: this.model,
     });
 
-    yield contactPoint.save();
+    if (this.adres) {
+      const addresses = yield this.store.query('adres', {
+        filter: {
+          'volledig-adres': this.adres.volledigAdres,
+        },
+      });
 
-    this.router.transitionTo('eredienst-mandatenbeheer.mandataris.edit');
+      let newAdres;
+
+      if (addresses.length == 0) {
+        newAdres = this.store.createRecord('adres', this.adres);
+        yield newAdres.save();
+      }
+
+      contactPoint.adres = newAdres || addresses.firstObject;
+    }
+
+    yield contactPoint.save();
+    yield this.router.transitionTo('eredienst-mandatenbeheer.mandataris.edit');
   }
 }
--- app/routes/eredienst-mandatenbeheer/mandataris/contact-points/new.js
+++ app/routes/eredienst-mandatenbeheer/mandataris/contact-points/new.js
@@ -1,3 +1,15 @@
 import Route from '@ember/routing/route';
 
-export default class EredienstMandatenbeheerMandatarisContactPointsNewRoute extends Route {}
+export default class EredienstMandatenbeheerMandatarisContactPointsNewRoute extends Route {
+  model() {
+    return this.modelFor('eredienst-mandatenbeheer.mandataris');
+  }
+
+  resetController(controller) {
+    super.resetController(...arguments);
+
+    controller.adres = null;
+    controller.telephone = '';
+    controller.email = '';
+  }
+}
