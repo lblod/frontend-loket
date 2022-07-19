e5f643101f5870d58ebb7b7e9e996214d49d18d0
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2022-07-19 11:34:32 +0200

    Small mandataris edit page improvements
    - disable the save button while it's already running
    - use a link for navigation

--- app/controllers/eredienst-mandatenbeheer/mandataris/edit.js
+++ app/controllers/eredienst-mandatenbeheer/mandataris/edit.js
@@ -2,6 +2,7 @@ import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { dropTask } from 'ember-concurrency';
 
 export default class EredienstMandatenbeheerMandatarisEditController extends Controller {
   @service currentSession;
@@ -20,13 +21,8 @@ export default class EredienstMandatenbeheerMandatarisEditController extends Con
     this.model[attributeName] = date;
   }
 
-  @action
-  onSave() {
-    this.model.save();
-  }
-
-  @action
-  onCancel() {
-    this.router.transitionTo('eredienst-mandatenbeheer.mandatarissen');
+  @dropTask
+  *save() {
+    yield this.model.save();
   }
 }
