9d84b639cf313f05b6a48e3801e3f69423486f9c
felix <ruiz.felixruiz@gmail.com>
2022-08-04 12:23:06 +0200

    Make sure all models follow inheritance pattern

--- app/models/minister-position.js
+++ app/models/minister-position.js
@@ -1,6 +1,7 @@
 import Model, { belongsTo, hasMany } from '@ember-data/model';
+import Post from './post';
 
-export default class MinisterPositionModel extends Model {
+export default class MinisterPositionModel extends Post {
   @belongsTo('minister-position-function') function;
   @belongsTo('worship-administrative-unit', { inverse: null })
   worshipService;
--- app/models/minister.js
+++ app/models/minister.js
@@ -1,7 +1,8 @@
 import Model, { hasMany, belongsTo } from '@ember-data/model';
+import AgentInPosition from './agent-in-position';
 
-export default class MinisterModel extends Model {
-  @belongsTo('minister-position') position;
+export default class MinisterModel extends AgentInPosition {
+  @belongsTo('minister-position') ministerPosition;
   @belongsTo('financing-code') financing;
 
   @hasMany('minister-condition') conditions;
