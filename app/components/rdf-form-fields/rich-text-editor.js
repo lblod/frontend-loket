a7b2f871c76c25a0ddaaf854f0085d6f93611847
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2022-08-08 09:54:47 +0200

    Use the `contentChanged` event
    This replaces the custom `focusOut` based setup with the editor's own event system which should reduce false positive updates.

--- app/components/rdf-form-fields/rich-text-editor.js
+++ app/components/rdf-form-fields/rich-text-editor.js
@@ -1,9 +1,11 @@
 import { action } from '@ember/object';
 import { guidFor } from '@ember/object/internals';
 import SimpleInputFieldComponent from '@lblod/ember-submission-form-fields/components/rdf-input-fields/simple-value-input-field';
+import { restartableTask } from 'ember-concurrency';
+import { timeout } from 'node_modules/ember-concurrency/addon/index';
 
 export default class RdfFormFieldsRichTextEditorComponent extends SimpleInputFieldComponent {
-  editor;
+  editorController;
   inputId = 'richtext-' + guidFor(this);
 
   get editorOptions() {
@@ -27,19 +29,38 @@ export default class RdfFormFieldsRichTextEditorComponent extends SimpleInputFie
   }
 
   @action
-  handleRdfaEditorInit(editor) {
-    this.editor = editor;
-    editor.setHtmlContent(this.value ? this.value : '');
+  handleRdfaEditorInit(editorController) {
+    this.editorController = editorController;
+
+    if (this.value) {
+      console.log('value', this.value);
+      editorController.setHtmlContent(this.value);
+    }
+
+    // We attach the event handler _after_ setting the initial value
+    // so the setting doesn't trigger the event itself.
+    editorController.onEvent('contentChanged', () => {
+      console.log('contentChanged', this.inputId);
+      this.handleContentChanged.perform();
+    });
+  }
+
+  @restartableTask
+  *handleContentChanged() {
+    yield timeout(300);
+
+    this.updateValue();
   }
 
   @action
-  updateValue(e) {
-    if (e && typeof e.preventDefault === 'function') e.preventDefault();
-
+  updateValue() {
     //rdfaEditor setup is async, updateValue may be called before it is set
-    if (this.editor) {
-      const editorValue = this.editor.htmlContent;
-      super.updateValue(editorValue);
+    if (this.editorController) {
+      const editorValue = this.editorController.htmlContent;
+
+      if (editorValue !== this.value) {
+        super.updateValue(editorValue);
+      }
     }
   }
 }
