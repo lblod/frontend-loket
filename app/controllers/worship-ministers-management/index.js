07b776c3ad02d8091319de453173159145bb26f6
David <david.journo.31@gmail.com>
2022-08-08 15:48:25 +0200

    WIP worshipMinistersManagement Index
    - Issue with the edit route
    
    - Todo: sorting
    
    - Todofix : start & einde date are undefined

--- app/controllers/worship-ministers-management/index.js
+++ app/controllers/worship-ministers-management/index.js
@@ -1,12 +1,17 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
-export default class MinisterManagementIndexController extends Controller {
+export default class WorshipMinistersManagementIndexController extends Controller {
   @service router;
+  @service store;
+
+  @tracked page = 0;
+  @tracked size = 20;
 
   @action
-  newMinister() {
-    this.router.transitionTo('minister-management.new');
+  addNewMinister() {
+    this.router.transitionTo('worship-ministers-management.new');
   }
 }
--- app/routes/worship-ministers-management/index.js
+++ app/routes/worship-ministers-management/index.js
@@ -3,24 +3,11 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import DataTableRouteMixin from 'ember-data-table/mixins/route';
 
-// ministers
-export default class MinisterManagementIndexRoute extends Route.extend(
+export default class WorshipMinistersManagementIndexRoute extends Route.extend(
   DataTableRouteMixin
 ) {
   @service session;
   @service store;
 
   modelName = 'minister';
-
-  // beforeModel(transition) {
-  //   this.session.requireAuthentication(transition, 'login');
-
-  //   if (!this.currentSession.canAccessBedienarenbeheer)
-  //     this.router.transitionTo('index');
-  // }
-
-  // async model() {
-  //   console.log(this.store.findAll('persoon'));
-  //   return this.store.findAll('agent-in-position');
-  // }
 }
