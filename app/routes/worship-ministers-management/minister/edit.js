54972bbeb810e69dacf5d1f0e942d3fbae0923db
David <david.journo.31@gmail.com>
2022-08-10 15:20:52 +0200

    WIP worshipMinister `edit` page
    - Todo : getting contacts into contactList

--- app/routes/worship-ministers-management/minister/edit.js
+++ app/routes/worship-ministers-management/minister/edit.js
@@ -1,44 +1,26 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
-// import {
-//   CONTACT_TYPE,
-//   findPrimaryContactPoint,
-// } from 'frontend-loket/models/contact-punt';
 
 export default class WorshipMinistersManagementMinisterEditRoute extends Route {
   @service store;
 
-  queryParams = ['ministerId'];
+  queryParams = ['minister_id'];
   @tracked ministerId = '';
 
-  // async beforeModel() {
-  //   // const bedienarenbeheer = this.modelFor('worship-ministers-management');
-  //   // this.bestuursorganen = bedienarenbeheer.naam;
-  //   // const persoon = await this.store.findRecord(
-  //   //   'minister',
-  //   //   params.to.params.bedienaar_id
-  //   // );
-  //   // console.log(persoon);
-  //   // let positionContacts = await bedienaar.contacts;
-  //   // this.selectedContact = findPrimaryContactPoint(positionContacts);
-  //   // this.contacts = await this.store.query('contact-punt', {
-  //   //   'filter[agents-in-position][is-bestuurlijke-alias-van][id]': persoon.id,
-  //   //   'filter[type]': CONTACT_TYPE.PRIMARY,
-  //   //   include: 'adres,secondary-contact-point',
-  //   // });
-  // }
+  async beforeModel() {
+    // TODO: Adding contacts to the table
+  }
 
   model(params) {
-    return this.store.findRecord('minister', params.bedienaar_id);
+    return this.store.findRecord('minister', params.minister_id);
   }
 
   // setupController(controller) {
   //   super.setupController(...arguments);
 
-  //   // controller.bestuursorganen = this.bestuursorganen;
-  //   // controller.contactList = this.contacts;
-  //   // controller.selectedContact = this.selectedContact;
+  //   controller.contactList = this.contacts;
+  //   controller.selectedContact = this.selectedContact;
   // }
 
   // resetController(controller) {
