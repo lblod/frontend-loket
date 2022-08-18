9244a59f6d1f04cec534ebea2a4ce062d3bef588
felix <ruiz.felixruiz@gmail.com>
2022-08-18 12:11:34 +0200

    Include status (again)

--- app/routes/public-services/index.js
+++ app/routes/public-services/index.js
@@ -29,6 +29,7 @@ export default class PublicServicesIndexRoute extends Route {
   *loadPublicServicesTask({ search, page, sort }) {
     let query = {
       'page[number]': page,
+      include: 'status',
       // TODO: Filter the results so only public services for the current organization are returned
     };
 
