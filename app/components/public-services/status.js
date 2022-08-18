5fe6bb687d338f80bd87fb0dff59384e19415748
felix <ruiz.felixruiz@gmail.com>
2022-08-18 12:11:52 +0200

    Refactor the status pill (+ remove lifcycleStatus)

--- app/components/public-services/status.js
+++ app/components/public-services/status.js
@@ -1,34 +1,23 @@
 import Component from '@glimmer/component';
 
-const LIFECYCLE_STATUS = {
-  CONCEPT:
-    'http://lblod.data.gift/concepts/79a52da4-f491-4e2f-9374-89a13cde8ecd',
-  ACTIVE:
-    'http://data.lblod.info/id/concept/ipdc-thema/27d784f8-e7ea-4f18-88f8-02d427033009',
-  NOT_ACTIVE:
-    'http://data.lblod.info/id/concept/ipdc-thema/12873bfb-d2cc-4cc5-9255-1bf0ca2747f9',
-};
+const DOCUMENT_STATUS = {
 
-const LABEL = {
-  'http://lblod.data.gift/concepts/79a52da4-f491-4e2f-9374-89a13cde8ecd':
-    'Ontwerp',
-  'http://data.lblod.info/id/concept/ipdc-thema/27d784f8-e7ea-4f18-88f8-02d427033009':
-    'Actief',
-  'http://data.lblod.info/id/concept/ipdc-thema/12873bfb-d2cc-4cc5-9255-1bf0ca2747f9':
-    'Stopgezet',
-};
+  'http://lblod.data.gift/concepts/79a52da4-f491-4e2f-9374-89a13cde8ecd': {
+    skin: 'warning',
+    label: 'ontwerp',
+  },
 
-const PUBLICATION_STATUS_SKIN = {
-  [LIFECYCLE_STATUS.ACTIVE]: 'success',
-  [LIFECYCLE_STATUS.NOT_ACTIVE]: 'error',
-  [LIFECYCLE_STATUS.CONCEPT]: 'warning',
+  'http://lblod.data.gift/concepts/9bd8d86d-bb10-4456-a84e-91e9507c374c': {
+    skin: 'success',
+    label: 'verzonden',
+  },
 };
 
 export default class Status extends Component {
   get label() {
-    return LABEL[this.args.uri];
+    return DOCUMENT_STATUS[this.args.uri].label;
   }
   get statusSkin() {
-    return PUBLICATION_STATUS_SKIN[this.args.uri];
+    return DOCUMENT_STATUS[this.args.uri].skin;
   }
 }
