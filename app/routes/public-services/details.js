4dc1cb5d5fd6184f9a15c27ccf984816c42c4880
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2022-08-23 13:57:47 +0200

    Add a custom `ConceptSelector` field
    This field works similar to the `ConceptSchemeSelector` and `ConceptSchemeMultiSelector` fields, but with several improvements:
    
    - it loads the data when the component is rendered or when the user searches, which improves the form rendering
    - it allows preloading the options in case there aren't that many so searching isn't beneficial
    - it supports both single and multi select modes by adding some configuration

--- app/routes/public-services/details.js
+++ app/routes/public-services/details.js
@@ -1,8 +1,9 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import { loadPublicServiceDetails } from 'frontend-loket/utils/public-services';
-import RichTextEditor from 'frontend-loket/components/rdf-form-fields/rich-text-editor';
 import { registerFormFields } from '@lblod/ember-submission-form-fields';
+import ConceptSelector from 'frontend-loket/components/rdf-form-fields/concept-selector';
+import RichTextEditor from 'frontend-loket/components/rdf-form-fields/rich-text-editor';
+import { loadPublicServiceDetails } from 'frontend-loket/utils/public-services';
 
 export default class PublicServicesDetailsRoute extends Route {
   @service store;
@@ -26,6 +27,10 @@ export default class PublicServicesDetailsRoute extends Route {
       {
         displayType: 'http://lblod.data.gift/display-types/richText',
         edit: RichTextEditor,
+      },
+      {
+        displayType: 'http://lblod.data.gift/display-types/conceptSelector',
+        edit: ConceptSelector,
       },
     ]);
   }
