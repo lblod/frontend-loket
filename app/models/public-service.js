0c478f5cf13e006e517986fcd22ca54b0ae710b1
felix <ruiz.felixruiz@gmail.com>
2022-08-24 08:23:24 +0200

    Attempt at toggling icons in the overview table when public service
    is sent.

--- app/models/public-service.js
+++ app/models/public-service.js
@@ -4,4 +4,8 @@ import ConceptualPublicServiceModel from './conceptual-public-service';
 export default class PublicServiceModel extends ConceptualPublicServiceModel {
   @belongsTo('conceptual-public-service', { inverse: null })
   concept;
+
+  get isSent() {
+    return this.status.get('uri') === 'http://lblod.data.gift/concepts/9bd8d86d-bb10-4456-a84e-91e9507c374c';
+  }
 }
