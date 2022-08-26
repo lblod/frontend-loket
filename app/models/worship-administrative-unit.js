18eb169dbf5877e35398f7a93046f6c4e518cd5f
cecemel <ruiz.felixruiz@gmail.com>
2022-08-26 10:50:27 +0200

    Update models (class hierarchy) (#173)

--- app/models/worship-administrative-unit.js
+++ app/models/worship-administrative-unit.js
@@ -1,6 +1,7 @@
-import Model, { belongsTo, hasMany } from '@ember-data/model';
+import { belongsTo, hasMany } from '@ember-data/model';
+import BestuurseenheidModel from './bestuurseenheid';
 
-export default class WorshipAdministrativeUnitModel extends Model {
+export default class WorshipAdministrativeUnitModel extends BestuurseenheidModel {
   @belongsTo('recognized-worship-type') recognizedWorshipType;
 
   @hasMany('minister-position', { inverse: null }) ministerPositions;
---
+++ app/models/worship-service.js
@@ -0,0 +1,3 @@
+import WorshipAdminstrativeUnitModel from './worship-administrative-unit';
+
+export default class WorshipServiceModel extends WorshipAdminstrativeUnitModel {}
