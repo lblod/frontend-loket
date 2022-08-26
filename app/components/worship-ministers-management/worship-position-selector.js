dcac7592a5151510a655dc33e07c572a101209c1
David <david.journo.31@gmail.com>
2022-08-26 19:19:01 +0200

    MinisterPositionSelector Component

---
+++ app/components/worship-ministers-management/worship-position-selector.js
@@ -0,0 +1,3 @@
+/* eslint-disable ember/no-empty-glimmer-component-classes */
+import Component from '@glimmer/component';
+export default class WorshipPositionSelectorComponent extends Component {}
