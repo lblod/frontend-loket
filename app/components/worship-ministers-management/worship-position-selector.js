c988e95c8360b49cc69fc2cb49e60e5a9ae26b9e
David <david.journo.31@gmail.com>
2022-08-29 09:16:10 +0200

    Deleting duplicate `PositionSelect` component

--- app/components/worship-ministers-management/worship-position-selector.js
+++
@@ -1,3 +0,0 @@
-/* eslint-disable ember/no-empty-glimmer-component-classes */
-import Component from '@glimmer/component';
-export default class WorshipPositionSelectorComponent extends Component {}
