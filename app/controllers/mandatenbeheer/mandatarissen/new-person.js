f4376c18c4b7c1d7cbc6102928cbaaad097c152a
David <david.journo.31@gmail.com>
2022-10-20 16:17:02 +0200

    Adding prefill for `Mandatenbeheer`

--- app/controllers/mandatenbeheer/mandatarissen/new-person.js
+++ app/controllers/mandatenbeheer/mandatarissen/new-person.js
@@ -1,9 +1,23 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 
 export default class MandatenbeheerMandatarissenNewPersonController extends Controller {
   @service() router;
+
+  queryParams = ['voornaam', 'achternaam', 'rijksregisternummer'];
+  @tracked voornaam;
+  @tracked achternaam;
+  @tracked rijksregisternummer;
+
+  get personPrefilledValues() {
+    return {
+      voornaam: this.voornaam,
+      familienaam: this.achternaam,
+      rijksregisternummer: this.rijksregisternummer,
+    };
+  }
 
   @action
   onCreate(user) {
