927269638317780df734b052d0603c74c1053171
David <david.journo.31@gmail.com>
2022-10-27 14:16:20 +0200

    WIP if persoon has contact, preselect the contact

--- app/routes/worship-ministers-management/new.js
+++ app/routes/worship-ministers-management/new.js
@@ -17,7 +17,7 @@ export default class WorshipMinisterManagementNewRoute extends Route {
 
   async model({ personId }) {
     if (personId) {
-      console.log('step 3');
+      console.log('Router : step 3');
       let person = await this.store.findRecord('persoon', personId, {
         backgroundReload: false,
       });
@@ -29,19 +29,21 @@ export default class WorshipMinisterManagementNewRoute extends Route {
         'filter[type]': CONTACT_TYPE.PRIMARY,
         include: 'adres,secondary-contact-point',
       });
-
-      console.log('contacts', contacts);
+      // Is there an other way to check without accessing private '.content' ?
+      if (contacts.content.length !== 0) {
+        // Pre-select existing contact for this person;
+        worshipMinister.contacts = contacts;
+      }
+      console.log('Router : contacts is valid', contacts.isValid);
+      console.log('Router : contacts', contacts);
 
       // Before model ?
-      let positionContacts = await worshipMinister.contacts;
+      let positionContacts = await worshipMinister.contacts; // useless since we create a new worship minister
+      console.log('Router : Position contacts', positionContacts);
       // We create a positionContacts but empty since it's a new worship minister
-      // When we found contacts for this person do we fill it ?
-      // if (worshipMinister.contacts.content) {
-      //   worshipMinister.contacts = contacts;
-      // }
       this.selectedContact = findPrimaryContactPoint(positionContacts);
 
-      console.log('findprimarycontact', this.selectedContact);
+      console.log('Router : findprimarycontact', this.selectedContact);
 
       return {
         worshipMinister,
@@ -50,7 +52,7 @@ export default class WorshipMinisterManagementNewRoute extends Route {
         contacts,
       };
     }
-    console.log('step 1');
+    console.log('Router : step 1');
     return {};
   }
 
