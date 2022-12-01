5ee80c2f081b349afb3521d22a22592818105512
David <david.journo.31@gmail.com>
2022-12-01 12:47:41 +0100

    Adding util functions
    - combineFullAddress(address) will be used to contruct the volledigAdres when the adres model is valid.
    
    - resetAddressAttributes() might not use it.
    
    - updateAddressAttributes()  Issue : breaks the relationship between two models but not sure.

--- app/models/adres.js
+++ app/models/adres.js
@@ -12,8 +12,33 @@ export default class AdresModel extends Model {
   @attr adresRegisterUri;
 }
 
+export function combineFullAddress(address) {
+  let fullStreet = `${address.straatnaam || ''} ${address.huisnummer || ''} ${
+    address.busnummer || ''
+  }`.trim();
+
+  let muncipalityInformation = `${address.postcode || ''} ${
+    address.gemeentenaam || ''
+  }`.trim();
+
+  if (fullStreet && muncipalityInformation) {
+    return `${fullStreet}, ${muncipalityInformation}`;
+  } else if (fullStreet) {
+    return fullStreet;
+  } else if (muncipalityInformation) {
+    return muncipalityInformation;
+  } else {
+    return null;
+  }
+}
+
 export async function isValidAdres(address) {
-  const requiredFields = ['straat', 'huisnummer', 'postcode', 'gemeentenaam'];
+  const requiredFields = [
+    'straatnaam',
+    'huisnummer',
+    'postcode',
+    'gemeentenaam',
+  ];
 
   requiredFields.forEach((field) => {
     let value = address[field];
@@ -24,4 +49,32 @@ export async function isValidAdres(address) {
   });
 
   return address.isValid;
+}
+
+export function resetAddressAttributes(address) {
+  address.setProperties({
+    busnummer: null,
+    huisnummer: null,
+    straatnaam: null,
+    postcode: null,
+    gemeentenaam: null,
+    land: null,
+    adresRegisterId: null,
+    adresRegisterUri: null,
+    volledigAdres: null,
+  });
+}
+
+export function updateAddressAttributes(address) {
+  address.setProperties({
+    busnummer: null,
+    huisnummer: address.huisnummer,
+    straatnaam: address.straatnaam,
+    postcode: address.postcode,
+    gemeentenaam: address.gemeentenaam,
+    land: null,
+    adresRegisterId: null,
+    adresRegisterUri: null,
+    volledigAdres: address.volledigAdres,
+  });
 }
