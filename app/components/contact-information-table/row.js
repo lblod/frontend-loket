a33d50b5a25bac2cbef31de1b1bde26621c142f9
David <david.journo.31@gmail.com>
2022-12-01 14:27:15 +0100

    Trying to resolve the adres issue
    See BUG comment.

--- app/components/contact-information-table/row.js
+++ app/components/contact-information-table/row.js
@@ -1,12 +1,9 @@
 import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
 
 export default class ContactInformationTableRowComponent extends Component {
   @service store;
-
-  @tracked manualInputMode = false;
 
   @action
   async handleAdresChange(adres) {
@@ -19,10 +16,22 @@ export default class ContactInformationTableRowComponent extends Component {
 
       let newAdres;
 
+      // Here it creates a new address even if the selector does have an address
       if (addresses.length == 0) {
+        /* BUG: Steps to reproduce
+        1) Select a person in the list from a new bedienaar
+        2) Use the address selector to search for an address and select one
+        3) newAdres is somehow empty when it's created (even if we await for the record)
+        */
+        console.log('adres (does have data)', adres);
+        console.log(
+          'adres arg (proxy) might be async/await relationship issue ?',
+          this.args.contact.adres
+        );
+        console.log('createRecord but does not populate the adres model');
         newAdres = this.store.createRecord('adres', adres);
+        console.log('it should contain data', newAdres);
       }
-
       this.args.contact.adres = newAdres || addresses.firstObject;
     } else {
       this.args.contact.adres = null;
@@ -30,10 +39,5 @@ export default class ContactInformationTableRowComponent extends Component {
 
     // Updating a relationship value doesn't seem to clear the corresponding error messages, so we do it manually
     this.args.contact.errors.remove('adres');
-  }
-
-  @action
-  toggleInputMode() {
-    this.manualInputMode = !this.manualInputMode;
   }
 }
