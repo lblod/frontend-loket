250d52c333eea6caf8bfbfbb12922259fc0b0e71
David <david.journo.31@gmail.com>
2022-12-05 12:05:37 +0100

    Removing comments
    This was an ember-inspector issue, disabling it will solve the problem.

--- app/components/contact-information-table/row.js
+++ app/components/contact-information-table/row.js
@@ -15,22 +15,8 @@ export default class ContactInformationTableRowComponent extends Component {
       });
 
       let newAdres;
-
-      // Here it creates a new address even if the selector does have an address
       if (addresses.length == 0) {
-        /* BUG: Steps to reproduce
-        1) Select a person in the list from a new bedienaar
-        2) Use the address selector to search for an address and select one
-        3) newAdres is somehow empty when it's created (even if we await for the record)
-        */
-        console.log('adres (does have data)', adres);
-        console.log(
-          'adres arg (proxy) might be async/await relationship issue ?',
-          this.args.contact.adres
-        );
-        console.log('createRecord but does not populate the adres model');
         newAdres = this.store.createRecord('adres', adres);
-        console.log('it should contain data', newAdres);
       }
       this.args.contact.adres = newAdres || addresses.firstObject;
     } else {
