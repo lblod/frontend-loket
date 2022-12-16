959a9b275f0c51d49076291466fdcf575d19018a
David <david.journo.31@gmail.com>
2022-12-16 13:53:38 +0100

    Adjusting `Busnummer` placeholder value
    Replaced the placeholder “Geen busnummer beschikbaar bij dit adres” to “/” to create more space

--- app/components/adressenregister-busnumber-selector.js
+++ app/components/adressenregister-busnumber-selector.js
@@ -2,8 +2,6 @@ import Component from '@glimmer/component';
 
 export default class AdressenregisterBusnumberSelectorComponent extends Component {
   get placeholder() {
-    return this.args.disabled
-      ? 'Geen busnummer beschikbaar bij dit adres.'
-      : '';
+    return this.args.disabled ? '/' : '';
   }
 }
