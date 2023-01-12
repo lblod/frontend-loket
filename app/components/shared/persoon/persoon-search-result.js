ff1008237d49ab7fee5ab6a1adef2780d2b07b89
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2023-01-12 13:57:16 +0100

    Remove unused code from the `PersoonSearchResult` component

--- app/components/shared/persoon/persoon-search-result.js
+++ app/components/shared/persoon/persoon-search-result.js
@@ -1,20 +1,13 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class SharedPersoonPersoonSearchResultComponent extends Component {
   @service currentSession;
-  @tracked showDetails = false;
 
   @action
   select() {
     this.args.onSelect(this.args.persoon);
-  }
-
-  @action
-  toggleDetails() {
-    this.showDetails = !this.showDetails;
   }
 
   get mailContent() {
