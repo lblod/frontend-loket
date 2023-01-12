30f2e3fcd8ea2eded6aba0f56d4c81d5eb895860
David <david.journo.31@gmail.com>
2023-01-12 14:37:45 +0100

    Refactoring detail route
    contacts are shown when a relationship between a worship-mandatee is set.

--- app/routes/eredienst-mandatenbeheer/mandataris/details.js
+++ app/routes/eredienst-mandatenbeheer/mandataris/details.js
@@ -1,36 +1,6 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
-import {
-  CONTACT_TYPE,
-  findPrimaryContactPoint,
-} from 'frontend-loket/models/contact-punt';
 export default class EredienstMandatenbeheerMandatarisDetailsRoute extends Route {
-  @service currentSession;
-  @service store;
-
-  async beforeModel() {
-    const mandataris = await this.modelFor(
-      'eredienst-mandatenbeheer.mandataris'
-    );
-    const persoon = await mandataris.isBestuurlijkeAliasVan;
-
-    let positionContacts = await mandataris.contacts;
-    this.selectedContact = findPrimaryContactPoint(positionContacts);
-
-    this.contacts = await this.store.query('contact-punt', {
-      'filter[agents-in-position][is-bestuurlijke-alias-van][id]': persoon.id,
-      'filter[type]': CONTACT_TYPE.PRIMARY,
-      include: 'adres,secondary-contact-point',
-    });
-  }
-
   model() {
     return this.modelFor('eredienst-mandatenbeheer.mandataris');
-  }
-
-  setupController(controller) {
-    super.setupController(...arguments);
-
-    controller.contactList = this.contacts;
   }
 }
