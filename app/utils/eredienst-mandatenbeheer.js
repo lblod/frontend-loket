80b956ff20ce9df4f26f9944de2b76830a6c1b27
David <david.journo.31@gmail.com>
2023-02-20 17:47:18 +0100

    Adding new util functions
    This allows to get the active time periodes and check when needed if the dates provided by the user falls outside of the active time periode.

--- app/utils/eredienst-mandatenbeheer.js
+++ app/utils/eredienst-mandatenbeheer.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 /*
  * Assuming bestuursorganen in tijd are passed linked to mandates.
  * This function sets the expectedEndDate from the array of bestuursorganenInTijd where records are ordered by DESC,
@@ -20,4 +22,88 @@ export async function setExpectedEndDate(store, mandataris, mandaat) {
     );
     mandataris.expectedEndDate = plannedEndDates[0];
   }
+}
+
+/**
+ * Gets `activeTimePeriodes` for the selected `mandate`.
+ * @param {Object} mandate Mandate record
+ * @param {Object} store Ember store
+ * @returns {Object} Active time periodes
+ */
+export async function getActiveTimePeriodes(mandate, store) {
+  let bestuursorganenInTijd = await store.query('bestuursorgaan', {
+    'filter[bevat][:uri:]': mandate.uri,
+    sort: 'binding-start',
+  });
+
+  let activeTimePeriodes = bestuursorganenInTijd.map((bestuursorgaan) => {
+    return {
+      bindingStart: bestuursorgaan.bindingStart,
+      bindingEinde: bestuursorgaan.bindingEinde,
+    };
+  });
+
+  return activeTimePeriodes;
+}
+
+/**
+ * Checks the mandate time periode limit, if it falls outside, we send a warning message to the user.
+ * Lifetime mandates does not trigger warning messages.
+ * @param {Object} mandate Mandate record
+ * @param {Object} store Ember store
+ * @param {Object} startDate Mandate starting date
+ * @param {Object} endDate Mandate ending date
+ * @returns {Object} Corresponding warning message with the {startDate} and {endDate} cases.
+ */
+export async function checkMandateTimePeriodeLimit(
+  mandate,
+  store,
+  startDate,
+  endDate
+) {
+  if (!(await mandate)) {
+    return; // Handling the case where we create a new worship-mandatee.
+  }
+  const activeTimePeriodes = await getActiveTimePeriodes(mandate, store);
+  let activeTimePeriodeLimitStart;
+  let activeTimePeriodeLimitEnd;
+  let warningMessages = {};
+  const { label } = await mandate.bestuursfunctie;
+  // Maybe we should use uri instead
+  switch (label) {
+    case 'Voorzitter van het bestuur van de eredienst':
+    case 'Penningmeester van het bestuur van de eredienst':
+    case 'Secretaris van het bestuur van de eredienst':
+      activeTimePeriodeLimitStart = activeTimePeriodes[0].bindingStart;
+      activeTimePeriodeLimitEnd = activeTimePeriodes[0].bindingEinde;
+      break;
+    case 'Bestuurslid van het bestuur van de eredienst (Grote Helft)':
+    case 'Bestuurslid van het bestuur van de eredienst (Kleine Helft)':
+      activeTimePeriodeLimitStart = activeTimePeriodes[0].bindingStart;
+      activeTimePeriodeLimitEnd = activeTimePeriodes[1].bindingEinde;
+      break;
+    case 'Bestuurslid (van rechtswege) van het bestuur van de eredienst':
+      return; // no warnings needed for lifetime mandate
+    default:
+    // Handle unexpected ministerType
+  }
+
+  if (moment(startDate).isBefore(activeTimePeriodeLimitStart)) {
+    warningMessages.startDateMessage = `Deze startdatum ${moment(
+      startDate
+    ).format(
+      'DD-MM-YYYY'
+    )} valt buiten het start van deze actieve tijdsperiode ${moment(
+      activeTimePeriodeLimitStart
+    ).format('DD-MM-YYYY')}.`;
+  }
+  if (moment(endDate).isAfter(activeTimePeriodeLimitEnd)) {
+    warningMessages.endDateMessage = `Deze einddatum ${moment(endDate).format(
+      'DD-MM-YYYY'
+    )} valt buiten het einde van deze actieve tijdsperiode ${moment(
+      activeTimePeriodeLimitEnd
+    ).format('DD-MM-YYYY')}.`;
+  }
+
+  return warningMessages;
 }
