f1913a6bb08f41caae9f3c4da89d7a8efcac9313
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2023-03-15 12:02:14 +0100

    Ignore the remaining classic component linting errors
    These aren't a blocker for the Ember v4 update.

--- app/components/mandatenbeheer/mandataris-duplicate-selector.js
+++ app/components/mandatenbeheer/mandataris-duplicate-selector.js
@@ -1,3 +1,5 @@
+/* eslint-disable ember/no-classic-components, ember/no-classic-classes, ember/require-tagless-components, ember/no-component-lifecycle-hooks, ember/no-actions-hash */
+
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 
