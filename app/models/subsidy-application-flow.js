9388500c3cf024f2896ce5ab96dbb047d1f535e8
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2023-03-16 14:52:01 +0100

    Fix an issue that occurred going to the subsidy details page
    The details page requires some extra relationships to be loaded. If you start from the overview page, the base record is already loaded and Ember Data will return it from the cache. Some code depends on this data being loaded in the model hooks before being rendered. We now force a reload so that the model hooks properly await the full data set.

--- app/models/subsidy-application-flow.js
+++ app/models/subsidy-application-flow.js
@@ -3,11 +3,16 @@ import Model, { belongsTo, hasMany } from '@ember-data/model';
 
 export default class SubsidyApplicationFlowModel extends Model {
   @belongsTo('subsidy-measure-offer-series') subsidyMeasureOfferSeries;
-  @belongsTo('subsidy-application-flow-step') firstApplicationStep;
+  @belongsTo('subsidy-application-flow-step', { inverse: null })
+  firstApplicationStep;
   @hasMany('subsidy-application-flow-step', { inverse: 'applicationFlow' })
   definedSteps;
 
   get sortedDefinedSteps() {
-    return this.get('definedSteps').sortBy('order');
+    return this.hasMany('definedSteps').value()?.slice().sort(sortByOrder);
   }
+}
+
+function sortByOrder(itemA, itemB) {
+  return itemA?.order - itemB?.order;
 }
