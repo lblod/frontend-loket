6915ede479b916b2eed9373caab5126753f0acf3
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2023-04-21 16:25:50 +0200

    Allow users to link an instance to a concept (#297)

--- app/controllers/public-services/concept-details.js
+++ app/controllers/public-services/concept-details.js
@@ -1,6 +1,11 @@
 import Controller from '@ember/controller';
 
 export default class PublicServicesConceptDetailsController extends Controller {
-  queryParams = [{ isPreview: 'preview' }];
+  queryParams = [{ isPreview: 'preview', publicServiceId: 'id' }];
   isPreview = false;
+  publicServiceId = '';
+
+  get isLinkFlowPreview() {
+    return Boolean(this.publicServiceId);
+  }
 }
