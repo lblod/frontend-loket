42ff62932089f7c7b28517de8c8fec1a1abc48bb
Andreo Van Onckelen <andreo141mlg@gmail.com>
2023-08-11 11:15:01 +0200

    Added bestuurseenheid and besuurseenheidId to route

--- app/routes/administrative-units/administrative-unit/sites/index.js
+++ app/routes/administrative-units/administrative-unit/sites/index.js
@@ -3,32 +3,43 @@ import { inject as service } from '@ember/service';
 
 export default class AdministrativeUnitsAdministrativeUnitSitesIndexRoute extends Route {
   @service store;
+  @service session;
 
-  // async model() {
-  //   let { id: administrativeUnitId } = this.paramsFor(
-  //     'administrative-units.administrative-unit'
-  //   );
+  async model() {
+    let bestuurseenheidId =
+      this.session.data.authenticated.relationships.group.data.id;
+    let bestuurseenheid = await this.store.findRecord(
+      'bestuurseenheid',
+      bestuurseenheidId,
+      { includes: 'contactinfo.adres' }
+    );
+    let addresses = [];
+    for (let i = 0; i < bestuurseenheid.contactinfo.length; i++) {
+      addresses.push(bestuurseenheid.contactinfo[i].adres);
+    }
+    console.log(bestuurseenheid.contactinfo);
+    //   let { id: administrativeUnitId } = this.paramsFor(
+    //     'administrative-units.administrative-unit'
+    //   );
 
-  //   let administrativeUnit = await this.store.findRecord(
-  //     'administrative-unit',
-  //     administrativeUnitId,
-  //     {
-  //       reload: true,
-  //       include: [
-  //         'primary-site.address',
-  //         'primary-site.contacts',
-  //         'primary-site.site-type',
-  //         'sites.address',
-  //         'sites.contacts',
-  //         'sites.site-type',
-  //       ].join(),
-  //     }
-  //   );
+    //   let administrativeUnit = await this.store.findRecord(
+    //     'administrative-unit',
+    //     administrativeUnitId,
+    //     {
+    //       reload: true,
+    //       include: [
+    //         'primary-site.address',
+    //         'primary-site.contacts',
+    //         'primary-site.site-type',
+    //         'sites.address',
+    //         'sites.contacts',
+    //         'sites.site-type',
+    //       ].join(),
+    //     }
+    //   );
 
-  //   return {
-  //     administrativeUnit: administrativeUnit,
-  //     primarySite: await administrativeUnit.primarySite,
-  //     sites: await administrativeUnit.sites,
-  //   };
-  // }
+    return {
+      sites: addresses,
+    };
+  }
 }
