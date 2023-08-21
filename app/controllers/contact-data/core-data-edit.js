d87fb3880e4e69d793ff4b338f25fc622171eae0
Dennis Van Eecke <dennis.van.eecke@codifly.be>
2023-08-21 15:50:03 +0200

    Added one panel of the form

--- app/controllers/contact-data/core-data-edit.js
+++ app/controllers/contact-data/core-data-edit.js
@@ -1,6 +1,21 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class CoreDataOverviewController extends Controller {
   @service currentSession;
+
+  selectTypeBestuurOptions = ['Test 1', 'Test 2'];
+  selectedTypeBestuur = 'Test 1';
+
+  selectRegioOptions = ['Chocowakije', 'Carpetland'];
+  selectedRegio = 'Chocowakije';
+
+  selectStatusOptions = ['Actief'];
+  selectedStatus = 'Actief';
+
+  kboMaskOptions = {
+    mask: '9999.999.999',
+    placeholder: '0000.000.000',
+  };
 }
