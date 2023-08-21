ef0aeb7b96f21a50ec584a770f25f7182d7737b7
Dennis Van Eecke <dennis.van.eecke@codifly.be>
2023-08-21 11:33:04 +0200

    Edit pagina toegevoegd.

---
+++ app/routes/contact-data/core-data-edit.js
@@ -0,0 +1,56 @@
+import Route from '@ember/routing/route';
+
+export default class CoreDataOverviewRoute extends Route {
+  async model() {
+    const administrativeUnit = {
+      name: 'Aalst',
+      classification: {
+        label: 'OCMW',
+      },
+      organizationStatus: {
+        id: '63cc561de9188d64ba5840a42ae8f0d6',
+        label: 'Actief',
+      },
+      identifiers: [
+        {
+          idName: 'KBO nummer',
+          structuredIdentifier: {
+            localId: '0212.237.186',
+          },
+        },
+        {
+          idName: 'SharePoint identificator',
+          structuredIdentifier: {
+            localId: '324',
+          },
+        },
+        {
+          idName: 'OVO-nummer',
+          structuredIdentifier: {
+            localId: 'OVO002601',
+          },
+        },
+      ],
+      primarySite: {
+        address: {
+          fullAddress: 'Gasthuisstraat 40, 9300 Aalst, België',
+          province: 'Oost-Vlaanderen',
+        },
+      },
+      primaryContact: {
+        telephone: '081 00 0000',
+        email: 'fakeemail@gmail.com',
+        website: 'https://google.com',
+      },
+      secondaryContact: {
+        telephone: '081 00 0002',
+        email: 'fakeemail2@gmail.com',
+        website: 'https://wikipedia.org',
+      },
+    };
+    const kbo = administrativeUnit.identifiers[0].structuredIdentifier.localId;
+    const ovo = administrativeUnit.identifiers[2].structuredIdentifier.localId;
+
+    return { administrativeUnit, kbo, ovo };
+  }
+}
