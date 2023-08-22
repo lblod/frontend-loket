9ed8fead34fe204991c94bc908b026beb09e96e8
Andreo Van Onckelen <andreo141mlg@gmail.com>
2023-08-22 17:28:19 +0200

    added helper function

---
+++ app/helpers/printAsJson.js
@@ -0,0 +1,11 @@
+import { htmlSafe } from '@ember/template';
+
+export default function printAsJson(ob) {
+  return htmlSafe(
+    `<span style="white-space: pre-line;"><pre>${JSON.stringify(
+      ob,
+      undefined,
+      '&Tab;'
+    )}</</span>`
+  );
+}
