33eaa00ed866ebcbc1cd6bffd6f8b3143b9d42d1
Dennis Van Eecke <dennis.van.eecke@codifly.be>
2023-08-22 14:41:34 +0200

    WIP edit form

--- app/controllers/contact-data/core-data-edit.js
+++ app/controllers/contact-data/core-data-edit.js
@@ -14,8 +14,28 @@ export default class CoreDataOverviewController extends Controller {
   selectStatusOptions = ['Actief'];
   selectedStatus = 'Actief';
 
+  selectProvincieOptions = ['West-Vlaanderen'];
+  selectedAutomaticProvincie = 'West-Vlaanderen';
+
   kboMaskOptions = {
     mask: '9999.999.999',
     placeholder: '0000.000.000',
   };
+  postcodeMaskOptions = {
+    mask: '9999',
+    placeholder: '0000',
+  };
+  /** @type {'automatic' | 'manual'} */
+  addressInputMode = 'automatic';
+
+  selectedAutomaticAddress = 'Grote Markt 3, 9300 Aalst';
+
+  automaticAddressOptions = ['Kleine dorpstraat 99, 5000 Snuiteghem'];
+
+  automaticAddressSearchChanged(...args) {
+    console.log('automaticAddressSearchChanged', args);
+  }
+
+  municipalityOptions = ['Kerkeghem'];
+  selectedManualMunicipality = 'Kerkeghem';
 }
