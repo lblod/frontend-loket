bc8fa57c62e2e2ea127e274cb00e0dc2fd552de6
Andreo Van Onckelen <andreo141mlg@gmail.com>
2023-08-25 14:45:56 +0200

    Revert "Merge branch 'feature/clb-19-edit-core-data' into feature/clb-23-consult-site"
    This reverts commit 5fccf93ba7808d259067e45c366f4504312614e2.

--- app/controllers/contact-data/core-data-edit.js
+++ app/controllers/contact-data/core-data-edit.js
@@ -1,26 +1,52 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-import { action } from '@ember/object';
 
 export default class CoreDataEditController extends Controller {
   @service currentSession;
-  selectedName = 'Naam';
-  selectTypeBestuurOptions = ['Test 1', 'Test 2', 'Type bestuur'];
-  selectedTypeBestuur = 'Type Bestuur';
+
+  selectTypeBestuurOptions = ['Test 1', 'Test 2'];
+  selectedTypeBestuur = 'Test 1';
+
   selectRegioOptions = ['Chocowakije', 'Carpetland'];
   selectedRegio = 'Chocowakije';
+
   selectStatusOptions = ['Actief'];
   selectedStatus = 'Actief';
-  selectedKbo = '';
+
+  selectProvincieOptions = ['West-Vlaanderen'];
+  selectedAutomaticProvincie = 'West-Vlaanderen';
+
   kboMaskOptions = {
     mask: '9999.999.999',
     placeholder: '0000.000.000',
   };
-  selectedNis = '';
-  seletedOvo = '';
+  postcodeMaskOptions = {
+    mask: '9999',
+    placeholder: '0000',
+  };
+  /** @type {'automatic' | 'manual'} */
+  addressInputMode = 'automatic';
 
-  @action
-  handleContactDataForm(contactDataResult) {
-    console.log(contactDataResult);
+  selectedAutomaticAddress = 'Kleine dorpstraat 99, 5000 Snuiteghem';
+
+  automaticAddressOptions = ['Kleine dorpstraat 99, 5000 Snuiteghem'];
+
+  automaticAddressSearchChanged(...args) {
+    console.log('automaticAddressSearchChanged', args);
   }
+
+  municipalityOptions = ['Kerkeghem'];
+  selectedManualMunicipality = 'Kerkeghem';
+
+  phoneNumberMaskOptions = {
+    placeholder: '+32 400 10 20 30',
+  };
+
+  emailMaskOptions = {
+    placeholder: 'naam.achternaam@provider.be',
+  };
+
+  websiteMaskOptions = {
+    placeholder: 'naam.achternaam@provider.be',
+  };
 }
