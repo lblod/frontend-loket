8c3e170ff680e7bbafd467b8b956dfacf316ff6b
Andreo Van Onckelen <andreo141mlg@gmail.com>
2023-08-29 14:19:16 +0200

    fix: endless page-title chain bug fixed

--- app/routes/contact-data/sites/site.js
+++ app/routes/contact-data/sites/site.js
@@ -1,3 +1,19 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class ContactDataSitesSiteRoute extends Route {}
+export default class ContactDataSitesSiteIndexRoute extends Route {
+  @service store;
+
+  async model() {
+    const sites = this.modelFor('contact-data.sites');
+    let { id: siteId } = this.paramsFor('contact-data.sites.site');
+    const site = sites['sites'].find((site) => site.id === siteId);
+    const primaryContact = site['contacts'].find(
+      (contact) => contact.type === 'Primary'
+    );
+    const secondaryContact = site['contacts'].find(
+      (contact) => contact.type === 'Secondary'
+    );
+    return { site, sites, primaryContact, secondaryContact };
+  }
+}
