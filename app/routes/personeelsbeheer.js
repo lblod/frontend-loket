7a05cf245fae813041bf53ea14a0d2f12d1c914f
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2023-08-29 10:27:13 +0200

    Fix the page title in the "Personeelsbeheer" module

--- app/routes/personeelsbeheer.js
+++ app/routes/personeelsbeheer.js
@@ -14,6 +14,8 @@ export default class PersoneelsbeheerRoute extends Route {
   }
 
   model() {
-    return this.currentSession.group; // bestuurseenheid
+    return {
+      bestuurseenheid: this.currentSession.group,
+    };
   }
 }
--- app/routes/personeelsbeheer/personeelsaantallen/index.js
+++ app/routes/personeelsbeheer/personeelsaantallen/index.js
@@ -2,10 +2,11 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
 export default class PersoneelsbeheerPersoneelsaantallenIndexRoute extends Route {
+  @service currentSession;
   @service store;
 
   async model() {
-    const bestuurseenheid = this.modelFor('personeelsbeheer');
+    const bestuurseenheid = this.currentSession.group;
     const datasets = await this.store.query('employee-dataset', {
       'filter[bestuurseenheid][id]': bestuurseenheid.id,
     });
