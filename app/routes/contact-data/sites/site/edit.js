7a86d1ad56405d914ab8a3e0f4b540bedb4e4282
Andreo Van Onckelen <andreo141mlg@gmail.com>
2023-09-05 21:04:39 +0200

    Added ember changeset

--- app/routes/contact-data/sites/site/edit.js
+++ app/routes/contact-data/sites/site/edit.js
@@ -1,8 +1,20 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { getSiteValidations } from 'frontend-loket/validations/sites';
+import { createValidatedChangeset } from 'frontend-loket/utils/changeset';
 
 export default class ContactDataSitesSiteEditRoute extends Route {
   @service currentSession;
+  @service store;
+  @service router;
+
+  beforeModel() {
+    if (!this.currentSession.canEdit) {
+      this.router.transitionTo('route-not-found', {
+        wildcard: 'pagina-niet-gevonden',
+      });
+    }
+  }
 
   async model() {
     const sites = this.modelFor('contact-data.sites');
@@ -14,6 +26,11 @@ export default class ContactDataSitesSiteEditRoute extends Route {
     const secondaryContact = site['contacts'].find(
       (contact) => contact.type === 'Secondary'
     );
-    return { site, sites, primaryContact, secondaryContact };
+    return {
+      site: createValidatedChangeset(site, getSiteValidations()),
+      sites,
+      primaryContact,
+      secondaryContact,
+    };
   }
 }
