57dae28b22e5dda0b893ad69d60b36f6bd627f23
Andreo Van Onckelen <andreo141mlg@gmail.com>
2023-09-05 21:07:56 +0200

    Added powersearch for site type

--- app/controllers/contact-data/sites/site/index.js
+++ app/controllers/contact-data/sites/site/index.js
@@ -1,6 +1,6 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 
-export default class SitesSiteEditController extends Controller {
+export default class SitesSiteIndexController extends Controller {
   @service currentSession;
 }
