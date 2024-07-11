e8c2b09e65812fb143b88df44b38d4d34da4abdd
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2024-07-11 11:11:17 +0200

    Add a status filter to the "Toezicht" overview page

--- app/controllers/supervision/submissions/index.js
+++ app/controllers/supervision/submissions/index.js
@@ -1,15 +1,24 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 import { CONCEPT_STATUS } from 'frontend-loket/models/submission-document-status';
 
 export default class SupervisionSubmissionsIndexController extends Controller {
   @service router;
   @service store;
 
-  page = 0;
+  queryParams = ['page', 'size', 'sort', 'status'];
+
+  @tracked status;
+  @tracked page = 0;
   size = 20;
   sort = '-modified';
+
+  @action handleStatusFilterChange(statusUri) {
+    this.status = statusUri;
+    this.page = 0;
+  }
 
   @action
   async reopen(submission) {
