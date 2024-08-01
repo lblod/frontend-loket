218fd80f6ce5a220badabe5cbede2f942087b38d
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2024-08-01 17:39:58 +0200

    Update to Appuniversum v3.5.0

--- config/dependency-lint.js
+++ config/dependency-lint.js
@@ -2,4 +2,7 @@
 
 module.exports = {
   generateTests: false,
+  allowedVersions: {
+    'ember-template-imports': '^3.0.0 || ^4.0.0', // This is a build-time addon so we don't need a single version. We can remove this once we update to v4 in the app itself.
+  },
 };
