1f994fe4273566501f653afc20453b8a395a67e0
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2024-12-02 17:18:38 +0100

    Disable the prototype extensions
    More info: https://deprecations.emberjs.com/id/deprecate-array-prototype-extensions

--- app/app.js
+++ app/app.js
@@ -17,14 +17,6 @@ export default class App extends Application {
   Resolver = Resolver;
 }
 
-// By default `Array.prototype._super` is enumerable which causes conflicts with rdflib.
-// We can force it to be non-enumerable so everything works as expected.
-// This code can be removed once the bug is fixed and released, or if we disable the prototype extensions in the app.
-// More information: https://github.com/emberjs/ember.js/issues/19289
-Object.defineProperty(Array.prototype, '_super', {
-  enumerable: false,
-});
-
 browserUpdate({
   vs: { i: 11, f: -3, o: -3, s: -3, c: -3 },
   style: 'corner',
