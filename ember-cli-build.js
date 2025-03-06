53b0c18e055721593ed6d4114b9251fa82a7f88c
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2025-03-06 11:17:03 +0100

    Resolve an EmberData deprecation
    This resolves the "ember-data:deprecate-store-extends-ember-object" deprecation by adding some build config.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,7 +2,9 @@
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-module.exports = function (defaults) {
+module.exports = async function (defaults) {
+  const { setConfig } = await import('@warp-drive/build-config');
+
   const app = new EmberApp(defaults, {
     // This is needed when `staticEmberSource` is enabled
     'ember-fetch': {
@@ -18,6 +20,12 @@ module.exports = function (defaults) {
     '@appuniversum/ember-appuniversum': {
       dutchDatePickerLocalization: true,
       disableWormholeElement: true,
+    },
+  });
+
+  setConfig(app, __dirname, {
+    deprecations: {
+      DEPRECATE_STORE_EXTENDS_EMBER_OBJECT: false,
     },
   });
 
