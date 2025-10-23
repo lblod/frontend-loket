7f1e2a3e7fe7d0559aadd63663de4d92a223a720
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2025-10-23 15:10:09 +0200

    Fix the class name of the Website model

--- app/models/website.js
+++ app/models/website.js
@@ -1,6 +1,6 @@
 import Model, { attr, belongsTo } from '@ember-data/model';
 
-export default class PublicServiceModel extends Model {
+export default class WebsiteModel extends Model {
   @attr('language-string-set') title;
   @attr('language-string-set') description;
   @attr('string') url;
