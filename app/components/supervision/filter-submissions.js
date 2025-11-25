b7e1985e8348d86718fe0a1380adc98f808d29d1
NikiDeVisscher <niki.de.visscher@gmail.com>
2025-11-25 17:09:56 +0100

    Search decision type in filter

--- app/components/supervision/filter-submissions.js
+++ app/components/supervision/filter-submissions.js
@@ -2,7 +2,7 @@ import Component from '@glimmer/component';
 import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import { task } from 'ember-concurrency';
+import { task, restartableTask, timeout } from 'ember-concurrency';
 import InzendingenFilter from 'frontend-loket/utils/inzendingen-filter';
 import { typeOf } from '@ember/utils';
 
@@ -39,13 +39,21 @@ export default class SupervisionFilterSubmissions extends Component {
     return this.besluitTypes.length === 0;
   }
 
-  get selectedBesluitTypesNull() {
-    const { besluitTypeUri } = this.args;
-    if (besluitTypeUri && !this.isLoading) {
-      return this.besluitTypes.find((besluitType) => besluitType.uri === besluitTypeUri);
-    }
+  @restartableTask
+  *search(term) {
+    yield timeout(600);
+    let results = yield this.store.query('concept', {
+      filter: {
+        label: term,
+        'concept-schemes': {
+          ':uri:': 'http://lblod.data.gift/concept-schemes/71e6455e-1204-46a6-abf4-87319f58eaa5',
+        },
+      },
+      sort: 'label',
+      page: { size: 100 },
+    });
 
-    return null;
+    return results.slice();
   }
 
   @action
