743973d0922b28bf6adab43d53691c0166dc75b5
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2026-08-12 10:47:16 +0200

    Update to Ember v6.7
    This is the last release without Vite as the default. We can also switch to the legacy blueprint from here so we can do the switch to Vite later.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -42,9 +42,7 @@ module.exports = async function (defaults) {
   return require('@embroider/compat').compatBuild(app, Webpack, {
     staticAddonTestSupportTrees: true,
     staticAddonTrees: true,
-    staticHelpers: true,
-    staticModifiers: true,
-    staticComponents: true,
+    staticInvokables: true,
     staticEmberSource: true,
     splitAtRoutes: [
       'mock-login',
