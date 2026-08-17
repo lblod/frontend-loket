a199780308fcd794b668b1a64adbc1440378e3c2
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2026-08-17 12:28:39 +0200

    Remove ember-async-data
    We only use it in a single place that can easily be replaced with a different pattern. WarpDrive also includes a similar feature, so we can just use that if we need it in the future.

--- app/components/broader-concept-pills.js
+++ app/components/broader-concept-pills.js
@@ -1,3 +1,4 @@
+import { assert } from '@ember/debug';
 import Component from '@glimmer/component';
 import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
@@ -7,13 +8,23 @@ export default class BroaderConceptPillsComponent extends Component {
   @service store;
 
   @tracked concept;
+  @tracked parents;
 
   constructor() {
     super(...arguments);
     this.loadData.perform();
   }
 
+  get label() {
+    assert(
+      'parents is expected to be loaded before we access it here.',
+      Array.isArray(this.parents) && this.parents.length > 0,
+    );
+    return this.parents.at(0).label;
+  }
+
   loadData = keepLatestTask(async () => {
     this.concept = await this.store.findRecordByUri('concept', this.args.uri);
+    this.parents = await this.concept.broader;
   });
 }
